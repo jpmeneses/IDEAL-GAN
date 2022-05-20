0f0f6add8c3ab12de4c2a53f458fe7261ad7b5ca
jpmeneses <jpmeneses@uc.cl>
2022-05-20 10:03:52 -0400

    Original version: Update n_batch; edit gitignore

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -7,4 +7,4 @@
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-IDEAL.py --dataset 'IDEAL-GAN-001' --FM_D_model 'PatchGAN' --R2_D_model 'PatchGAN' --n_G_filters 32 --n_D_filters 64 --batch_size 8 --epochs 50 --epoch_decay 50 --lr 0.0001 --D_R2_nds 1 --D_FM_nds 3 --cycle_loss_weight 100.0 --B2A2B_weight 0.5 --R2_critic_weight 1.0
+python train-IDEAL.py --dataset 'IDEAL-GAN-001' --FM_D_model 'PatchGAN' --R2_D_model 'PatchGAN' --n_G_filters 32 --n_D_filters 64 --batch_size 4 --epochs 50 --epoch_decay 50 --lr 0.0001 --D_R2_nds 1 --D_FM_nds 3 --cycle_loss_weight 100.0 --B2A2B_weight 0.5 --R2_critic_weight 1.0
