bc1a8a3073663e1d3e5ae4cd1330dcffa9a8744f
jpmeneses <jpmeneses@uc.cl>
2022-05-22 21:53:47 -0400

    Submit v003 (increase L1_FM weight)

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v002-IDEALGAN
-#SBATCH --output=out_IDEALGAN_002.txt
+#SBATCH --job-name=v003-IDEALGAN
+#SBATCH --output=out_IDEALGAN_003.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-IDEAL.py --dataset 'IDEAL-GAN-002' --n_G_filters 32 --n_D_filters 64 --batch_size 4 --epochs 50 --epoch_decay 50 --lr 0.0001 --cycle_loss_weight 100.0 --B2A2B_weight 0.1 --R2_critic_weight 1.0 --FM_L1_weight 1.0
+python train-IDEAL.py --dataset 'IDEAL-GAN-003' --n_G_filters 32 --n_D_filters 64 --batch_size 4 --epochs 50 --epoch_decay 50 --lr 0.0001 --cycle_loss_weight 100.0 --B2A2B_weight 0.1 --R2_critic_weight 1.0 --FM_L1_weight 10.0
