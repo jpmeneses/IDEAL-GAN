Correct R2* scaling

DLlib/complex_utils.py
@@ -85,7 +85,7 @@ class complex_Conv2D(Layer):
             tf_output = cardioid(tf.complex(real_out, imag_out))
         elif self.activation == 'last_layer':
             real_out = tf.nn.tanh(real_out)
-            imag_out = tf.nn.sigmoid(imag_out/np.pi)
+            imag_out = tf.nn.sigmoid(imag_out)
             tf_output = tf.complex(real_out, imag_out)
 
         return tf_output

train-IDEAL.py
@@ -232,7 +232,7 @@ def train_G(A, B, te_A=None, te_B=None, ep=args.epochs):
                 A2B_FM = (A2B_FM - 0.5) * 2
                 A2B_PM = tf.concat([A2B_R2,A2B_FM],axis=-1)
         else:
-            A2B_R2 = tf.math.imag(A2B_PM)/np.pi
+            A2B_R2 = tf.math.imag(A2B_PM)*(2*np.pi)
             A2B_FM = tf.math.real(A2B_PM)
             A2B_PM = tf.concat([A2B_R2,A2B_FM],axis=-1)
             A2B_PM = tf.where(A[:,:,:,:2]!=0.0,A2B_PM,0.0)
@@ -263,7 +263,7 @@ def train_G(A, B, te_A=None, te_B=None, ep=args.epochs):
             B2A2B_FM = (B2A2B_FM - 0.5) * 2
             B2A2B_PM = tf.concat([B2A2B_R2,B2A2B_FM],axis=-1)
         elif args.G_model == 'complex':
-            B2A2B_PM = tf.concat([tf.math.imag(B2A2B_PM)/np.pi,tf.math.real(B2A2B_PM)],axis=-1)
+            B2A2B_PM = tf.concat([tf.math.imag(B2A2B_PM)*(2*np.pi),tf.math.real(B2A2B_PM)],axis=-1)
         
         # B2A2B Mask
         B2A2B_PM = tf.where(B_PM!=0.0,B2A2B_PM,0.0)
@@ -389,7 +389,7 @@ def sample(A, B, te_B=None):
         A2B_FM = (A2B_FM - 0.5) * 2
         A2B_PM = tf.concat([A2B_R2,A2B_FM],axis=-1)
     elif args.G_model == 'complex':
-        A2B_R2 = tf.math.imag(A2B_PM)/np.pi
+        A2B_R2 = tf.math.imag(A2B_PM)*(2*np.pi)
         A2B_FM = tf.math.real(A2B_PM)
         A2B_PM = tf.concat([A2B_R2,A2B_FM],axis=-1)
     # A2B Mask
@@ -405,7 +405,7 @@ def sample(A, B, te_B=None):
     else:
         B2A2B_PM = G_A2B([B2A,(te_B-1e-3)/(11.5*1e-3)], training=False)
     if args.G_model == 'complex':
-        B2A2B_R2 = tf.math.imag(B2A2B_PM)/np.pi
+        B2A2B_R2 = tf.math.imag(B2A2B_PM)*(2*np.pi)
         B2A2B_FM = tf.math.real(B2A2B_PM)
         B2A2B_PM = tf.concat([B2A2B_R2,B2A2B_FM],axis=-1)
     # B2A2B Mask