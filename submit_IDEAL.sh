a0db2e5a6a373aab6081e4a5102c9ff1cc609969
jpmeneses <jpmeneses@uc.cl>
2022-05-30 15:15:33 -0400

    Update v006: Add TV weighting

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -7,4 +7,4 @@
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-IDEAL.py --dataset 'IDEAL-GAN-006' --G_model 'complex' --n_G_filters 32 --n_D_filters 64 --batch_size 4 --epochs 50 --epoch_decay 50 --lr 0.0001 --cycle_loss_weight 100.0 --B2A2B_weight 1.0
+python train-IDEAL.py --dataset 'IDEAL-GAN-006' --G_model 'complex' --n_G_filters 32 --n_D_filters 64 --batch_size 4 --epochs 50 --epoch_decay 50 --lr 0.0001 --cycle_loss_weight 100.0 --B2A2B_weight 1.0 --R2_TV_weight 0.0001 --FM_TV_weight 0.0001
