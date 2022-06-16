52ddc9827b0de5cd8873473fcb7850b9185a5444
jpmeneses <jpmeneses@uc.cl>
2022-06-16 17:20:24 -0400

    Re-submit v003: Update R2s TV reg (x10^-1)

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -7,4 +7,4 @@
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-IDEAL.py --dataset 'IDEAL-GAN-003' --G_model 'encod-decod' --n_G_filters 32 --n_D_filters 32 --batch_size 4 --epochs 40 --epoch_decay 60 --epoch_ckpt 5 --lr 0.0002 --cycle_loss_weight 100.0 --B2A2B_weight 1.0 --R2_TV_weight 0.00001
+python train-IDEAL.py --dataset 'IDEAL-GAN-003' --G_model 'encod-decod' --n_G_filters 32 --n_D_filters 32 --batch_size 4 --epochs 40 --epoch_decay 60 --epoch_ckpt 5 --lr 0.0002 --cycle_loss_weight 100.0 --B2A2B_weight 1.0 --R2_TV_weight 0.000001
