040124db84d87b8fdfaa2a8265f7315816618f39
jpmeneses <jpmeneses@uc.cl>
2022-06-23 20:30:22 -0400

    Submit Unsup-v001: Includes R2s from the beginning

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v000-unsup
-#SBATCH --output=out_unsup_000.txt
+#SBATCH --job-name=v001-unsup
+#SBATCH --output=out_unsup_001.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-IDEAL-unsup.py --dataset 'Unsup-000' --G_model 'encod-decod' --n_G_filters 32 --batch_size 1 --epochs 50 --epoch_decay 50 --epoch_ckpt 5 --lr 0.0001 --beta_1 0.9 --beta_2 0.999
+python train-IDEAL-unsup.py --dataset 'Unsup-001' --G_model 'encod-decod' --n_G_filters 32 --batch_size 1 --epochs 30 --epoch_decay 30 --epoch_ckpt 5 --lr 0.0001 --beta_1 0.9 --beta_2 0.999
