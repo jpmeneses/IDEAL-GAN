57142fbc100d1ae7495464adb161a195b46aaebe
jpmeneses <jpmeneses@uc.cl>
2022-06-23 20:17:51 -0400

    Re-submit Unsup-v000: Train more epochs, with R2s

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -7,4 +7,4 @@
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-IDEAL-unsup.py --dataset 'Unsup-000' --G_model 'encod-decod' --n_G_filters 32 --batch_size 1 --epochs 25 --epoch_decay 25 --epoch_ckpt 5 --lr 0.0001 --beta_1 0.9 --beta_2 0.999 --R2_L1_weight 1.0
+python train-IDEAL-unsup.py --dataset 'Unsup-000' --G_model 'encod-decod' --n_G_filters 32 --batch_size 1 --epochs 50 --epoch_decay 50 --epoch_ckpt 5 --lr 0.0001 --beta_1 0.9 --beta_2 0.999
