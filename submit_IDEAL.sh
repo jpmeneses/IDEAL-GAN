1f6b7fcfa40b075bbed4a23039916aa7e05b780d
jpmeneses <jpmeneses@uc.cl>
2022-06-29 11:17:31 -0400

    Submit IDEAL-GAN-v006: High R2-TV regularization

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v005-IDEALGAN
-#SBATCH --output=out_IDEALGAN_005.txt
+#SBATCH --job-name=v006-IDEALGAN
+#SBATCH --output=out_IDEALGAN_006.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-IDEAL.py --dataset 'IDEAL-GAN-005' --G_model 'encod-decod' --n_G_filters 32 --batch_size 4 --epochs 50 --epoch_decay 50 --epoch_ckpt 5 --lr 0.0002 --FM_fix True
+python train-IDEAL.py --dataset 'IDEAL-GAN-006' --G_model 'encod-decod' --n_G_filters 32 --batch_size 4 --epochs 50 --epoch_decay 50 --epoch_ckpt 5 --lr 0.0002 --R2_TV_weight 0.0001
