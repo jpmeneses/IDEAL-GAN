741b103f3c1b6b08d131d7e5bf5fb7064de7c222
jpmeneses <jpmeneses@uc.cl>
2022-06-29 09:15:38 -0400

    Re-submit IDEAL-GAN-v005: Continue training with fixed FM-decod weights

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -7,4 +7,4 @@
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-IDEAL.py --dataset 'IDEAL-GAN-005' --G_model 'encod-decod' --n_G_filters 32 --batch_size 4 --epochs 30 --epoch_decay 30 --epoch_ckpt 5 --lr 0.0002 --R2_TV_weight 2.0e-05
+python train-IDEAL.py --dataset 'IDEAL-GAN-005' --G_model 'encod-decod' --n_G_filters 32 --batch_size 4 --epochs 50 --epoch_decay 50 --epoch_ckpt 5 --lr 0.0002 --FM_fix True
