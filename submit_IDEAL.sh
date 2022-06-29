ca757c57e7bbb17129a4a408383ee686578b8045
jpmeneses <jpmeneses@uc.cl>
2022-06-29 11:19:46 -0400

    Re-submit IDEAL-GAN-v006: Fewer training epochs (40)

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -7,4 +7,4 @@
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-IDEAL.py --dataset 'IDEAL-GAN-006' --G_model 'encod-decod' --n_G_filters 32 --batch_size 4 --epochs 50 --epoch_decay 50 --epoch_ckpt 5 --lr 0.0002 --R2_TV_weight 0.0001
+python train-IDEAL.py --dataset 'IDEAL-GAN-006' --G_model 'encod-decod' --n_G_filters 32 --batch_size 4 --epochs 40 --epoch_decay 40 --epoch_ckpt 5 --lr 0.0002 --R2_TV_weight 0.0001
