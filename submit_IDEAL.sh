f2fb69688fe411fa0fdf9c6a6aa4f23624be55a3
jpmeneses <jpmeneses@uc.cl>
2022-06-30 16:20:39 -0400

    Submit Unsup-v002: Decrease R2s-TV regularization

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v003-unsup
-#SBATCH --output=out_unsup_003.txt
+#SBATCH --job-name=v002-unsup
+#SBATCH --output=out_unsup_002.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-IDEAL-unsup.py --dataset 'Unsup-003' --G_model 'encod-decod' --n_G_filters 32 --batch_size 1 --epochs 35 --epoch_decay 35 --epoch_ckpt 5 --lr 0.0001 --FM_fix True
+python train-IDEAL-unsup.py --dataset 'Unsup-002' --G_model 'encod-decod' --n_G_filters 32 --batch_size 1 --epochs 25 --epoch_decay 25 --epoch_ckpt 5 --lr 0.0001 --R2_TV_weight 0.00001
