71e3d2f1ed9b97b0ebf9a26a79a60512fdaf7939
jpmeneses <jpmeneses@uc.cl>
2022-06-30 11:06:58 -0400

    Submit Unsup-v003: Strong R2s TV regularization

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v006-IDEALGAN
-#SBATCH --output=out_IDEALGAN_006.txt
+#SBATCH --job-name=v003-unsup
+#SBATCH --output=out_unsup_006.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-IDEAL.py --dataset 'IDEAL-GAN-006' --G_model 'encod-decod' --n_G_filters 32 --batch_size 4 --epochs 60 --epoch_decay 60 --epoch_ckpt 5 --lr 0.0002 --FM_fix True
+python train-IDEAL-unsup.py --dataset 'Unsup-003' --G_model 'encod-decod' --n_G_filters 32 --batch_size 1 --epochs 25 --epoch_decay 25 --epoch_ckpt 5 --lr 0.0001 --beta_1 0.9 --beta_2 0.999 --R2_TV_weight 0.0001
