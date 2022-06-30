8f1044267b60a7c07f7b8a1fcabe7978aafeb53c
jpmeneses <jpmeneses@uc.cl>
2022-06-30 12:40:20 -0400

    Re-submit Unsup-v003: Train for 15 more epochs

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v005-IDEALGAN
-#SBATCH --output=out_IDEALGAN_005.txt
+#SBATCH --job-name=v003-unsup
+#SBATCH --output=out_unsup_003.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-IDEAL.py --dataset 'IDEAL-GAN-005' --G_model 'encod-decod' --n_G_filters 32 --batch_size 4 --epochs 40 --epoch_decay 40 --epoch_ckpt 5 --lr 0.0002 --R2_TV_weight 0.00005
+python train-IDEAL-unsup.py --dataset 'Unsup-003' --G_model 'encod-decod' --n_G_filters 32 --batch_size 1 --epochs 40 --epoch_decay 40 --epoch_ckpt 5 --lr 0.0001 --R2_TV_weight 0.0001
