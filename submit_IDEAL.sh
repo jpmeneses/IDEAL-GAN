43eaa296367f54eebfc2956a08f3b8e8e50c2fc2
jpmeneses <jpmeneses@uc.cl>
2022-07-01 12:45:29 -0400

    Re-submit Unsup-v002: Increase R2s-TV regularization

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -7,4 +7,4 @@
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-IDEAL-unsup.py --dataset 'Unsup-002' --G_model 'encod-decod' --n_G_filters 32 --batch_size 1 --epochs 25 --epoch_decay 25 --epoch_ckpt 5 --lr 0.0001 --R2_TV_weight 0.00001
+python train-IDEAL-unsup.py --dataset 'Unsup-002' --G_model 'encod-decod' --n_G_filters 32 --batch_size 1 --epochs 25 --epoch_decay 25 --epoch_ckpt 5 --lr 0.0001 --R2_TV_weight 0.00005
