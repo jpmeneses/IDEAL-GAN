7909e0dae994e03a098c4e79f362555f2c5cbf24
jpmeneses <jpmeneses@uc.cl>
2022-07-03 21:47:05 -0400

    Re-submit IDEAL-GAN-v002: Train from 55th ckpt, with fixed FM

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v002-unsup
-#SBATCH --output=out_unsup_002.txt
+#SBATCH --job-name=v002-IDEALGAN
+#SBATCH --output=out_IDEALGAN_002.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-IDEAL-unsup.py --dataset 'Unsup-002' --G_model 'encod-decod' --n_G_filters 32 --batch_size 1 --epochs 25 --epoch_decay 25 --epoch_ckpt 5 --lr 0.0001 --R2_TV_weight 0.00005
+python train-IDEAL.py --dataset 'IDEAL-GAN-002' --G_model 'encod-decod' --n_G_filters 32 --n_D_filters 32 --batch_size 4 --epochs 80 --epoch_decay 80 --epoch_ckpt 5 --lr 0.0002 --cycle_loss_weight 100.0 --FM_fix True
