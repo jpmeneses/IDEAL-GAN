b9db88f4ebd97528e8b17bcd31f1c2d09da4742f
jpmeneses <jpmeneses@uc.cl>
2022-07-11 17:43:01 -0400

    Submit Unsup-v004: Edit beta_1 and beta_2

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v006-IDEALGAN
-#SBATCH --output=out_IDEALGAN_006.txt
+#SBATCH --job-name=v004-unsup
+#SBATCH --output=out_unsup_004.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-IDEAL.py --dataset 'IDEAL-GAN-006' --G_model 'encod-decod' --n_G_filters 32 --n_D_filters 32 --batch_size 4 --epochs 30 --epoch_decay 30 --epoch_ckpt 5 --lr 0.0002 --cycle_loss_weight 100 --R2_TV_weight 0.0001
+python train-IDEAL-unsup.py --dataset 'Unsup-004' --G_model 'encod-decod' --n_G_filters 32 --batch_size 1 --epochs 30 --epoch_decay 30 --epoch_ckpt 5 --lr 0.0001 --beta_1 0.9 --beta_2 0.999 --R2_TV_weight 0.00005
