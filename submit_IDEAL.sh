9f39aa6d0d2411a9fdc5773048ada526865557e9
jpmeneses <jpmeneses@uc.cl>
2022-07-11 16:11:05 -0400

    Debug Unsup-v003: Change .py file

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -7,4 +7,4 @@
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-IDEAL.py --dataset 'Unsup-003' --G_model 'encod-decod' --n_G_filters 32 --batch_size 1 --epochs 30 --epoch_decay 30 --epoch_ckpt 5 --lr 0.0001 --R2_TV_weight 0.00005
+python train-IDEAL-unsup.py --dataset 'Unsup-003' --G_model 'encod-decod' --n_G_filters 32 --batch_size 1 --epochs 30 --epoch_decay 30 --epoch_ckpt 5 --lr 0.0001 --R2_TV_weight 0.00005
