e19e5348e5b60f8ef6fc606d960a9712d20a0048
jpmeneses <jpmeneses@uc.cl>
2022-07-12 11:19:42 -0400

    Re-submit IDEAL-GAN-v006: Train over-regularized for 10 more epochs

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v003-unsup
-#SBATCH --output=out_unsup_003.txt
+#SBATCH --job-name=v006-IDEALGAN
+#SBATCH --output=out_IDEALGAN_006.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-IDEAL-unsup.py --dataset 'Unsup-003' --G_model 'encod-decod' --n_G_filters 32 --batch_size 1 --epochs 40 --epoch_decay 40 --epoch_ckpt 5 --lr 0.0001 --beta_1 0.9 --beta_2 0.999 --FM_fix True
+python train-IDEAL-unsup.py --dataset 'IDEAL-GAN-006' --G_model 'encod-decod' --n_G_filters 32 --batch_size 4 --epochs 40 --epoch_decay 40 --epoch_ckpt 5 --lr 0.0002 --cycle_loss_weight 100 --R2_TV_weight 0.00003
