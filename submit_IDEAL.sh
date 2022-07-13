5a4bb3e136836c5a6042b889316e6160a8d9956b
jpmeneses <jpmeneses@uc.cl>
2022-07-12 23:09:11 -0400

    Debug Sup-v001: Reduce batch size and learning rate

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -7,4 +7,4 @@
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-sup.py --dataset 'Sup-001' --out_vars 'WF' --G_model 'MEBCRN' 
+python train-sup.py --dataset 'Sup-001' --out_vars 'WF' --G_model 'MEBCRN' --batch_size 8 --lr 0.0002
