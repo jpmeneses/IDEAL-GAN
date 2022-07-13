f279c26fafd75cd537a1c8e6f108172ec83dcf9e
jpmeneses <jpmeneses@uc.cl>
2022-07-13 14:58:34 -0400

    Re-submit IDEAL-GAN-v007: Train for 5 more epochs with fixed FM and decreased R2s critic weight

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v001-sup
-#SBATCH --output=out_sup_001.txt
+#SBATCH --job-name=v007-IDEALGAN
+#SBATCH --output=out_IDEALGAN_007.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-sup.py --dataset 'Sup-002' --out_vars 'PM' --G_model 'U-Net' 
+python train-IDEAL.py --dataset 'IDEAL-GAN-007' --G_model 'encod-decod' --n_G_filters 32 --n_D_filters 32 --batch_size 4 --epochs 40 --epoch_decay 40 --epoch_ckpt 1 --lr 0.0002 --cycle_loss_weight 100 --FM_fix True
