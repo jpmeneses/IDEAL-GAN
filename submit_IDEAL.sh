5b8186df28bc3d1c848055f28cd3945011a9dca4
jpmeneses <jpmeneses@uc.cl>
2022-07-14 09:51:48 -0400

    Re-submit Sup-v003: Multi-decoder for PM estimation (with modified loss)

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v002-sup
-#SBATCH --output=out_sup_002.txt
+#SBATCH --job-name=v003-sup
+#SBATCH --output=out_sup_003.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-sup.py --dataset 'Sup-002' --G_model 'U-Net' --n_filters 72
+python train-sup.py --dataset 'Sup-003' --G_model 'multi-decod' --n_filters 72
