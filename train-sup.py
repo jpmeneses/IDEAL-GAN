3a81c5e3b46eb85739555c106df7b55da38dbce0
jpmeneses <jpmeneses@uc.cl>
2022-07-16 00:42:02 -0400

    Submit Sup-v004

--- train-sup.py
+++ train-sup.py
@@ -267,7 +267,7 @@ def train_G(A, B):
             if args.G_model=='U-Net' or args.G_model=='MEBCRN':
                 A2B_FM = (A2B_FM - 0.5) * 2
                 A2B_FM = tf.where(A[:,:,:,:1]!=0.0,A2B_FM,0.0)
-                A2B_abs = tf.stack([A2B_WF_abs,A2B_R2,A2B_FM],axis=-1)
+                A2B_abs = tf.concat([A2B_WF_abs,A2B_R2,A2B_FM],axis=-1)
 
             # Compute loss
             sup_loss = sup_loss_fn(B_abs, A2B_abs)
