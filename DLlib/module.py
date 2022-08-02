Re-submit Sup-v004: Correct output's masking

DLlib/module.py
@@ -1,6 +1,7 @@
 import tensorflow as tf
 import tensorflow_addons as tfa
 import tensorflow.keras as keras
+import tensorflow_probability as tfp
 
 from DLlib import bn, complex_utils
 from DLlib.attention import SelfAttention, AdaIN
@@ -131,6 +132,7 @@ def MDWF_Generator(
     te_shape=(6,),
     filters=72,
     num_layers=4,
+    dropout=0.0,
     WF_self_attention=False,
     R2_self_attention=False,
     FM_self_attention=True,
@@ -144,6 +146,7 @@ def MDWF_Generator(
     def _conv2d_block(
         inputs,
         filters=16,
+        dropout=0.0,
         kernel_size=(3, 3),
         kernel_initializer="he_normal",
         padding="same",
@@ -157,6 +160,8 @@ def MDWF_Generator(
             use_bias=False,
             )(inputs)
         c = Norm()(c)
+        if dropout > 0.0:
+            c = keras.layers.SpatialDropout2D(dropout)(c)
         c = keras.layers.Conv2D(
             filters,
             kernel_size,
@@ -177,6 +182,7 @@ def MDWF_Generator(
         x = _conv2d_block(
             inputs=x,
             filters=filters,
+            dropout=dropout
             )
         down_layers.append(x)
         x = keras.layers.MaxPooling2D((2, 2))(x)
@@ -197,6 +203,7 @@ def MDWF_Generator(
     x = _conv2d_block(
         inputs=x,
         filters=filters,
+        dropout=dropout
         )
 
     cont = 0
@@ -217,7 +224,8 @@ def MDWF_Generator(
             x2 = SelfAttention(ch=2*filters)(x2)
         x2 = _conv2d_block(
             inputs=x2,
-            filters=filters
+            filters=filters,
+            dropout=dropout
             )
 
         # R2* decoder
@@ -226,7 +234,8 @@ def MDWF_Generator(
             x3 = SelfAttention(ch=2*filters)(x3)
         x3 = _conv2d_block(
             inputs=x3,
-            filters=filters
+            filters=filters,
+            dropout=dropout
             )
 
         # Field map decoder
@@ -235,7 +244,8 @@ def MDWF_Generator(
             x4 = SelfAttention(ch=2*filters)(x4)
         x4 = _conv2d_block(
             inputs=x4,
-            filters=filters
+            filters=filters,
+            dropout=dropout
             )
 
         # Update counter
@@ -255,10 +265,12 @@ def MDWF_Generator(
 
 def PM_Generator(
     input_shape,
+    bayesian=False,
     te_input=False,
     te_shape=(6,),
     filters=72,
     num_layers=4,
+    dropout=0.0,
     R2_self_attention=False,
     FM_self_attention=True,
     norm='instance_norm'):
@@ -271,6 +283,7 @@ def PM_Generator(
     def _conv2d_block(
         inputs,
         filters=16,
+        dropout=0.0,
         kernel_size=(3, 3),
         kernel_initializer="he_normal",
         padding="same",
@@ -283,8 +296,9 @@ def PM_Generator(
             padding=padding,
             use_bias=False,
             )(inputs)
-        # c = tf.nn.relu6(c)
         c = Norm()(c)
+        if dropout > 0.0:
+            c = keras.layers.SpatialDropout2D(dropout)(c)
         c = keras.layers.Conv2D(
             filters,
             kernel_size,
@@ -293,7 +307,6 @@ def PM_Generator(
             padding=padding,
             use_bias=False,
             )(c)
-        # c = tf.nn.relu6(c)
         c = Norm()(c)
         return c
 
@@ -306,6 +319,7 @@ def PM_Generator(
         x = _conv2d_block(
             inputs=x,
             filters=filters,
+            dropout=dropout
             )
 
         if te_input:
@@ -322,6 +336,7 @@ def PM_Generator(
     x = _conv2d_block(
         inputs=x,
         filters=filters,
+        dropout=dropout
         )
 
     cont = 0
@@ -340,7 +355,8 @@ def PM_Generator(
             x2 = SelfAttention(ch=2*filters)(x2)
         x2 = _conv2d_block(
             inputs=x2,
-            filters=filters
+            filters=filters,
+            dropout=dropout
             )
 
         # Field map decoder
@@ -349,15 +365,19 @@ def PM_Generator(
             x3 = SelfAttention(ch=2*filters)(x3)
         x3 = _conv2d_block(
             inputs=x3,
-            filters=filters
+            filters=filters,
+            dropout=dropout
             )
 
         # Update counter
         cont += 1
 
-    x2 = keras.layers.Conv2D(1, (1, 1), activation='sigmoid', kernel_initializer='glorot_normal')(x2)
-    # x2 = tf.nn.relu6(x2)
-    x3 = keras.layers.Conv2D(1, (1, 1), activation='tanh', kernel_initializer='glorot_normal')(x3)
+    if not(bayesian):
+        x2 = keras.layers.Conv2D(1, (1, 1), activation='sigmoid', kernel_initializer='glorot_normal')(x2)
+        x3 = keras.layers.Conv2D(1, (1, 1), activation='tanh', kernel_initializer='glorot_normal')(x3)
+    else:
+        x2 = tfp.layers.Convolution2DFlipout(1, (1, 1), activation='sigmoid')(x2)
+        x3 = tfp.layers.Convolution2DFlipout(1, (1, 1), activation='tanh')(x3)
 
     outputs = keras.layers.concatenate([x2,x3])
 

test-IDEAL.py
@@ -21,7 +21,12 @@ from skimage.metrics import structural_similarity
 # ==============================================================================
 
 py.arg('--experiment_dir',default='output/WF-IDEAL')
+py.arg('--out_vars', default='PM', choices=['WF','PM','WF-PM'])
 py.arg('--te_input', type=bool, default=False)
+py.arg('--single_slice', type=bool, default=False)
+py.arg('--n_G_filters', type=int, default=72)
+py.arg('--D1_SelfAttention',type=bool, default=False)
+py.arg('--D2_SelfAttention',type=bool, default=True)
 test_args = py.args()
 args = py.args_from_yaml(py.join(test_args.experiment_dir, 'settings.yml'))
 args.__dict__.update(test_args.__dict__)
@@ -79,6 +84,13 @@ n4_div = 434 # 113
 testX   = np.concatenate((acqs_1[:n1_div,:,:,:],acqs_4[:n4_div,:,:,:]),axis=0)
 testY   = np.concatenate((out_maps_1[:n1_div,:,:,:],out_maps_4[:n4_div,:,:,:]),axis=0)
 
+# Sample to plot
+samples =  [12,36,56,78,104,128,151,173,195,212,238,284,260,307,331,354,379,
+            404,429,455,478,500,527,548,571,596,621,648,671]
+if args.single_slice:
+    testX = testX[samples,:,:,:]
+    testY = testY[samples,:,:,:]
+
 # Overall dataset statistics
 len_dataset,hgt,wdt,d_ech = np.shape(testX)
 _,_,_,n_out = np.shape(testY)
@@ -97,72 +109,103 @@ A_B_dataset_test = tf.data.Dataset.from_tensor_slices((testX,testY))
 A_B_dataset_test.batch(1)
 
 # model
-with tf.device('/cpu:0'):
-    if args.G_model == 'encod-decod':
+if args.G_model == 'multi-decod' or args.G_model == 'encod-decod':
+    if args.out_vars == 'WF-PM':
+        G_A2B=dl.MDWF_Generator(input_shape=(hgt,wdt,d_ech),
+                                te_input=args.te_input,
+                                filters=args.n_filters,
+                                dropout=0.0,
+                                WF_self_attention=args.D1_SelfAttention,
+                                R2_self_attention=args.D2_SelfAttention,
+                                FM_self_attention=args.D3_SelfAttention)
+    else:
         G_A2B = dl.PM_Generator(input_shape=(hgt,wdt,d_ech),
                                 te_input=args.te_input,
                                 te_shape=(args.n_echoes,),
-                                filters=args.n_G_filters,
-                                R2_self_attention=args.R2_SelfAttention,
-                                FM_self_attention=args.FM_SelfAttention)
-    elif args.G_model == 'U-Net':
-        G_A2B = custom_unet(input_shape=(hgt,wdt,d_ech),
-                            num_classes=2,
-                            use_attention=args.FM_SelfAttention,
-                            filters=args.n_G_filters)
-    elif args.G_model == 'MEBCRN':
-        G_A2B  =  dl.MEBCRN(input_shape=(hgt,wdt,d_ech),
-                            n_res_blocks=5,
-                            n_downsamplings=2,
-                            filters=args.n_G_filters,
-                            self_attention=args.FM_SelfAttention)
-    else:
-        raise(NameError('Unrecognized Generator Architecture'))
+                                filters=args.n_filters,
+                                dropout=0.0,
+                                R2_self_attention=args.D1_SelfAttention,
+                                FM_self_attention=args.D2_SelfAttention)
 
-    # restore
-    tl.Checkpoint(dict(G_A2B=G_A2B), py.join(args.experiment_dir, 'checkpoints')).restore()
+elif args.G_model == 'U-Net':
+    if args.out_vars == 'WF-PM':
+        n_out = 4
+    else:
+        n_out = 2
+    G_A2B = custom_unet(input_shape=(hgt,wdt,d_ech),
+                        num_classes=n_out,
+                        dropout=0.0,
+                        use_dropout_on_upsampling=True,
+                        use_attention=args.D1_SelfAttention,
+                        filters=args.n_filters)
+
+elif args.G_model == 'MEBCRN':
+    if args.out_vars == 'WF-PM':
+        n_out = 4
+    else:
+        n_out = 2
+    G_A2B=dl.MEBCRN(input_shape=(hgt,wdt,d_ech),
+                    n_outputs=n_out,
+                    n_res_blocks=5,
+                    n_downsamplings=2,
+                    filters=args.n_filters,
+                    self_attention=args.D1_SelfAttention)
+
+else:
+    raise(NameError('Unrecognized Generator Architecture'))
+
+# restore
+tl.Checkpoint(dict(G_A2B=G_A2B), py.join(args.experiment_dir, 'checkpoints')).restore()
 
 @tf.function
 def sample_A2B(A,TE=None):
+    indx_B = tf.concat([tf.zeros_like(A[:,:,:,:4],dtype=tf.int32),
+                        tf.ones_like(A[:,:,:,:2],dtype=tf.int32)],axis=-1)
+    indx_B_abs = tf.concat([tf.zeros_like(A[:,:,:,:2],dtype=tf.int32),
+                            tf.ones_like(A[:,:,:,:2],dtype=tf.int32)],axis=-1)
     indx_PM =tf.concat([tf.zeros_like(A[:,:,:,:1],dtype=tf.int32),
                         tf.ones_like(A[:,:,:,:1],dtype=tf.int32)],axis=-1)
-    if args.te_input and (TE is not(None)):
-        A2B_PM = G_A2B([A,TE], training=False)
-    else:
-        A2B_PM = G_A2B(A, training=False)
-    if args.G_model == 'U-Net':
-        orig_shape = A2B_PM.shape
-        A2B_R2, A2B_FM = tf.dynamic_partition(A2B_PM,indx_PM,num_partitions=2)
-        A2B_R2 = tf.reshape(A2B_R2,A[:,:,:,:1].shape)
-        A2B_FM = tf.reshape(A2B_FM,A[:,:,:,:1].shape)
-        A2B_FM = (A2B_FM - 0.5) * 2
-        A2B_PM = tf.concat([A2B_R2,A2B_FM],axis=-1)
-    A2B_PM = tf.where(A[:,:,:,:2]!=0.0,A2B_PM,0)
-    A2B_WF, A2B2A = wf.acq_to_acq(A,A2B_PM,TE)
-    A2B = tf.concat([A2B_WF,A2B_PM],axis=-1)
-    return A2B, A2B2A
+    # Estimate A2B
+    if args.out_vars == 'WF':
+        A2B_WF_abs = G_A2B(A, training=False)
+        A2B_WF_abs = tf.where(A[:,:,:,:2]!=0.0,A2B_WF_abs,0.0)
+        A2B_PM = tf.zeros_like(A[:,:,:,:2])
+        A2B_abs = tf.concat([A2B_WF_abs,A2B_PM],axis=-1)
+    elif args.out_vars == 'PM':
+        if args.te_input:
+            A2B_PM = G_A2B([A,TE], training=False)
+        else:
+            A2B_PM = G_A2B(A, training=False)
+        A2B_PM = tf.where(A[:,:,:,:2]!=0.0,A2B_PM,0.0)
+        if args.G_model=='U-Net' or args.G_model=='MEBCRN':
+            A2B_R2, A2B_FM = tf.dynamic_partition(A2B_PM,indx_PM,num_partitions=2)
+            A2B_R2 = tf.reshape(A2B_R2,A[:,:,:,:1].shape)
+            A2B_FM = tf.reshape(A2B_FM,A[:,:,:,:1].shape)
+            A2B_FM = (A2B_FM - 0.5) * 2
+            A2B_FM = tf.where(A[:,:,:,:1]!=0.0,A2B_FM,0.0)
+            A2B_PM = tf.concat([A2B_R2,A2B_FM],axis=-1)
+        A2B_WF = wf.get_rho(A,A2B_PM)
+        A2B_WF_real = A2B_WF[:,:,:,0::2]
+        A2B_WF_imag = A2B_WF[:,:,:,1::2]
+        A2B_WF_abs = tf.abs(tf.complex(A2B_WF_real,A2B_WF_imag))
+        A2B_abs = tf.concat([A2B_WF_abs,A2B_PM],axis=-1)
+    elif args.out_vars == 'WF-PM':
+        if args.te_input:
+            A2B_abs = G_A2B([A,TE], training=False)
+        else:
+            A2B_abs = G_A2B(A, training=False)
+        A2B_WF_abs,A2B_PM = tf.dynamic_partition(A2B_abs,indx_B_abs,num_partitions=2)
+        A2B_WF_abs = tf.reshape(A2B_WF_abs,A[:,:,:,:2].shape)
+        A2B_PM = tf.reshape(A2B_PM,A[:,:,:,:2].shape)
+        if args.G_model=='U-Net' or args.G_model=='MEBCRN':
+            A2B_R2, A2B_FM = tf.dynamic_partition(A2B_PM,indx_PM,num_partitions=2)
+            A2B_R2 = tf.reshape(A2B_R2,A[:,:,:,:1].shape)
+            A2B_FM = tf.reshape(A2B_FM,A[:,:,:,:1].shape)
+            A2B_FM = (A2B_FM - 0.5) * 2
+            A2B_FM = tf.where(A[:,:,:,:1]!=0.0,A2B_FM,0.0)
+            A2B_abs = tf.concat([A2B_WF_abs,A2B_R2,A2B_FM],axis=-1)
 
-
-@tf.function
-def sample_B2A(B,TE=None):
-    indx_PM =tf.concat([tf.zeros_like(B[:,:,:,:1],dtype=tf.int32),
-                        tf.ones_like(B[:,:,:,:1],dtype=tf.int32)],axis=-1)
-    B2A = wf.IDEAL_model(B,echoes)
-    if args.te_input and (TE is not(None)):
-        B2A2B_PM = G_A2B([B2A,TE], training=False)
-    else:
-        B2A2B_PM = G_A2B(B2A, training=False)
-    if args.G_model == 'U-Net':
-        orig_shape = B2A2B_PM.shape
-        B2A2B_R2, B2A2B_FM = tf.dynamic_partition(A2B_PM,indx_PM,num_partitions=2)
-        B2A2B_R2 = tf.reshape(B2A2B_R2,B[:,:,:,:1].shape)
-        B2A2B_FM = tf.reshape(B2A2B_FM,B[:,:,:,:1].shape)
-        B2A2B_FM = (B2A2B_FM - 0.5) * 2
-        B2A2B_PM = tf.concat([B2A2B_R2,B2A2B_FM],axis=-1)
-        B2A2B_PM = tf.reshape(B2A2B_PM,orig_shape)
-    B2A2B_WF = wf.get_rho(B2A,B2A2B_PM,TE)
-    B2A2B = tf.concat([B2A2B_WF,B2A2B_PM],axis=-1)
-    return B2A, B2A2B
+    return A2B_abs
 
 
 # run
@@ -178,72 +221,66 @@ for A, B in tqdm.tqdm(A_B_dataset_test, desc='Testing Samples Loop', total=len_d
     B = tf.expand_dims(B,axis=0)
 
     if args.te_input:
-        A2B, A2B2A = sample_A2B(A,TE)
+        A2B = sample_A2B(A,TE)
     else:
-        A2B, A2B2A = sample_A2B(A)
+        A2B = sample_A2B(A)
     
-    # fig, ((ax1,ax2,ax3,ax4),(ax5,ax6,ax7,ax8))=plt.subplots(figsize=(14, 6),
-    #                                                         nrows=2, ncols=4)
-    # # Ground truth in the first row
-    w_aux = np.squeeze(np.abs(tf.complex(A2B[:,:,:,0],A2B[:,:,:,1])))
-    # W_ok = ax1.imshow(w_aux, cmap='bone',
-    #                   interpolation='none', vmin=0, vmax=1)
-    # fig.colorbar(W_ok, ax=ax1)
-    # ax1.axis('off')
+    fig, ((ax1,ax2,ax3,ax4),(ax5,ax6,ax7,ax8))=plt.subplots(figsize=(14, 6),
+                                                         nrows=2, ncols=4)
+    # Ground truth in the first row
+    w_aux = np.squeeze(A2B[:,:,:,0])
+    W_ok = ax1.imshow(w_aux, cmap='bone', interpolation='none', vmin=0, vmax=1)
+    fig.colorbar(W_ok, ax=ax1)
+    ax1.axis('off')
 
-    f_aux = np.squeeze(np.abs(tf.complex(A2B[:,:,:,2],A2B[:,:,:,3])))
-    # F_ok = ax2.imshow(f_aux, cmap='pink',
-    #                   interpolation='none', vmin=0, vmax=1)
-    # fig.colorbar(F_ok, ax=ax2)
-    # ax2.axis('off')
+    f_aux = np.squeeze(A2B[:,:,:,1])
+    F_ok = ax2.imshow(f_aux, cmap='pink', interpolation='none', vmin=0, vmax=1)
+    fig.colorbar(F_ok, ax=ax2)
+    ax2.axis('off')
 
-    r2_aux = np.squeeze(A2B[:,:,:,4])
-    # r2_ok = ax3.imshow(r2_aux*r2_sc, cmap='copper',
-    #                    interpolation='none', vmin=0, vmax=r2_sc)
-    # fig.colorbar(r2_ok, ax=ax3)
-    # ax3.axis('off')
+    r2_aux = np.squeeze(A2B[:,:,:,2])
+    r2_ok = ax3.imshow(r2_aux*r2_sc, cmap='copper', interpolation='none', vmin=0, vmax=r2_sc)
+    fig.colorbar(r2_ok, ax=ax3)
+    ax3.axis('off')
 
-    field_aux = np.squeeze(A2B[:,:,:,5])
-    # field_ok = ax4.imshow(field_aux*fm_sc, cmap='twilight',
-    #                       interpolation='none', vmin=-fm_sc/2, vmax=fm_sc/2)
-    # fig.colorbar(field_ok, ax=ax4)
-    # ax4.axis('off')
+    field_aux = np.squeeze(A2B[:,:,:,3])
+    field_ok = ax4.imshow(field_aux*fm_sc, cmap='twilight', interpolation='none', vmin=-fm_sc/2, vmax=fm_sc/2)
+    fig.colorbar(field_ok, ax=ax4)
+    ax4.axis('off')
 
-    # # Computed maps in the second row
+    # Computed maps in the second row
     wn_aux = np.squeeze(np.abs(tf.complex(B[:,:,:,0],B[:,:,:,1])))
-    # W_unet = ax5.imshow(wn_aux, cmap='bone',
-    #                     interpolation='none', vmin=0, vmax=1)
-    # fig.colorbar(W_unet, ax=ax5)
-    # ax5.axis('off')
+    W_unet = ax5.imshow(wn_aux, cmap='bone', interpolation='none', vmin=0, vmax=1)
+    fig.colorbar(W_unet, ax=ax5)
+    ax5.axis('off')
 
     fn_aux = np.squeeze(np.abs(tf.complex(B[:,:,:,2],B[:,:,:,3])))
-    # F_unet = ax6.imshow(fn_aux, cmap='pink',
-    #                     interpolation='none', vmin=0, vmax=1)
-    # fig.colorbar(F_unet, ax=ax6)
-    # ax6.axis('off')
+    F_unet = ax6.imshow(fn_aux, cmap='pink', interpolation='none', vmin=0, vmax=1)
+    fig.colorbar(F_unet, ax=ax6)
+    ax6.axis('off')
 
     r2n_aux = np.squeeze(B[:,:,:,4])
-    # r2_unet = ax7.imshow(r2n_aux*r2_sc, cmap='copper',
-    #                      interpolation='none', vmin=0, vmax=r2_sc)
-    # fig.colorbar(r2_unet, ax=ax7)
-    # ax7.axis('off')
+    r2_unet = ax7.imshow(r2n_aux*r2_sc, cmap='copper', interpolation='none', vmin=0, vmax=r2_sc)
+    fig.colorbar(r2_unet, ax=ax7)
+    ax7.axis('off')
 
     fieldn_aux = np.squeeze(B[:,:,:,5])
-    # field_unet = ax8.imshow(fieldn_aux*fm_sc, cmap='twilight',
-    #                         interpolation='none', vmin=-fm_sc/2, vmax=fm_sc/2)
-    # fig.colorbar(field_unet, ax=ax8)
-    # ax8.axis('off')
+    field_unet = ax8.imshow(fieldn_aux*fm_sc, cmap='twilight', interpolation='none', vmin=-fm_sc/2, vmax=fm_sc/2)
+    fig.colorbar(field_unet, ax=ax8)
+    ax8.axis('off')
 
-    # # plt.show()
-    # plt.gca().set_axis_off()
-    # plt.subplots_adjust(top = 1, bottom = 0, right = 1, left = 0, 
-    #             hspace = 0.1, wspace = 0)
-    # plt.margins(0,0)
-    # plt.gca().xaxis.set_major_locator(plt.NullLocator())
-    # plt.gca().yaxis.set_major_locator(plt.NullLocator())
-    # plt.savefig(save_dir+'/sample'+str(i).zfill(3)+'.png',bbox_inches = 'tight',
-    #     pad_inches = 0)
-    # plt.close(fig)
+    # plt.show()
+    plt.gca().set_axis_off()
+    plt.subplots_adjust(top = 1, bottom = 0, right = 1, left = 0, 
+                        hspace = 0.1, wspace = 0)
+    plt.margins(0,0)
+    plt.gca().xaxis.set_major_locator(plt.NullLocator())
+    plt.gca().yaxis.set_major_locator(plt.NullLocator())
+    if not(args.single_slice):
+        plt.savefig(save_dir+'/sample'+str(i).zfill(3)+'.png',bbox_inches = 'tight', pad_inches = 0)
+    else:
+        plt.savefig(save_dir+'/sample'+str(samples[i]).zfill(3)+'.png',bbox_inches = 'tight', pad_inches = 0)
+    plt.close(fig)
 
     PDFF_aux = f_aux/(w_aux+f_aux)
     PDFF_aux[np.isnan(PDFF_aux)] = 0.0

test-uncertainty.py
@@ -21,7 +21,12 @@ from keras_unet.models import custom_unet
 # ==============================================================================
 
 py.arg('--experiment_dir',default='output/WF-IDEAL')
-py.arg('--batch_size', type=int, default=4)
+py.arg('--out_vars', default='PM', choices=['WF','PM','WF-PM'])
+py.arg('--te_input', type=bool, default=False)
+py.arg('--n_filters', type=int, default=32)
+py.arg('--batch_size', type=int, default=1)
+py.arg('--D1_SelfAttention',type=bool, default=False)
+py.arg('--D2_SelfAttention',type=bool, default=True)
 test_args = py.args()
 args = py.args_from_yaml(py.join(test_args.experiment_dir, 'settings.yml'))
 args.__dict__.update(test_args.__dict__)
@@ -32,9 +37,9 @@ args.__dict__.update(test_args.__dict__)
 # ==============================================================================
 workbook = xlsxwriter.Workbook(py.join(args.experiment_dir, 'uncertainty.xlsx'))
 ws_unc = workbook.add_worksheet('Mean Unc')
-ws_unc.write(0,0,'Water')
-ws_unc.write(0,1,'Fat')
-ws_unc.write(0,2,'PDFF')
+ws_unc.write(0,0,'Num. Sample')
+ws_unc.write(0,1,'Water')
+ws_unc.write(0,2,'Fat')
 ws_unc.write(0,3,'R2*')
 ws_unc.write(0,4,'FieldMap')
 
@@ -70,7 +75,8 @@ testX   = np.concatenate((acqs_1[:n1_div,:,:,:],acqs_4[:n4_div,:,:,:]),axis=0)
 testY   = np.concatenate((out_maps_1[:n1_div,:,:,:],out_maps_4[:n4_div,:,:,:]),axis=0)
 
 # Sample to plot
-samples = [12,36,128,173,260,379,478,527,648]
+samples =  [12,36,56,78,104,128,151,173,195,212,238,284,260,307,331,354,379,
+            404,429,455,478,500,527,548,571,596,621,648,671]
 testX = testX[samples,:,:,:]
 testY = testY[samples,:,:,:]
 
@@ -95,16 +101,21 @@ A_ds_test.batch(args.batch_size)
 # ==============================================================================
 
 # with tf.device('/cpu:0'):
-if args.G_model == 'multi-decod':
+if args.G_model == 'multi-decod' or args.G_model == 'encod-decod':
     if args.out_vars == 'WF-PM':
         G_A2B=dl.MDWF_Generator(input_shape=(hgt,wdt,d_ech),
+                                te_input=args.te_input,
                                 filters=args.n_filters,
+                                dropout=0.01,
                                 WF_self_attention=args.D1_SelfAttention,
                                 R2_self_attention=args.D2_SelfAttention,
                                 FM_self_attention=args.D3_SelfAttention)
     else:
         G_A2B = dl.PM_Generator(input_shape=(hgt,wdt,d_ech),
+                                te_input=args.te_input,
+                                te_shape=(args.n_echoes,),
                                 filters=args.n_filters,
+                                dropout=0.01,
                                 R2_self_attention=args.D1_SelfAttention,
                                 FM_self_attention=args.D2_SelfAttention)
 
@@ -143,9 +154,11 @@ tl.Checkpoint(dict(G_A2B=G_A2B), py.join(args.experiment_dir, 'checkpoints')).re
 # ==============================================================================
 
 @tf.function
-def sample_A2B(A):
+def sample_A2B(A,TE=None):
     indx_B = tf.concat([tf.zeros_like(A[:,:,:,:4],dtype=tf.int32),
                         tf.ones_like(A[:,:,:,:2],dtype=tf.int32)],axis=-1)
+    indx_B_abs = tf.concat([tf.zeros_like(A[:,:,:,:2],dtype=tf.int32),
+                            tf.ones_like(A[:,:,:,:2],dtype=tf.int32)],axis=-1)
     indx_PM =tf.concat([tf.zeros_like(A[:,:,:,:1],dtype=tf.int32),
                         tf.ones_like(A[:,:,:,:1],dtype=tf.int32)],axis=-1)
     # Estimate A2B
@@ -155,13 +168,17 @@ def sample_A2B(A):
         A2B_PM = tf.zeros_like(A[:,:,:,:2])
         A2B_abs = tf.concat([A2B_WF_abs,A2B_PM],axis=-1)
     elif args.out_vars == 'PM':
-        A2B_PM = G_A2B(A, training=True)
+        if args.te_input:
+            A2B_PM = G_A2B([A,TE], training=True)
+        else:
+            A2B_PM = G_A2B(A, training=True)
         A2B_PM = tf.where(A[:,:,:,:2]!=0.0,A2B_PM,0.0)
         if args.G_model=='U-Net' or args.G_model=='MEBCRN':
             A2B_R2, A2B_FM = tf.dynamic_partition(A2B_PM,indx_PM,num_partitions=2)
             A2B_R2 = tf.reshape(A2B_R2,A[:,:,:,:1].shape)
             A2B_FM = tf.reshape(A2B_FM,A[:,:,:,:1].shape)
             A2B_FM = (A2B_FM - 0.5) * 2
+            A2B_FM = tf.where(A[:,:,:,:1]!=0.0,A2B_FM,0.0)
             A2B_PM = tf.concat([A2B_R2,A2B_FM],axis=-1)
         A2B_WF = wf.get_rho(A,A2B_PM)
         A2B_WF_real = A2B_WF[:,:,:,0::2]
@@ -169,15 +186,20 @@ def sample_A2B(A):
         A2B_WF_abs = tf.abs(tf.complex(A2B_WF_real,A2B_WF_imag))
         A2B_abs = tf.concat([A2B_WF_abs,A2B_PM],axis=-1)
     elif args.out_vars == 'WF-PM':
-        A2B_abs = G_A2B(A, training=True)
+        if args.te_input:
+            A2B_abs = G_A2B([A,TE], training=True)
+        else:
+            A2B_abs = G_A2B(A, training=True)
+        A2B_abs = tf.where(A[:,:,:,:4]!=0.0,A2B_abs,0.0)
         if args.G_model=='U-Net' or args.G_model=='MEBCRN':
-            A2B_WF_abs,A2B_PM = tf.dynamic_partition(A2B_abs,indx_B,num_partitions=2)
+            A2B_WF_abs,A2B_PM = tf.dynamic_partition(A2B_abs,indx_B_abs,num_partitions=2)
             A2B_WF_abs = tf.reshape(A2B_WF_abs,A[:,:,:,:2].shape)
-            A2B_PM = tf.reshape(A2B_PM,A[:,:,:,4:].shape)
+            A2B_PM = tf.reshape(A2B_PM,A[:,:,:,:2].shape)
             A2B_R2, A2B_FM = tf.dynamic_partition(A2B_PM,indx_PM,num_partitions=2)
             A2B_R2 = tf.reshape(A2B_R2,A[:,:,:,:1].shape)
             A2B_FM = tf.reshape(A2B_FM,A[:,:,:,:1].shape)
             A2B_FM = (A2B_FM - 0.5) * 2
+            A2B_FM = tf.where(A[:,:,:,:1]!=0.0,A2B_FM,0.0)
             A2B_abs = tf.concat([A2B_WF_abs,A2B_R2,A2B_FM],axis=-1)
 
     return A2B_abs
@@ -191,6 +213,11 @@ py.mkdir(save_dir)
 
 dropout_levels = [0.01,0.02,0.05,0.1,0.2,0.5]
 
+if args.te_input:
+    TE_smp = wf.gen_TEvar(args.n_echoes,args.batch_size,orig=True)
+else:
+    TE_smp = None
+
 i = 0
 for d_lev in dropout_levels:
     print('Dropout level:',str(d_lev))
@@ -198,17 +225,15 @@ for d_lev in dropout_levels:
         if isinstance(layer,tf.keras.layers.Dropout):
             layer.rate = d_lev
 
-    d_lev_Y = tf.zeros_like(testX)
     sl = 0
     for X_batch in A_ds_test.as_numpy_iterator():
         X_batch = tf.expand_dims(X_batch,0)
-        slice_d_lev_Y = sample_A2B(X_batch)
+        slice_d_lev_Y = sample_A2B(X_batch,TE=TE_smp)
         slice_d_lev_Y = tf.where(X_batch[:,:,:,:4]!=0.0,slice_d_lev_Y,0.0)
-        # Estimate PDFF
-        PDFF_batch = slice_d_lev_Y[:,:,:,1]/(slice_d_lev_Y[:,:,:,0]+slice_d_lev_Y[:,:,:,1])
-        # PDFF_batch[np.isnan(PDFF_batch)] = 0.0
-        PDFF_batch = tf.expand_dims(PDFF_batch,axis=-1)
-        slice_d_lev_Y = tf.concat([slice_d_lev_Y,PDFF_batch],axis=-1)
+        # PDFF_batch = X_batch[:,:,:,1]/(X_batch[:,:,:,0]+X_batch[:,:,:,1])
+        # PDFF_batch = tf.where(tf.math.is_nan(PDFF_batch),0.0,PDFF_batch)
+        # PDFF_batch = tf.expand_dims(PDFF_batch,axis=-1)
+        # slice_d_lev_Y = tf.concat([slice_d_lev_Y,PDFF_batch],axis=-1)
         if sl == 0:
             d_lev_Y = slice_d_lev_Y
         else:
@@ -224,9 +249,15 @@ for d_lev in dropout_levels:
 
 print('Computing Uncertainty Maps...')
 uncertain_maps = tf.math.reduce_variance(dropout_Y,axis=0)
+# uncertain_maps = tf.where(tf.math.is_nan(uncertain_maps),0.0,uncertain_maps)
 
 print('Saving images...')
-for n_sample in range(len(samples)):
+for n_sample in range(len_dataset):
+    wn_unc = uncertain_maps[n_sample,:,:,0]
+    fn_unc = uncertain_maps[n_sample,:,:,1]
+    r2n_unc = uncertain_maps[n_sample,:,:,2]*np.square(r2_sc)
+    FMn_unc = uncertain_maps[n_sample,:,:,3]*np.square(fm_sc)
+
     out_Y_1 = dropout_Y[0]
     A2B_1 = out_Y_1[n_sample,:,:,:]
     out_Y_mid = dropout_Y[len(dropout_levels)//2]
@@ -235,9 +266,12 @@ for n_sample in range(len(samples)):
     A2B_end = out_Y_end[n_sample,:,:,:]
     B = testY[n_sample,:,:,:]
 
+    # ==========================================================================
+    # =                           Fig 1: W/F images                            =
+    # ==========================================================================
     fig, axs = plt.subplots(figsize=(14, 6), nrows=2, ncols=4)
 
-    # Smallest dropout W/F images in the second column
+    # Smallest dropout W/F images in the first column
     wn_aux_1 = A2B_1[:,:,0]
     fn_aux_1 = A2B_1[:,:,1]
     Wn_ok_1 = axs[0,0].imshow(wn_aux_1, cmap='bone', interpolation='none', vmin=0, vmax=1)
@@ -247,7 +281,7 @@ for n_sample in range(len(samples)):
     fig.colorbar(Fn_ok_1, ax=axs[1,0])
     axs[1,0].axis('off')
 
-    # Smallest dropout W/F images in the second column
+    # Medium dropout W/F images in the second column
     wn_aux_mid = A2B_mid[:,:,0]
     fn_aux_mid = A2B_mid[:,:,1]
     Wn_ok_mid = axs[0,1].imshow(wn_aux_mid, cmap='bone', interpolation='none', vmin=0, vmax=1)
@@ -268,8 +302,6 @@ for n_sample in range(len(samples)):
     axs[1,2].axis('off')
 
     # Uncertainty images in the fourth column
-    wn_unc = uncertain_maps[n_sample,:,:,0]
-    fn_unc = uncertain_maps[n_sample,:,:,1]
     Wn_unc = axs[0,3].imshow(wn_unc, cmap='cividis', interpolation='none', vmin=0, vmax=0.001)
     fig.colorbar(Wn_unc, ax=axs[0,3])
     axs[0,3].axis('off')
@@ -283,21 +315,76 @@ for n_sample in range(len(samples)):
     plt.margins(0,0)
     plt.gca().xaxis.set_major_locator(plt.NullLocator())
     plt.gca().yaxis.set_major_locator(plt.NullLocator())
-    plt.savefig(save_dir+'/sample'+str(samples[n_sample]).zfill(3)+'.png',
+    plt.savefig(save_dir+'/WF_sample'+str(samples[n_sample]).zfill(3)+'.png',
         bbox_inches = 'tight', pad_inches = 0)
     plt.close(fig)
 
-    # Mean uncertainty
+    # ==========================================================================
+    # =                       Fig 2: Quantitative maps                         =
+    # ==========================================================================
+    if not(args.out_vars == 'WF'):
+        fig, axs = plt.subplots(figsize=(14, 6), nrows=2, ncols=4)
+
+        # Smallest dropout param maps in the first column
+        r2n_aux_1 = A2B_1[:,:,2]*r2_sc
+        FMn_aux_1 = A2B_1[:,:,3]*fm_sc
+        r2n_ok_1 = axs[0,0].imshow(r2n_aux_1, cmap='copper', interpolation='none', vmin=0, vmax=r2_sc)
+        fig.colorbar(r2n_ok_1, ax=axs[0,0])
+        axs[0,0].axis('off')
+        FMn_ok_1 = axs[1,0].imshow(FMn_aux_1, cmap='twilight', interpolation='none', vmin=-fm_sc, vmax=fm_sc)
+        fig.colorbar(FMn_ok_1, ax=axs[1,0])
+        axs[1,0].axis('off')
+
+        # Medium dropout param maps in the second column
+        r2n_aux_mid = A2B_mid[:,:,2]*r2_sc
+        FMn_aux_mid = A2B_mid[:,:,3]*fm_sc
+        r2n_ok_mid = axs[0,1].imshow(r2n_aux_mid, cmap='copper', interpolation='none', vmin=0, vmax=r2_sc)
+        fig.colorbar(r2n_ok_mid, ax=axs[0,1])
+        axs[0,1].axis('off')
+        FMn_ok_mid = axs[1,1].imshow(FMn_aux_mid, cmap='twilight', interpolation='none', vmin=-fm_sc, vmax=fm_sc)
+        fig.colorbar(FMn_ok_mid, ax=axs[1,1])
+        axs[1,1].axis('off')
+
+        # Largest dropout param maps in the third column
+        r2n_aux_end = A2B_end[:,:,2]*r2_sc
+        FMn_aux_end = A2B_end[:,:,3]*fm_sc
+        r2n_ok_end = axs[0,2].imshow(r2n_aux_end, cmap='copper', interpolation='none', vmin=0, vmax=r2_sc)
+        fig.colorbar(r2n_ok_end, ax=axs[0,2])
+        axs[0,2].axis('off')
+        FMn_ok_end = axs[1,2].imshow(FMn_aux_end, cmap='twilight', interpolation='none', vmin=-fm_sc, vmax=fm_sc)
+        fig.colorbar(FMn_ok_end, ax=axs[1,2])
+        axs[1,2].axis('off')
+
+        # Uncertainty images in the fourth column
+        r2n_ok_unc = axs[0,3].imshow(r2n_unc, cmap='plasma', interpolation='none', vmin=0, vmax=10.0)
+        fig.colorbar(r2n_ok_unc, ax=axs[0,3])
+        axs[0,3].axis('off')
+        FMn_ok_unc = axs[1,3].imshow(FMn_unc, cmap='viridis', interpolation='none', vmin=0, vmax=10.0)
+        fig.colorbar(FMn_ok_unc, ax=axs[1,3])
+        axs[1,3].axis('off')
+
+        plt.gca().set_axis_off()
+        plt.subplots_adjust(top = 1, bottom = 0, right = 1, left = 0, 
+                    hspace = 0.1, wspace = 0)
+        plt.margins(0,0)
+        plt.gca().xaxis.set_major_locator(plt.NullLocator())
+        plt.gca().yaxis.set_major_locator(plt.NullLocator())
+        plt.savefig(save_dir+'/PM_sample'+str(samples[n_sample]).zfill(3)+'.png',
+            bbox_inches = 'tight', pad_inches = 0)
+        plt.close(fig)
+
+    # ==========================================================================
+    # =                       Save mean uncertainties                          =
+    # ==========================================================================
     unc_w = np.mean(wn_unc)
     unc_f = np.mean(fn_unc)
-    # unc_pdff = np.sqrt(np.mean(tf.square(PDFF_aux-PDFFn_aux), axis=(0,1)))
-    # unc_r2 = np.sqrt(np.mean(tf.square(r2_aux-r2n_aux), axis=(0,1)))
-    # unc_fm = np.sqrt(np.mean(tf.square(field_aux-fieldn_aux), axis=(0,1)))
+    unc_r2 = np.mean(r2n_unc)
+    unc_fm = np.mean(FMn_unc)
 
-    ws_unc.write(n_sample+1,0,unc_w)
-    ws_unc.write(n_sample+1,1,unc_f)
-    # ws_MSE.write(i+1,2,MSE_pdff)
-    # ws_MSE.write(i+1,3,MSE_r2)
-    # ws_MSE.write(i+1,4,MSE_fm)
+    ws_unc.write(n_sample+1,0,n_sample)
+    ws_unc.write(n_sample+1,1,unc_w)
+    ws_unc.write(n_sample+1,2,unc_f)
+    ws_unc.write(n_sample+1,3,unc_r2)
+    ws_unc.write(n_sample+1,4,unc_fm)
 
 workbook.close()