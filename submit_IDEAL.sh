c6581fd9f549e199490a9663d63fde1583644256
jpmeneses <jpmeneses@uc.cl>
2022-08-02 17:08:59 -0400

    Submit Sup-v006: WF-PM U-Net without Self-Attention

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v004-sup
-#SBATCH --output=out_sup_004.txt
+#SBATCH --job-name=v006-sup
+#SBATCH --output=out_sup_006.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-sup.py --dataset 'Sup-004' --out_vars 'WF-PM' --G_model 'U-Net' --n_filters 72 --D1_SelfAttention True
+python train-sup.py --dataset 'Sup-006' --out_vars 'WF-PM' --G_model 'U-Net' --n_filters 72
