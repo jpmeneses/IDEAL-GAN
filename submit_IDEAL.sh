d39b79f4a276d46da424fe0e92cec2c39fb2569a
jpmeneses <jpmeneses@uc.cl>
2022-08-12 11:49:16 -0400

    Submit TEaug-v003: More training data and 3 echoes

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v002-TEaug
-#SBATCH --output=out_TEaug_002.txt
+#SBATCH --job-name=v003-TEaug
+#SBATCH --output=out_TEaug_003.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-IDEAL-TEaug.py --dataset 'TEaug-002' --epochs 100 --epoch_decay 100 --lr 0.0001 --beta_1 0.9 --beta_2 0.999
+python train-IDEAL-TEaug.py --dataset 'TEaug-003' --n_echoes 3 --epochs 100 --epoch_decay 100 --lr 0.0001 --beta_1 0.9 --beta_2 0.999
