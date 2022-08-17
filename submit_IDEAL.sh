5451f8742f0ec8cc68ef401982fe479f39a86383
jpmeneses <jpmeneses@uc.cl>
2022-08-17 09:49:27 -0400

    Debug Unsup-v004: Correct R2_TV_weight argument

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -7,4 +7,4 @@
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-IDEAL-unsup.py --dataset 'Unsup-004' --UQ True --epochs 30 --epoch_decay 30 --lr 0.0002 --beta_1 0.9 --beta_2 0.999 --R2_TV_weight: 0.0001
+python train-IDEAL-unsup.py --dataset 'Unsup-004' --UQ True --epochs 30 --epoch_decay 30 --lr 0.0002 --beta_1 0.9 --beta_2 0.999 --R2_TV_weight 0.0001
