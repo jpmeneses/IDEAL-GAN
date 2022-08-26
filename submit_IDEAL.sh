0060ac44285178bf7cd2962ab2c9b5976b805999
jpmeneses <jpmeneses@uc.cl>
2022-08-26 11:56:50 -0400

    Submit TEaug-v004: 72 filters, 200 epochs

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v003-TEaug
-#SBATCH --output=out_TEaug_003.txt
+#SBATCH --job-name=v004-TEaug
+#SBATCH --output=out_TEaug_004.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-IDEAL-TEaug.py --dataset 'TEaug-003' --n_G_filters 32 --epochs 200 --epoch_decay 200 --epoch_ckpt 25 --lr 0.0001 --beta_1 0.9 --beta_2 0.999
+python train-IDEAL-TEaug.py --dataset 'TEaug-004' --epochs 200 --epoch_decay 200 --epoch_ckpt 25 --lr 0.0001 --beta_1 0.9 --beta_2 0.999
