d9543a1141c714740fb1b62b9cb37b2b060bf9b9
jpmeneses <jpmeneses@uc.cl>
2022-08-26 11:52:32 -0400

    Submit TEaug-v004: Reduce number of conv filters (32)

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -7,4 +7,4 @@
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-IDEAL-TEaug.py --dataset 'TEaug-003' --epochs 200 --epoch_decay 200 --epoch_ckpt 25 --lr 0.0001 --beta_1 0.9 --beta_2 0.999
+python train-IDEAL-TEaug.py --dataset 'TEaug-003' --n_G_filters 32 --epochs 200 --epoch_decay 200 --epoch_ckpt 25 --lr 0.0001 --beta_1 0.9 --beta_2 0.999
