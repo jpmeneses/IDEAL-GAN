4741791d46153c86a040f0e8de23ba2e15bca65f
jpmeneses <jpmeneses@uc.cl>
2022-08-31 20:13:26 -0400

    Submit Unsup-v005: Only-FM estimation with uncertainty

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v004-unsup
-#SBATCH --output=out_unsup_004.txt
+#SBATCH --job-name=v005-unsup
+#SBATCH --output=out_unsup_005.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-IDEAL-unsup.py --dataset 'Unsup-004' --n_G_filters 64 --epochs 25 --epoch_decay 25 --epoch_ckpt 5 --lr 0.0001 --beta_1 0.9 --beta_2 0.999
+python train-IDEAL-unsup.py --dataset 'Unsup-005' --UQ True --n_G_filters 64 --epochs 40 --epoch_decay 40 --epoch_ckpt 5 --lr 0.0001 --beta_1 0.9 --beta_2 0.999
