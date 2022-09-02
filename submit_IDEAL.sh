2189a6eddf7119221cd0f66a27ea32a769a29a13
jpmeneses <jpmeneses@uc.cl>
2022-09-02 00:11:16 -0400

    Re-submit Unsup-v005: Activate uncertainty awareness (UQ = True)

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -7,4 +7,4 @@
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-IDEAL-unsup.py --dataset 'Unsup-005' --n_G_filters 64 --epochs 200 --epoch_decay 200 --epoch_ckpt 25 --lr 0.0001 --beta_1 0.9 --beta_2 0.999 --std_log_weight 0.01
+python train-IDEAL-unsup.py --dataset 'Unsup-005' --UQ True --n_G_filters 32 --epochs 40 --epoch_decay 40 --epoch_ckpt 5 --lr 0.0001 --beta_1 0.9 --beta_2 0.999 --std_log_weight 0.01
