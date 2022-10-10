5735a736ecc35135c942de9d5c5c3a8716b51c2b
jpmeneses <jpmeneses@uc.cl>
2022-10-10 19:23:05 -0300

    Submit Unsup-v007: R2_log with lower limit & no R2_L1 weighting

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v006-unsup
-#SBATCH --output=out_unsup_006.txt
+#SBATCH --job-name=v007-unsup
+#SBATCH --output=out_unsup_007.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-IDEAL-unsup.py --dataset 'Unsup-006' --R2_log True --epochs 40 --epoch_decay 40 --epoch_ckpt 5 --lr 0.0001 --beta_1 0.9 --beta_2 0.999
+python train-IDEAL-unsup.py --dataset 'Unsup-007' --R2_log True --epochs 25 --epoch_decay 25 --epoch_ckpt 5 --lr 0.0001 --beta_1 0.9 --beta_2 0.999
