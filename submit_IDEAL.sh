cf8b452552dbcf13976a61b82636989afafb13e6
jpmeneses <jpmeneses@uc.cl>
2022-10-11 12:46:25 -0300

    Submit Unsup-v008: IDEAL model (no fat char) and no R2*

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v007-unsup
-#SBATCH --output=out_unsup_007.txt
+#SBATCH --job-name=v008-unsup
+#SBATCH --output=out_unsup_008.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-IDEAL-unsup.py --dataset 'Unsup-007' --fat_char True --epochs 30 --epoch_decay 30 --epoch_ckpt 5 --lr 0.0001 --beta_1 0.9 --beta_2 0.999
+python train-IDEAL-unsup.py --dataset 'Unsup-008' --epochs 30 --epoch_decay 30 --epoch_ckpt 5 --lr 0.0001 --beta_1 0.9 --beta_2 0.999
