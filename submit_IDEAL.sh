7017b1285a79a118aa7e0ecd3f5d04ba935bc87a
jpmeneses <jpmeneses@uc.cl>
2022-10-10 22:43:38 -0300

    Re-submit Unsup-v007: R"_lig with R2_L1 weighting

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -7,4 +7,4 @@
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-IDEAL-unsup.py --dataset 'Unsup-007' --R2_log True --epochs 25 --epoch_decay 25 --epoch_ckpt 5 --lr 0.0001 --beta_1 0.9 --beta_2 0.999
+python train-IDEAL-unsup.py --dataset 'Unsup-007' --R2_log True --epochs 25 --epoch_decay 25 --epoch_ckpt 5 --lr 0.0001 --beta_1 0.9 --beta_2 0.999 --R2_L1_weight 0.1
