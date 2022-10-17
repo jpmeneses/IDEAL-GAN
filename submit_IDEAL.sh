6ba00685c8514fa869a8e67a1ef853768573e307
jpmeneses <jpmeneses@uc.cl>
2022-10-17 20:03:07 -0300

    Submit Sup-v101: 3-echoes MDWF-Net to get training/validation curves

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v100-sup
-#SBATCH --output=out_sup_100.txt
+#SBATCH --job-name=v101-sup
+#SBATCH --output=out_sup_101.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-sup.py --dataset 'Sup-100' --out_vars 'WF-PM' --epochs 120 --epoch_decay 100 --epoch_ckpt 20 --lr 0.0005
+python train-sup.py --dataset 'Sup-101' --n_echoes 3 --out_vars 'WF-PM' --epochs 120 --epoch_decay 100 --epoch_ckpt 20 --lr 0.0005
