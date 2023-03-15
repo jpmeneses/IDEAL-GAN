f926775b581ef184975c5dde49a5462a02000bed
jpmeneses <jpmeneses@uc.cl>
2023-03-15 09:40:26 -0300

    Specify length and dimensions of training dataset

--- train-IDEAL-unsup.py
+++ train-IDEAL-unsup.py
@@ -151,7 +151,8 @@ A_B_dataset_val.batch(1)
 # =                                   models                                   =
 # ==============================================================================
 
-len_dataset = total_data - len(valX)
+len_val,hgt,wdt,d_ech = np.shape(valX)
+len_dataset = total_data - len_val
 total_steps = np.ceil(len_dataset/args.batch_size)*args.epochs
 
 # with mirrored_strategy.scope():
