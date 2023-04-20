531bf3d21e8ab51f84a6db7400f8512aed80f3b5
jpmeneses <jpmeneses@uc.cl>
2023-04-20 12:08:16 +1000

    Submit MEBCRN (to run in GPUs)

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v011-test
-#SBATCH --output=out_test_011.txt
+#SBATCH --job-name=v011-TEaug
+#SBATCH --output=out_TEaug_011.txt
+#SBATCH --partition=gpus
+#SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-IDEAL-TEaug.py --dataset 'test-011' --G_model 'MEBCRN' --out_vars 'WFc' --te_input False --n_G_filters 64 --epoch_decay 200 --epoch_ckpt 50 --beta_1 0.9 --beta_2 0.999
+python train-IDEAL-TEaug.py --dataset 'TEaug-011' --G_model 'MEBCRN' --out_vars 'WFc' --te_input False --n_G_filters 64 --epoch_decay 200 --epoch_ckpt 50 --beta_1 0.9 --beta_2 0.999
