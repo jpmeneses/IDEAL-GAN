6748fee630238a9b5ac4b9373a5f2d66ffe8711c
jpmeneses <jpmeneses@uc.cl>
2023-04-27 14:24:57 +1000

    Re-submit MEBCRN with modified architectures (echoes' reshaping removed)

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v102-sup
-#SBATCH --output=out_sup_102.txt
+#SBATCH --job-name=v103-sup
+#SBATCH --output=out_sup_103.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-sup.py --dataset 'Sup-102' --out_vars 'WFc' --G_model 'MEBCRN' --n_G_filters 64 --epoch_decay 200 --epoch_ckpt 50 --beta_1 0.9 --beta_2 0.999
+python train-sup.py --dataset 'Sup-103' --out_vars 'WFc' --G_model 'MEBCRN' --n_G_filters 64 --epoch_decay 200 --epoch_ckpt 50 --beta_1 0.9 --beta_2 0.999
