4f9658607fd9b82e202013b873907289e98fa4fa
jpmeneses <jpmeneses@uc.cl>
2023-04-29 11:23:10 +1000

    Train MEBCRN for another 100 epochs

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -7,4 +7,4 @@
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-sup.py --dataset 'Sup-103' --out_vars 'WFc' --G_model 'MEBCRN' --n_G_filters 64 --epoch_decay 200 --epoch_ckpt 50 --beta_1 0.9 --beta_2 0.999
+python train-sup.py --dataset 'Sup-103' --out_vars 'WFc' --G_model 'MEBCRN' --n_G_filters 64 --epochs 200 --epoch_decay 200 --epoch_ckpt 50 --beta_1 0.9 --beta_2 0.999
