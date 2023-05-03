4e1f3b43764a19abcf17aac81992d70c1e1f2d3a
jpmeneses <jpmeneses@uc.cl>
2023-05-03 10:06:36 +1000

    Submit IDEAL-GAN (second test): Increase in discriminative loss weight

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v001-GAN
-#SBATCH --output=out_GAN_001.txt
+#SBATCH --job-name=v002-GAN
+#SBATCH --output=out_GAN_002.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-IDEAL-GAN.py --dataset 'GAN-001' --epochs 50 --epoch_decay 50 --epoch_ckpt 5
+python train-IDEAL-GAN.py --dataset 'GAN-002' --epochs 50 --epoch_decay 50 --epoch_ckpt 5 --cycle_loss_weight 1.0
