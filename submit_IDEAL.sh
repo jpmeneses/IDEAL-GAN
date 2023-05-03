39e4b8f224c9df240b9c7f2f1356d4ef97363bd8
jpmeneses <jpmeneses@uc.cl>
2023-05-03 11:44:37 +1000

    Submit TEaug-013: Classic U-Net (no TE-input)

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v002-GAN
-#SBATCH --output=out_GAN_002.txt
+#SBATCH --job-name=v013-TEaug
+#SBATCH --output=out_TEaug_013.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-IDEAL-GAN.py --dataset 'GAN-002' --epochs 50 --epoch_decay 50 --epoch_ckpt 5 --cycle_loss_weight 1.0
+python train-IDEAL-TEaug.py --dataset 'TEaug-013' --G_model 'U-Net' --te_input False --epochs 200 --epoch_decay 200 --epoch_ckpt 50 --beta_1 0.9 --beta_2 0.999
