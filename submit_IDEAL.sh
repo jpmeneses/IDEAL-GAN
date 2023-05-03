3441092c05953f276bf19f4ae8b7d3133cde4def
jpmeneses <jpmeneses@uc.cl>
2023-05-03 11:52:11 +1000

    Run VET-Net (with corrected Ada-IN layers) in testing node

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-#SBATCH --job-name=v013-TEaug
-#SBATCH --output=out_TEaug_013.txt
-#SBATCH --partition=gpus
-#SBATCH --gres=gpu:quadro_rtx_8000:1
+#SBATCH --job-name=v014-TEaug
+#SBATCH --output=out_TEaug_014.txt
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-IDEAL-TEaug.py --dataset 'TEaug-013' --G_model 'U-Net' --te_input False --epochs 200 --epoch_decay 200 --epoch_ckpt 50 --beta_1 0.9 --beta_2 0.999
+python train-IDEAL-TEaug.py --dataset 'TEaug-014' --out_vars 'PM' --epochs 200 --epoch_decay 200 --epoch_ckpt 50 --beta_1 0.9 --beta_2 0.999
