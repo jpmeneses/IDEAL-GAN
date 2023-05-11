524941521da6601448b3740c66d936d3c2f2ca0d
jpmeneses <jpmeneses@uc.cl>
2023-05-11 16:28:06 +1000

    Submit IDEAL-GAN: Increased discriminative loss and G num. of filters. Self-attention was also incorporated

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v014-TEaug
-#SBATCH --output=out_TEaug_014.txt
+#SBATCH --job-name=v003-GAN
+#SBATCH --output=out_GAN_003.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-IDEAL-TEaug.py --dataset 'TEaug-014' --out_vars 'PM' --epochs 200 --epoch_decay 200 --epoch_ckpt 50 --beta_1 0.9 --beta_2 0.999
+python train-IDEAL-GAN.py --dataset 'TEaug-003' --n_G_filters 48 --epochs 100 --cycle_loss_weight 0.5 --D1_SelfAttention True
