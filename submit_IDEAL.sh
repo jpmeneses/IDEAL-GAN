1531cd214c90e7176f6d87fb5a84d3ac00de30dd
jpmeneses <jpmeneses@uc.cl>
2023-05-16 21:28:33 +1000

    Submit IDEAL-GAN(v005): Regularized latent space without Dense layers

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v004-GAN
-#SBATCH --output=out_GAN_004.txt
+#SBATCH --job-name=v005-GAN
+#SBATCH --output=out_GAN_005.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-IDEAL-GAN.py --dataset 'GAN-004' --n_G_filters 48 --encoded_size 8192 --epochs 100 --cycle_loss_weight 0.5 --D1_SelfAttention True
+python train-IDEAL-GAN.py --dataset 'GAN-005' --n_G_filters 48 --encoded_size 8192 --epochs 100 --cycle_loss_weight 0.5 --D1_SelfAttention True
