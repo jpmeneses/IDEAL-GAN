0a96827a560f77871cdeb0dceb86b5394ceac9e8
jpmeneses <jpmeneses@uc.cl>
2023-05-16 17:00:43 +1000

    Submit IDEAL-GAN(v004): Regularized latent space (VAE)

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v003-GAN
-#SBATCH --output=out_GAN_003.txt
+#SBATCH --job-name=v004-GAN
+#SBATCH --output=out_GAN_004.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-IDEAL-GAN.py --dataset 'GAN-003' --n_G_filters 48 --epochs 100 --cycle_loss_weight 0.5 --D1_SelfAttention True
+python train-IDEAL-GAN.py --dataset 'GAN-004' --n_G_filters 48 --epochs 100 --cycle_loss_weight 0.5 --D1_SelfAttention True
