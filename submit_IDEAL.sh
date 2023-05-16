f5b3f417b8c030943df2308f4c0b4bd4bede59b9
jpmeneses <jpmeneses@uc.cl>
2023-05-16 17:19:09 +1000

    Re-submit IDEAL-GAN(v004): Regularized latent space (VAE) with specified size (1024)

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -7,4 +7,4 @@
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-IDEAL-GAN.py --dataset 'GAN-004' --n_G_filters 48 --epochs 100 --cycle_loss_weight 0.5 --D1_SelfAttention True
+python train-IDEAL-GAN.py --dataset 'GAN-004' --n_G_filters 48 --encoded_size 1024 --epochs 100 --cycle_loss_weight 0.5 --D1_SelfAttention True
