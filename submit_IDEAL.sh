482d59b6afdb11d903bca14e4b730f11724bfd83
jpmeneses <jpmeneses@uc.cl>
2023-05-17 20:19:43 +1000

    Submit IDEAL-GAN(v006): Regularized latent space with corrected prior distribution (zero mean)

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v005-GAN
-#SBATCH --output=out_GAN_005.txt
+#SBATCH --job-name=v006-GAN
+#SBATCH --output=out_GAN_006.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-IDEAL-GAN.py --dataset 'GAN-005' --n_G_filters 48 --epochs 100 --cycle_loss_weight 0.5 --D1_SelfAttention True
+python train-IDEAL-GAN.py --dataset 'GAN-006' --n_G_filters 48 --epochs 100 --cycle_loss_weight 0.5 --D1_SelfAttention True
