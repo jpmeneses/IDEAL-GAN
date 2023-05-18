6561e37518665aa5f01c609de3729116ee71066b
jpmeneses <jpmeneses@uc.cl>
2023-05-18 13:27:53 +1000

    Submit IDEAL-GAN(v007): Reduce latent space size and base number of filters

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v006-GAN
-#SBATCH --output=out_GAN_006.txt
+#SBATCH --job-name=v007-GAN
+#SBATCH --output=out_GAN_007.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-IDEAL-GAN.py --dataset 'GAN-006' --n_G_filters 48 --epochs 100 --cycle_loss_weight 0.5 --D1_SelfAttention True
+python train-IDEAL-GAN.py --dataset 'GAN-007' --n_G_filters 24 --encoded_size 256 --epochs 100 --cycle_loss_weight 0.5 --D1_SelfAttention True
