0dbdb6c1c7b04912bf258fa7830da6b1d1a53fc0
jpmeneses <jpmeneses@uc.cl>
2023-05-18 15:03:52 +1000

    Modify requested GPU

--- m3_submit.sh
+++ m3_submit.sh
@@ -5,7 +5,7 @@
 #SBATCH --time=01:00:00
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
-#SBATCH --gres=gpu:1
+#SBATCH --gres=gpu:P100:1
 #SBATCH --partition=m3h
 
 python train-IDEAL-unsup.py --dataset 'Unsup-000' --UQ True --epochs 30 --epochs_decay 30
