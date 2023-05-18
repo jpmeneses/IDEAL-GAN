ac58bc4fbfab6dcabc0457e609b91f4e684b3c3e
jpmeneses <jpmeneses@uc.cl>
2023-05-18 15:00:15 +1000

    Submit file for M3 Massive cluster

---
+++ m3_submit.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#SBATCH --job-name=v000-Unsup
+#SBATCH --account=jmeneses
+#SBATCH --time=01:00:00
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --gres=gpu:1
+#SBATCH --partition=m3h
+
+python train-IDEAL-unsup.py --dataset 'Unsup-000' --UQ True --epochs 30 --epochs_decay 30
