f7a05cf69f19c61dc179f1f14315c7cb5f3f8a23
jpmeneses <jpmeneses@uc.cl>
2023-05-22 12:21:55 +1000

    Submit IDEAL-GAN(v010): Keep latent space size and train with multi-decoder architecture

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v009-GAN
-#SBATCH --output=out_GAN_009.txt
+#SBATCH --job-name=v010-GAN
+#SBATCH --output=out_GAN_010.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-IDEAL-GAN.py --dataset 'GAN-009' --n_G_filters 24 --encoded_size 4096 --epochs 150 --epoch_decay 150 --epoch_ckpt 25 --B2A2B_weight 0.5 --ls_reg_weight 10.0 --D1_SelfAttention True
+python train-IDEAL-GAN.py --dataset 'GAN-010' --n_G_filters 24 --encoded_size 4096 --epochs 100 --epoch_ckpt 20 --B2A2B_weight 0.5 --ls_reg_weight 10.0 --D1_SelfAttention True
