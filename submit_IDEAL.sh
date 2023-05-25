460823dc2fd9bef6bae6c1495a00123e7ed0ec88
jpmeneses <jpmeneses@uc.cl>
2023-05-25 13:55:15 +1000

    Submit IDEAL-GAN(v012): No masking, No-critic, no self-attention and increased latent-space regularization

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v011-GAN
+#SBATCH --job-name=v012-GAN
 #SBATCH --output=out_GAN_011.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-IDEAL-GAN.py --dataset 'GAN-011' --n_G_filters 24 --encoded_size 4096 --epochs 100 --epoch_ckpt 20 --B2A2B_weight 0.5 --ls_reg_weight 20.0 --D1_SelfAttention False
+python train-IDEAL-GAN.py --dataset 'GAN-012' --n_G_filters 24 --encoded_size 4096 --epochs 100 --epoch_ckpt 20 --B2A2B_weight 0.5 --ls_reg_weight 100.0 --D1_SelfAttention False
