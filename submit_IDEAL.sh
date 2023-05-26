21c76bd7993db3baf428607a377b6ad698ebbdb1
jpmeneses <jpmeneses@uc.cl>
2023-05-26 14:48:54 +1000

    Submit VAE-GAN(v001): Reduced latent space regularization - weight=0.1

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v000-GAN
-#SBATCH --output=out_GAN_000.txt
+#SBATCH --job-name=v001-GAN
+#SBATCH --output=out_GAN_001.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-IDEAL-GAN.py --dataset 'GAN-000' --n_G_filters 24 --encoded_size 4096 --epochs 100 --epoch_ckpt 20 --B2A2B_weight 0.5 --D1_SelfAttention False
+python train-IDEAL-GAN.py --dataset 'GAN-001' --n_G_filters 24 --encoded_size 4096 --epochs 100 --epoch_ckpt 20 --B2A2B_weight 0.5 --ls_reg_weight 0.1 --D1_SelfAttention False
