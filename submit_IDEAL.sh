6d063b188f8c8180118b9775390d1e2a30b14526
jpmeneses <jpmeneses@uc.cl>
2023-05-27 17:27:40 +1000

    Submit VAE-GAN(v005): Set latent space regularization weighting to 1e-7

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v004-GAN
-#SBATCH --output=out_GAN_004.txt
+#SBATCH --job-name=v005-GAN
+#SBATCH --output=out_GAN_005.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-IDEAL-GAN.py --dataset 'GAN-004' --n_G_filters 24 --encoded_size 4096 --epochs 100 --epoch_ckpt 20 --B2A2B_weight 0.5 --ls_reg_weight 1e-8 --D1_SelfAttention False
+python train-IDEAL-GAN.py --dataset 'GAN-005' --n_G_filters 24 --encoded_size 4096 --epochs 100 --epoch_ckpt 20 --B2A2B_weight 0.5 --ls_reg_weight 1e-7 --D1_SelfAttention False
