2a28c2f8675369719529ec9f598b8331d3c4f847
jpmeneses <jpmeneses@uc.cl>
2023-05-29 15:23:49 +1000

    Debug: Remove critic loss from validation step

--- train-IDEAL-GAN.py
+++ train-IDEAL-GAN.py
@@ -312,14 +312,13 @@ def sample(A, B):
     # B2A2B = tf.where(B!=0.0,B2A2B,0.0)
 
     # Discriminative Losses
-    A2B2A_d_logits = D_A(A2B2A, training=True)
-    val_A2B2A_g_loss = g_loss_fn(A2B2A_d_logits)
+    # A2B2A_d_logits = D_A(A2B2A, training=True)
+    # val_A2B2A_g_loss = g_loss_fn(A2B2A_d_logits)
     
     # Validation losses
     val_A2B2A_loss = tf.abs(cycle_loss_fn(A, A2B2A))
     val_B2A2B_loss = cycle_loss_fn(B, B2A2B)
-    return A2B, B2A, A2B2A, B2A2B, {'A2B2A_g_loss': val_A2B2A_g_loss,
-                                    'A2B2A_cycle_loss': val_A2B2A_loss,
+    return A2B, B2A, A2B2A, B2A2B, {'A2B2A_cycle_loss': val_A2B2A_loss,
                                     'B2A2B_cycle_loss': val_B2A2B_loss}
 
 def validation_step(A, B):
