b0f4b3e87044b87f4cad6617bdcb9e59b094bbf1
jpmeneses <jpmeneses@uc.cl>
2023-05-29 11:00:05 +1000

    Submit VAE-GAN(v007): Add self-attention block to decoder architecture

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=t006-GAN
-#SBATCH --output=test_GAN_006.txt
+#SBATCH --job-name=v007-GAN
+#SBATCH --output=out_GAN_007.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python test-genData.py --experiment_dir 'output/GAN-006'
+python train-IDEAL-GAN.py --dataset 'GAN-007' --n_G_filters 24 --encoded_size 9216 --epochs 100 --epoch_ckpt 20 --B2A2B_weight 0.5 --ls_reg_weight 1e-7 --D1_SelfAttention True
