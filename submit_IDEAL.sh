e87338a61cca84b54a3b3a2d7ee63a2780992616
jpmeneses <jpmeneses@uc.cl>
2023-05-29 14:57:32 +1000

    Submit VAE-GAN(v009): First trial with MONAI architectures (MSE loss, SelfAttention, no adversarial training)

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -7,4 +7,4 @@
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-IDEAL-GAN.py --dataset 'GAN-009' --n_G_filters 24 --encoded_size 9216 --epochs 100 --epoch_ckpt 20 --B2A2B_weight 0.5 --ls_reg_weight 1e-7 --D1_SelfAttention False
+python train-IDEAL-GAN.py --dataset 'GAN-009' --n_G_filters 24 --encoded_size 128 --epochs 100 --epoch_ckpt 20 --B2A2B_weight 0.5 --ls_reg_weight 1e-7 --NL_SelfAttention True
