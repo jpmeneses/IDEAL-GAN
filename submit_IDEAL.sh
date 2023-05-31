8bbd0bae02c5f2fc08ee51255c071a87373ab6f0
jpmeneses <jpmeneses@uc.cl>
2023-05-31 14:24:01 +1000

    Submit VAE-GAN(v014): Encoder with two separate Conv2D layers to compute mean and variance (LS-dims=128, strong KL reg, NL-SA)

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v013-GAN
-#SBATCH --output=out_GAN_013.txt
+#SBATCH --job-name=v014-GAN
+#SBATCH --output=out_GAN_014.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-IDEAL-GAN.py --dataset 'GAN-013' --n_G_filters 36 --encoded_size 64 --epochs 100 --epoch_ckpt 20 --B2A2B_weight 0.5 --ls_reg_weight 1e-7 --NL_SelfAttention False
+python train-IDEAL-GAN.py --dataset 'GAN-014' --n_G_filters 36 --encoded_size 128 --epochs 100 --epoch_ckpt 20 --B2A2B_weight 0.5 --ls_reg_weight 1e-7 --NL_SelfAttention True
