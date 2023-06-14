d6c4276e5fd07c63ce025062d88af6be9bc8f71d
jpmeneses <jpmeneses@uc.cl>
2023-06-14 15:05:55 +1000

    Test direct LS reshaping at encoder's last layer

--- flatten-test.py
+++ flatten-test.py
@@ -10,7 +10,7 @@ def encoder(input_shape):
 	x = tf.keras.layers.concatenate([x,std],axis=-1)
 	# x = tf.keras.layers.Lambda(lambda x: tf.stack([x,tf.ones_like(x)*1e-7],2))(x)
 	# x = tf.keras.layers.Reshape(target_shape=(2*encoded_size,))(x)
-	x = tfp.layers.IndependentNormal(encoded_size)(x)
+	x = tfp.layers.IndependentNormal(input_shape)(x)
 	return tf.keras.Model(inputs=inputs,outputs=x)
 
 hgt,wdt,n_ch = 24,24,1
@@ -26,12 +26,12 @@ dec = decoder((hgt*wdt*n_ch))
 
 G_A2B = tf.keras.Sequential()
 G_A2B.add(enc)
-G_A2B.add(dec)
+#G_A2B.add(dec)
 
-A_sc = 2e1
+A_sc = 1e1
 A_zero_horiz = tf.zeros((1,hgt//3,wdt,1),dtype=tf.float32)
 A_zero = tf.zeros((1,hgt//3,wdt//3,1),dtype=tf.float32)
-A_one = tf.ones((1,hgt//3,wdt//3,1),dtype=tf.float32)*A_sc/2
+A_one = tf.ones((1,hgt//3,wdt//3,1),dtype=tf.float32)*3*A_sc/4
 A_aux = tf.concat([A_zero,A_one,A_zero],axis=2)
 A = tf.concat([A_zero_horiz,A_aux,A_zero_horiz],axis=1)
 B = G_A2B(A)
