f5d2dd11d179deeb541dce8a33f168f4a79a1c7f
jpmeneses <jpmeneses@uc.cl>
2023-06-14 11:56:52 +1000

    Re-submit VAE-GAN(v019): Increase data consistency loss weighting

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -7,4 +7,4 @@
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-IDEAL-GAN.py --dataset 'GAN-019' --n_G_filters 36 --encoded_size 128 --epochs 100 --epoch_ckpt 20 --B2A2B_weight 0.5 --ls_reg_weight 1e-7 --NL_SelfAttention True
+python train-IDEAL-GAN.py --dataset 'GAN-019' --n_G_filters 36 --encoded_size 128 --epochs 100 --epoch_ckpt 20 --cycle_loss_weight 1e2 --B2A2B_weight 0.5 --ls_reg_weight 1e-6 --NL_SelfAttention True
