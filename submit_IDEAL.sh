bf0a807de0974af19f3d03867f7e77b9777a75e1
jpmeneses <jpmeneses@uc.cl>
2023-06-16 19:13:15 +1000

    Re-submit VAE-GAN(v020): Train for 40 more epochs

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v019-GAN
-#SBATCH --output=out_GAN_019.txt
+#SBATCH --job-name=v020-GAN
+#SBATCH --output=out_GAN_020.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-IDEAL-GAN.py --dataset 'GAN-019' --n_G_filters 36 --encoded_size 128 --epochs 140 --epoch_decay 140 --epoch_ckpt 20 --cycle_loss_weight 1e2 --ls_reg_weight 1e-6 --NL_SelfAttention True
+python train-IDEAL-GAN.py --dataset 'GAN-020' --n_G_filters 36 --encoded_size 128 --epochs 140 --epoch_decay 140 --epoch_ckpt 20 --cycle_loss_weight 1e1 --ls_reg_weight 1e-6 --NL_SelfAttention True
