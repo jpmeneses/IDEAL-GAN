d1257b0995f4c4de2915385459849af4e2774990
jpmeneses <jpmeneses@uc.cl>
2023-06-17 20:23:40 +1000

    Submit VAE-GAN(v022): Increase number of LS features to 256

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v021-GAN
-#SBATCH --output=out_GAN_021.txt
+#SBATCH --job-name=v022-GAN
+#SBATCH --output=out_GAN_022.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-IDEAL-GAN.py --dataset 'GAN-021' --n_G_filters 36 --encoded_size 128 --epochs 200 --epoch_decay 200 --epoch_ckpt 20 --cycle_loss_weight 1e0 --ls_reg_weight 1e-6 --NL_SelfAttention True
+python train-IDEAL-GAN.py --dataset 'GAN-022' --n_G_filters 36 --encoded_size 256 --epochs 140 --epoch_decay 140 --epoch_ckpt 20 --cycle_loss_weight 1e0 --ls_reg_weight 1e-6 --NL_SelfAttention True
