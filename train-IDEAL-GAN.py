e33d672f70c811060526cec58e18c50c687fd0e0
jpmeneses <jpmeneses@uc.cl>
2023-06-19 18:44:40 +1000

    Increase again critic optimizer's gradient clip value

--- train-IDEAL-GAN.py
+++ train-IDEAL-GAN.py
@@ -149,7 +149,7 @@ cycle_loss_fn = tf.losses.MeanSquaredError()
 G_lr_scheduler = dl.LinearDecay(args.lr, total_steps, args.epoch_decay * total_steps / args.epochs)
 D_lr_scheduler = dl.LinearDecay(4*args.lr, 5 * total_steps, 5 * args.epoch_decay * total_steps / args.epochs)
 G_optimizer = keras.optimizers.Adam(learning_rate=G_lr_scheduler, beta_1=args.beta_1, beta_2=args.beta_2)
-D_optimizer = keras.optimizers.Adam(learning_rate=D_lr_scheduler, beta_1=args.beta_1, beta_2=args.beta_2, clipvalue=1e5)
+D_optimizer = keras.optimizers.Adam(learning_rate=D_lr_scheduler, beta_1=args.beta_1, beta_2=args.beta_2, clipvalue=1e10)
 
 # ==============================================================================
 # =                                 train step                                 =
