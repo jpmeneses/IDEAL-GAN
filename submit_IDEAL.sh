0c993675c612dc73bd33a0a20b7a164c71bee3fa
jpmeneses <jpmeneses@uc.cl>
2023-06-22 16:54:42 +1000

    Re-submit VAE-GAN(v023): Add train_D step, increase critic loss weight and train for 5 more epochs

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -7,4 +7,4 @@
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-IDEAL-GAN.py --dataset 'GAN-023' --n_G_filters 36 --encoded_size 128 --epochs 5 --epoch_ckpt 1 --cycle_loss_weight 1e2 --ls_reg_weight 1e-4 --NL_SelfAttention True
+python train-IDEAL-GAN.py --dataset 'GAN-023' --n_G_filters 36 --encoded_size 128 --epochs 10 --epoch_ckpt 1 --cycle_loss_weight 1e1 --ls_reg_weight 1e-5 --NL_SelfAttention True
