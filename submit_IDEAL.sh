4418722c1a57cfb31db434175d18297f06a2d35c
jpmeneses <jpmeneses@uc.cl>
2023-06-27 12:12:52 +1000

    Re-submit VAE-GAN(v023): Try adversarial training with manually estimated gradients for IDEAL-model

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v024-GAN
-#SBATCH --output=out_GAN_024.txt
+#SBATCH --job-name=v023-GAN
+#SBATCH --output=out_GAN_023.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-IDEAL-GAN.py --dataset 'GAN-024' --n_G_filters 36 --encoded_size 128 --epochs 100 --epoch_ckpt 20 --cycle_loss_weight 1e0 --ls_reg_weight 5e-7 --NL_SelfAttention True
+python train-IDEAL-GAN.py --dataset 'GAN-023' --n_G_filters 36 --encoded_size 128 --epochs 10 --epoch_ckpt 1 --cycle_loss_weight 1e1 --ls_reg_weight 1e-5 --NL_SelfAttention True
