421aec5096c661805b20fa652df288c865885dfc
jpmeneses <jpmeneses@uc.cl>
2023-07-05 21:03:39 +1000

    Submit VAE-GAN(v025): Train adversarialy with customized gradient

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v023-GAN
-#SBATCH --output=out_GAN_023.txt
+#SBATCH --job-name=v025-GAN
+#SBATCH --output=out_GAN_025.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-IDEAL-GAN.py --dataset 'GAN-023' --adv_train False --n_G_filters 36 --encoded_size 128 --epochs 10 --epoch_ckpt 1 --cycle_loss_weight 5e1 --ls_reg_weight 5e-5 --NL_SelfAttention True
+python train-IDEAL-GAN.py --dataset 'GAN-025' --adv_train True --n_G_filters 36 --encoded_size 128 --epochs 10 --epoch_ckpt 1 --cycle_loss_weight 5e1 --ls_reg_weight 5e-5 --NL_SelfAttention True
