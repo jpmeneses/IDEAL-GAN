ae2cc70995a5bed43e1d7bebabad9a2b76c9b601
jpmeneses <jpmeneses@uc.cl>
2023-07-06 14:51:07 +1000

    Re-submit VAE-GAN(v025): Decrease critic-loss weighting

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -7,4 +7,4 @@
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-IDEAL-GAN.py --dataset 'GAN-025' --adv_train True --n_G_filters 36 --encoded_size 128 --epochs 10 --epoch_ckpt 1 --cycle_loss_weight 5e1 --ls_reg_weight 5e-5 --NL_SelfAttention True
+python train-IDEAL-GAN.py --dataset 'GAN-025' --adv_train True --n_G_filters 36 --encoded_size 128 --epochs 10 --epoch_ckpt 1 --cycle_loss_weight 1e3 --ls_reg_weight 1e-3 --NL_SelfAttention True
