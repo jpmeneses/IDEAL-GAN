3dff9a4c60f35bd8201fa1f8764fee777aa82b64
jpmeneses <jpmeneses@uc.cl>
2023-07-07 14:37:47 +1000

    Submit Sup-011: Train MDWF-Net for 100 epochs

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v010-sup
-#SBATCH --output=out_sup_010.txt
+#SBATCH --job-name=v011-sup
+#SBATCH --output=out_sup_011.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-sup.py --dataset 'Sup-010' --out_vars 'PM' --batch_size 1 --epochs 100 --epoch_ckpt 20 --lr 0.0001
+python train-sup.py --dataset 'Sup-011' --out_vars 'WF-PM' --batch_size 1 --epoch_ckpt 20 --lr 0.0001 --D2_SelfAttention False
