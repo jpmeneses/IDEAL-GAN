10084aae20fd50e0179e245ecaef97d72ffa3fe6
jpmeneses <jpmeneses@uc.cl>
2023-07-07 14:35:51 +1000

    Submit Sup-010: Train NLV-Net for 100 epochs

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v023-GAN
-#SBATCH --output=out_GAN_023.txt
+#SBATCH --job-name=v010-sup
+#SBATCH --output=out_sup_010.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-IDEAL-GAN.py --dataset 'GAN-023' --adv_train False --n_G_filters 36 --encoded_size 128 --epochs 100 --epoch_ckpt 20 --cycle_loss_weight 5e1 --ls_reg_weight 5e-5 --NL_SelfAttention True
+python train-sup.py --dataset 'Sup-010' --out_vars 'PM' --batch_size 1 --epochs 100 --epoch_ckpt 20 --lr 0.0001
