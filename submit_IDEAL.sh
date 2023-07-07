0371d8b9e91d89737d631c7a22bb6e6e231a3252
jpmeneses <jpmeneses@uc.cl>
2023-07-08 08:46:16 +1000

    Re-submit VAE-GAN(v025): Continue training up to 50 epochs

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -7,4 +7,4 @@
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-IDEAL-GAN.py --dataset 'GAN-025' --adv_train True --n_G_filters 36 --encoded_size 128 --epochs 10 --epoch_ckpt 1 --cycle_loss_weight 1e1 --NL_SelfAttention True
+python train-IDEAL-GAN.py --dataset 'GAN-025' --adv_train True --n_G_filters 36 --encoded_size 128 --epochs 50 --epoch_ckpt 10 --cycle_loss_weight 1e1 --NL_SelfAttention True
