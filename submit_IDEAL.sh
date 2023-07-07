35ea87e4559eafe6fc013ba46ece22de45f225f7
jpmeneses <jpmeneses@uc.cl>
2023-07-07 17:51:31 +1000

    Re-submit VAE-GAN(v025): Adversarial regularization over latent-space instead of KL

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v010-sup
-#SBATCH --output=out_sup_010.txt
+#SBATCH --job-name=v025-GAN
+#SBATCH --output=out_GAN-025.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-sup.py --dataset 'Sup-010' --out_vars 'PM' --batch_size 1 --epoch_ckpt 20 --lr 0.0001
+python train-IDEAL-GAN.py --dataset 'GAN-025' --adv_train True --n_G_filters 36 --encoded_size 128 --epochs 10 --epoch_ckpt 1 --cycle_loss_weight 1e1 --NL_SelfAttention True
