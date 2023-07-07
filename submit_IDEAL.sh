0d15affc76b9856413292a9163b8d35269478d87
jpmeneses <jpmeneses@uc.cl>
2023-07-07 14:51:36 +1000

    Re-submit Sup-010: Debug NLV-Net training

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v012-sup
-#SBATCH --output=out_sup_012.txt
+#SBATCH --job-name=v010-sup
+#SBATCH --output=out_sup_010.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-sup.py --dataset 'Sup-012' --out_vars 'WF' --G_model 'U-Net' --batch_size 1 --epoch_ckpt 20 --lr 0.0001 --D1_SelfAttention False
+python train-sup.py --dataset 'Sup-010' --out_vars 'PM' --batch_size 1 --epoch_ckpt 20 --lr 0.0001
