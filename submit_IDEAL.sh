0be5d47e9f63b1ab7881fe9df43b2ab049b6d054
jpmeneses <jpmeneses@uc.cl>
2023-07-07 14:50:27 +1000

    Submit Sup-012: Train U-Net for 100 epochs

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v011-sup
-#SBATCH --output=out_sup_011.txt
+#SBATCH --job-name=v012-sup
+#SBATCH --output=out_sup_012.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-sup.py --dataset 'Sup-011' --out_vars 'WF-PM' --batch_size 1 --epoch_ckpt 20 --lr 0.0001 --D2_SelfAttention False
+python train-sup.py --dataset 'Sup-012' --out_vars 'WF' --G_model 'U-Net' --batch_size 1 --epoch_ckpt 20 --lr 0.0001 --D1_SelfAttention False
