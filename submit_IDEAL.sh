f61d0ccb898d29328edafdda737e8dd932601a31
jpmeneses <jpmeneses@uc.cl>
2023-07-11 10:54:54 +1000

    Submit VAE-GAN(v029): Decrease data-consistency loss weighting to 1e-1

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v028-GAN
-#SBATCH --output=out_GAN_028.txt
+#SBATCH --job-name=v029-GAN
+#SBATCH --output=out_GAN_029.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-IDEAL-GAN.py --dataset 'GAN-028' --adv_train True --n_G_filters 36 --encoded_size 128 --epochs 100 --epoch_ckpt 20 --cycle_loss_weight 1e0 --NL_SelfAttention True
+python train-IDEAL-GAN.py --dataset 'GAN-029' --adv_train True --n_G_filters 36 --encoded_size 128 --epochs 100 --epoch_ckpt 20 --cycle_loss_weight 1e-1 --NL_SelfAttention True
