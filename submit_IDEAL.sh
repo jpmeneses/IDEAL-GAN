7af9f56e2f5c5d2c84000cf1f7e30daaa5654b11
jpmeneses <jpmeneses@uc.cl>
2023-07-13 16:09:14 +1000

    Submit Sup-013: Train MEBCRN (and temporarily edit data format)

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v029-GAN
-#SBATCH --output=out_GAN_029.txt
+#SBATCH --job-name=v013-sup
+#SBATCH --output=out_sup_013.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-IDEAL-GAN.py --dataset 'GAN-029' --adv_train True --n_G_filters 36 --encoded_size 128 --epochs 100 --epoch_ckpt 20 --cycle_loss_weight 1e-1 --NL_SelfAttention True
+python train-sup.py --dataset 'Sup-013' --out_vars 'WF' --G_model 'MEBCRN' --batch_size 1 --epoch_ckpt 20 --lr 0.0001
