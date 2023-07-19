74321d81cc724528ca6e738c34fc7d7f768f5162
jpmeneses <jpmeneses@uc.cl>
2023-07-19 11:24:38 +1000

    Submit VAE-GAN(v030): Deterministic encoder output

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v013-sup
-#SBATCH --output=out_sup_013.txt
+#SBATCH --job-name=v030-sup
+#SBATCH --output=out_sup_030.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-sup.py --dataset 'Sup-013' --out_vars 'WF' --G_model 'MEBCRN' --batch_size 1 --epoch_ckpt 20 --lr 0.0001
+python train-IDEAL-GAN.py --dataset 'GAN-030' --adv_train True --n_G_filters 36 --encoded_size 128 --epochs 100 --epoch_ckpt 20 --cycle_loss_weight 2e-1 --NL_SelfAttention True
