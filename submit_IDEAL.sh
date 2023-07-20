7299f23dc525bef0f976fe1c1be246aa3396f1cd
jpmeneses <jpmeneses@uc.cl>
2023-07-20 13:31:37 +1000

    Re-submit VAE-GAN(v031): Remove deterministic LS regularization, keep A2B2A_L critic and add Fourier regularization (high frequency enhancement)

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -7,4 +7,4 @@
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-IDEAL-GAN.py --dataset 'GAN-031' --adv_train True --n_G_filters 36 --encoded_size 128 --epochs 100 --epoch_ckpt 20 --cycle_loss_weight 1e1 --ls_reg_weight 1e-5 --NL_SelfAttention True
+python train-IDEAL-GAN.py --dataset 'GAN-031' --adv_train True --n_G_filters 36 --encoded_size 128 --epochs 100 --epoch_ckpt 20 --cycle_loss_weight 1e1 --ls_reg_weight 1e-5 --Fourier_reg_weight 1e-3 --NL_SelfAttention True
