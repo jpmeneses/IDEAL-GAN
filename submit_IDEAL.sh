531e4cc508f2b60a23813722d2d56de29063f800
jpmeneses <jpmeneses@uc.cl>
2023-07-23 21:57:11 +1000

    Submit VAE-GAN(v039): Increase both weights (decrease adversarial loss weighting)

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v038-sup
-#SBATCH --output=out_sup_038.txt
+#SBATCH --job-name=v039-sup
+#SBATCH --output=out_sup_039.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-IDEAL-GAN.py --dataset 'GAN-038' --adv_train True --n_G_filters 36 --encoded_size 128 --epochs 100 --epoch_decay 100 --epoch_ckpt 20 --cycle_loss_weight 5e0 --ls_reg_weight 5e-6 --Fourier_reg_weight 0.0 --NL_SelfAttention True
+python train-IDEAL-GAN.py --dataset 'GAN-039' --adv_train True --n_G_filters 36 --encoded_size 128 --epochs 100 --epoch_decay 100 --epoch_ckpt 20 --cycle_loss_weight 2e1 --ls_reg_weight 2e-5 --Fourier_reg_weight 0.0 --NL_SelfAttention True
