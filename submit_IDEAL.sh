92550679400b557e334bf4bc7823af2b3a81cfd0
jpmeneses <jpmeneses@uc.cl>
2023-08-03 13:13:27 +1000

    Submit VAE-GAN(v100): No adversarial train and weak KL-reg, to then use it for LDM

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v046-sup
-#SBATCH --output=out_sup_046.txt
+#SBATCH --job-name=v100-sup
+#SBATCH --output=out_sup_100.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-IDEAL-GAN.py --dataset 'GAN-046' --adv_train True --n_G_filters 36 --encoded_size 128 --epochs 60 --epoch_decay 60 --epoch_ckpt 5 --D_lr_factor 4 --critic_train_steps 2 --cycle_loss_weight 1e1 --ls_reg_weight 1e-5 --Fourier_reg_weight 0.0 --NL_SelfAttention True
+python train-IDEAL-GAN.py --dataset 'GAN-100' --adv_train False --n_G_filters 36 --encoded_size 128 --epochs 50 --epoch_decay 50 --epoch_ckpt 10 --cycle_loss_weight 1e1 --ls_reg_weight 1e-6 --Fourier_reg_weight 0.0 --NL_SelfAttention True
