d303d20d776b20eb1a089bc226880e0c359f0ea7
jpmeneses <jpmeneses@uc.cl>
2023-08-03 21:22:07 +1000

    Change zero mean prior size for KL regularization

--- DLlib/module.py
+++ DLlib/module.py
@@ -607,7 +607,7 @@ def encoder(
     
     x = keras.layers.concatenate([x_mean,x_std],axis=-1)
     
-    prior = tfp.distributions.Independent(tfp.distributions.Normal(loc=tf.zeros((ls_hgt,ls_wdt,encoded_dims)), scale=1))
+    prior = tfp.distributions.Independent(tfp.distributions.Normal(loc=tf.zeros_like(x_mean), scale=1))
     output = tfp.layers.IndependentNormal([ls_hgt,ls_wdt,encoded_dims],
                 activity_regularizer=tfp.layers.KLDivergenceRegularizer(prior, weight=ls_reg_weight))(x)
 
