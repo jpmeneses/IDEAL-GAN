ba0d99c5dc188e058897b24a3a744ba1dd46d491
jpmeneses <jpmeneses@uc.cl>
2023-08-03 20:45:43 +1000

    Submit VAE-DDPM(v100): First trial, train for 100 epochs

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v100-sup
-#SBATCH --output=out_GAN_100.txt
+#SBATCH --job-name=v100-DDPM
+#SBATCH --output=out_DDPM_100.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-IDEAL-GAN.py --dataset 'GAN-100' --adv_train False --n_G_filters 36 --n_downsamplings 3 --encoded_size 64 --epochs 50 --epoch_decay 50 --epoch_ckpt 10 --cycle_loss_weight 1e1 --ls_reg_weight 1e-6 --Fourier_reg_weight 0.0 --NL_SelfAttention True
+python train-ldm.py --experiment_dir 'GAN-100' --epoch_ldm_ckpt 5 --data_augmentation True
