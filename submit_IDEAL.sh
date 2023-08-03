b7c6ab28ff137007f717fe0b47e02b3c8bce7419
jpmeneses <jpmeneses@uc.cl>
2023-08-03 13:22:25 +1000

    Change txt output filename

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #SBATCH --job-name=v100-sup
-#SBATCH --output=out_sup_100.txt
+#SBATCH --output=out_GAN_100.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
