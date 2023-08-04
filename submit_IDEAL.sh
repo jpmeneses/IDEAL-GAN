bdde9d0b85b5b94d77d8e6a5e106a9fc56c7f73c
jpmeneses <jpmeneses@uc.cl>
2023-08-04 23:59:57 +1000

    Submit VAE-DDPM(v102): Consider 1000 time-steps for DDPM with VAE stored at ckpt-50

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v103-GAN
-#SBATCH --output=out_GAN_103.txt
+#SBATCH --job-name=v102-DDPM
+#SBATCH --output=out_DDPM_102.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-IDEAL-GAN.py --dataset 'GAN-103' --adv_train False --n_G_filters 36 --n_downsamplings 3 --encoded_size 12 --epochs 100 --epoch_decay 100 --epoch_ckpt 10 --cycle_loss_weight 1e1 --ls_reg_weight 1e-6 --Fourier_reg_weight 0.0 --NL_SelfAttention True
+python train-ldm.py --experiment_dir 'GAN-102' --n_timesteps 1000 --epoch_ldm_ckpt 5 --data_augmentation False
