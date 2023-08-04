328eda1a0126bec5ac426a86e11583cfe1331904
jpmeneses <jpmeneses@uc.cl>
2023-08-04 17:42:24 +1000

    Edit: Change encoded-size to 6

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -7,4 +7,4 @@
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-IDEAL-GAN.py --dataset 'GAN-102' --adv_train False --n_G_filters 36 --n_downsamplings 3 --encoded_size 3 --epochs 100 --epoch_decay 100 --epoch_ckpt 10 --cycle_loss_weight 1e1 --ls_reg_weight 1e-6 --Fourier_reg_weight 0.0 --NL_SelfAttention True
+python train-IDEAL-GAN.py --dataset 'GAN-102' --adv_train False --n_G_filters 36 --n_downsamplings 3 --encoded_size 6 --epochs 100 --epoch_decay 100 --epoch_ckpt 10 --cycle_loss_weight 1e1 --ls_reg_weight 1e-6 --Fourier_reg_weight 0.0 --NL_SelfAttention True
