9f55eba47767132ff02fde1d758cbda79481e11d
jpmeneses <jpmeneses@uc.cl>
2023-08-04 10:28:39 +1000

    Submit VAE-DDPM(v101): Train more regularized VAE-LS for 100 epochs

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v101-GAN
-#SBATCH --output=out_GAN_101.txt
+#SBATCH --job-name=v101-DDPM
+#SBATCH --output=out_DDPM_101.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-IDEAL-GAN.py --dataset 'GAN-101' --adv_train False --n_G_filters 36 --n_downsamplings 3 --encoded_size 64 --epochs 100 --epoch_decay 100 --epoch_ckpt 10 --cycle_loss_weight 1e1 --ls_reg_weight 5e-6 --Fourier_reg_weight 0.0 --NL_SelfAttention True
+python train-ldm.py --experiment_dir 'GAN-101' --epoch_ldm_ckpt 5 --data_augmentation True
