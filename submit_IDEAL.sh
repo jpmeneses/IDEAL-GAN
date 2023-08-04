e3d6b4c9e5896e7e5bff2bdf5c7db5d0cbe88a04
jpmeneses <jpmeneses@uc.cl>
2023-08-04 17:38:39 +1000

    Submit VAE-GAN(v102): Significantly reduce encoded-dims (from 64 to 3, as done in Rombach et al)

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v046-GAN
-#SBATCH --output=out_GAN_046.txt
+#SBATCH --job-name=v102-GAN
+#SBATCH --output=out_GAN_102.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-IDEAL-GAN.py --dataset 'GAN-046' --adv_train True --n_G_filters 36 --encoded_size 128 --epochs 60 --epoch_decay 60 --epoch_ckpt 5 --D_lr_factor 4 --critic_train_steps 2 --cycle_loss_weight 1e1 --ls_reg_weight 1e-5 --Fourier_reg_weight 0.0 --NL_SelfAttention True
+python train-IDEAL-GAN.py --dataset 'GAN-102' --adv_train False --n_G_filters 36 --n_downsamplings 3 --encoded_size 3 --epochs 100 --epoch_decay 100 --epoch_ckpt 10 --cycle_loss_weight 1e1 --ls_reg_weight 1e-6 --Fourier_reg_weight 0.0 --NL_SelfAttention True
