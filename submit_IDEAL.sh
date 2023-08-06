194b100c8b78ec634b0ff439e8f5c8c4a867879a
jpmeneses <jpmeneses@uc.cl>
2023-08-06 17:59:34 +1000

    Submit VAE-GAN(v105): Further increase LS KL regularization

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v104-DDPM
-#SBATCH --output=out_DDPM_104.txt
+#SBATCH --job-name=v105-GAN
+#SBATCH --output=out_GAN_105.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-ldm.py --experiment_dir 'GAN-104' --n_timesteps 1000 --epoch_ldm_ckpt 5 --data_augmentation False
+python train-IDEAL-GAN.py --dataset 'GAN-105' --adv_train False --n_G_filters 36 --n_downsamplings 3 --encoded_size 6 --epochs 100 --epoch_decay 100 --epoch_ckpt 10 --cycle_loss_weight 1e1 --ls_reg_weight 1e-5 --Fourier_reg_weight 0.0 --NL_SelfAttention True
