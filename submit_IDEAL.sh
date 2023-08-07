adb6b1e51a7b4448c32505be22a9d0b0a1e6dea7
jpmeneses <jpmeneses@uc.cl>
2023-08-07 18:37:53 +1000

    Re-submit VAE-GAN(v104): Run with denoising U-Net with only three down-sampling levels

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v105-DDPM
-#SBATCH --output=out_DDPM_105.txt
+#SBATCH --job-name=v104-DDPM
+#SBATCH --output=out_DDPM_104.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-ldm.py --experiment_dir 'GAN-105' --n_timesteps 1000 --epoch_ldm_ckpt 5 --data_augmentation False
+python train-ldm.py --experiment_dir 'GAN-104' --n_timesteps 1000 --epoch_ldm_ckpt 5 --data_augmentation False
