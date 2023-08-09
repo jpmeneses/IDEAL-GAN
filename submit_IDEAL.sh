1fa238f99f27736af1e89b1863d90abc94913bf2
jpmeneses <jpmeneses@uc.cl>
2023-08-09 11:30:40 +1000

    Re-submit VAE-DDPM(v104): Re-train from LS normalized for unitary variance

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v104-GAN
-#SBATCH --output=out_GAN_104.txt
+#SBATCH --job-name=v105-GAN
+#SBATCH --output=out_GAN_105.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-ldm.py --experiment_dir 'GAN-104' --n_timesteps 1000 --epoch_ldm_ckpt 5 --data_augmentation False
+python train-ldm.py --experiment_dir 'GAN-105' --n_timesteps 1000 --epoch_ldm_ckpt 5 --data_augmentation False
