3f1e7abceb8284d41370003fe86b54828cb8d168
jpmeneses <jpmeneses@uc.cl>
2023-08-11 23:43:32 +1000

    Submit VAE-DDPM(v108): Start from VAE trained until 40 epochs

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v107-DDPM
-#SBATCH --output=out_DDPM_107.txt
+#SBATCH --job-name=v108-DDPM
+#SBATCH --output=out_DDPM_108.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-ldm.py --experiment_dir 'GAN-107' --n_timesteps 200 --batch_size 8 --epochs_ldm 1000 --epoch_ldm_ckpt 5 --data_augmentation True --lr 1e-4
+python train-ldm.py --experiment_dir 'GAN-108' --n_timesteps 200 --batch_size 8 --epochs_ldm 1000 --epoch_ldm_ckpt 5 --data_augmentation True --lr 1e-4
