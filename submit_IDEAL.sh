25db88f4a64c4dcb0f33eb65fc2b666f06a193fe
jpmeneses <jpmeneses@uc.cl>
2023-08-11 17:37:18 +1000

    Edit TXT filename

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-#SBATCH --job-name=v105-GAN
-#SBATCH --output=out_GAN_105.txt
+#SBATCH --job-name=v105-DDPM
+#SBATCH --output=out_DDPM_105.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
