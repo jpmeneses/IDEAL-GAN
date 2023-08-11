719301c95c14b7f286d3db1762e1aebdb4a07ab7
jpmeneses <jpmeneses@uc.cl>
2023-08-11 17:34:45 +1000

    Submit VAE-DDPM(v105): Run with 200 time steps, with batch size 8, and for 1000 epochs

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v108-GAN
-#SBATCH --output=out_GAN_108.txt
+#SBATCH --job-name=v105-GAN
+#SBATCH --output=out_GAN_105.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-IDEAL-GAN.py --dataset 'GAN-108' --adv_train False --n_G_filters 36 --n_downsamplings 2 --n_res_blocks 4 --encoded_size 6 --epochs 100 --epoch_decay 100 --epoch_ckpt 10 --cycle_loss_weight 1e1 --ls_reg_weight 8e-6 --Fourier_reg_weight 0.0 --NL_SelfAttention True
+python train-ldm.py --experiment_dir 'GAN-105' --n_timesteps 200 --batch_size 8 --epochs_ldm 1000 --epoch_ldm_ckpt 5 --data_augmentation True
