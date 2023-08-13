31a44aa73a646a76a4487909a27260ff1c82edc9
jpmeneses <jpmeneses@uc.cl>
2023-08-13 10:43:38 +1000

    Submit VAE-GAN(v010): Train with variance-normalized LS, 3 downsamplings and slightly-increased KL-reg

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v109-GAN
-#SBATCH --output=out_GAN_109.txt
+#SBATCH --job-name=v110-GAN
+#SBATCH --output=out_GAN_110.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-IDEAL-GAN.py --dataset 'GAN-109' --adv_train False --n_G_filters 36 --n_downsamplings 2 --n_res_blocks 4 --encoded_size 6 --epochs 100 --epoch_decay 100 --epoch_ckpt 10 --cycle_loss_weight 1e1 --ls_reg_weight 7e-6 --Fourier_reg_weight 0.0 --NL_SelfAttention True
+python train-IDEAL-GAN.py --dataset 'GAN-110' --adv_train False --n_G_filters 36 --n_downsamplings 3 --n_res_blocks 2 --encoded_size 6 --epochs 100 --epoch_decay 100 --epoch_ckpt 20 --cycle_loss_weight 1e1 --ls_reg_weight 8e-6 --Fourier_reg_weight 0.0 --NL_SelfAttention True
