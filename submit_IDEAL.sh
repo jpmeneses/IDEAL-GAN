4a674dfb1db0770330db7f78221df23b8237b492
jpmeneses <jpmeneses@uc.cl>
2023-08-16 10:51:34 +1000

    Re-submit VAE-GAN(v047): Activate adversarial training and decrease perceptual loss weighting

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -7,4 +7,4 @@
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-IDEAL-GAN.py --dataset 'GAN-047' --adv_train False --n_G_filters 36 --n_downsamplings 3 --n_res_blocks 2 --encoded_size 6 --epochs 100 --epoch_decay 60 --epoch_ckpt 5 --perceptual_loss True --cycle_loss_weight 1e0 --B2A2B_weight 1e1 --ls_reg_weight 5e-6 --Fourier_reg_weight 0.0 --NL_SelfAttention True
+python train-IDEAL-GAN.py --dataset 'GAN-047' --adv_train True --n_G_filters 36 --n_downsamplings 3 --n_res_blocks 2 --encoded_size 6 --epochs 100 --epoch_decay 60 --epoch_ckpt 5 --perceptual_loss True --cycle_loss_weight 5e-1 --B2A2B_weight 2e1 --ls_reg_weight 5e-6 --Fourier_reg_weight 0.0 --NL_SelfAttention True
