d686cece8aeded19bf7e1f8334fda971430625f7
jpmeneses <jpmeneses@uc.cl>
2023-08-17 14:29:49 +1000

    Submit VAE-GAN(v053): Slightly decrease KL-reg (compared to v051)

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v047-DDPM
-#SBATCH --output=out_DDPM_047.txt
+#SBATCH --job-name=v053-GAN
+#SBATCH --output=out_GAN_053.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-ldm.py --experiment_dir 'GAN-047' --n_timesteps 200 --batch_size 8 --epochs_ldm 200 --epoch_ldm_ckpt 5 --data_augmentation True --lr 1e-4
+python train-IDEAL-GAN.py --dataset 'GAN-053' --adv_train True --n_G_filters 36 --n_downsamplings 3 --n_res_blocks 2 --encoded_size 6 --epochs 60 --epoch_decay 60 --epoch_ckpt 5 --D_lr_factor 4 --critic_train_steps 2 --perceptual_loss False --cycle_loss_weight 5e0 --ls_reg_weight 9e-6 --Fourier_reg_weight 0.0 --NL_SelfAttention True
