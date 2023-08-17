0f8c2cc25d9481b3f7dd68bde9680eb74c46efbf
jpmeneses <jpmeneses@uc.cl>
2023-08-17 20:32:31 +1000

    Submit VAE-GAN(v053): Run VAE with reference loss weights and architecture (LS: 6 dims)

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v053-GAN
-#SBATCH --output=out_GAN_053.txt
+#SBATCH --job-name=v054-GAN
+#SBATCH --output=out_GAN_054.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-IDEAL-GAN.py --dataset 'GAN-053' --adv_train True --n_G_filters 36 --n_downsamplings 3 --n_res_blocks 2 --encoded_size 6 --epochs 60 --epoch_decay 60 --epoch_ckpt 5 --D_lr_factor 4 --critic_train_steps 2 --perceptual_loss False --cycle_loss_weight 5e0 --ls_reg_weight 9e-6 --Fourier_reg_weight 0.0 --NL_SelfAttention True
+python train-IDEAL-GAN.py --dataset 'GAN-054' --adv_train False --n_G_filters 36 --n_downsamplings 4 --n_res_blocks 2 --encoded_size 6 --epochs 100 --epoch_decay 100 --epoch_ckpt 20 --perceptual_loss False --cycle_loss_weight 1e0 --ls_reg_weight 1e-6 --Fourier_reg_weight 0.0 --NL_SelfAttention True
