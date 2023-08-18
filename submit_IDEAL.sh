b5515f2a1bbbb81ab1667312e963a77c8a403c37
jpmeneses <jpmeneses@uc.cl>
2023-08-18 22:43:15 +1000

    Submit VAE-GAN(v061): Same as v060 but with increased num. of LS channels (36)

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v060-GAN
-#SBATCH --output=out_GAN_060.txt
+#SBATCH --job-name=v061-GAN
+#SBATCH --output=out_GAN_061.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-IDEAL-GAN.py --dataset 'GAN-060' --adv_train False --n_G_filters 36 --n_downsamplings 4 --n_res_blocks 2 --encoded_size 24 --epochs 100 --epoch_decay 100 --epoch_ckpt 20 --perceptual_loss False --cycle_loss_weight 1.0 --ls_reg_weight 1e-6 --Fourier_reg_weight 0.0 --NL_SelfAttention True
+python train-IDEAL-GAN.py --dataset 'GAN-061' --adv_train False --n_G_filters 36 --n_downsamplings 4 --n_res_blocks 2 --encoded_size 36 --epochs 100 --epoch_decay 100 --epoch_ckpt 20 --perceptual_loss False --cycle_loss_weight 1.0 --ls_reg_weight 1e-6 --Fourier_reg_weight 0.0 --NL_SelfAttention True
