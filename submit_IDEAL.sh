c8b86b712ee31efbf85b049bba95f7012b175bb6
jpmeneses <jpmeneses@uc.cl>
2023-08-18 10:51:21 +1000

    Re-submit VAE-GAN(v057): Change LS channels to 128

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -7,4 +7,4 @@
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-IDEAL-GAN.py --dataset 'GAN-057' --adv_train True --n_G_filters 36 --n_downsamplings 4 --n_res_blocks 2 --encoded_size 6 --epochs 100 --epoch_decay 100 --epoch_ckpt 20 --D_lr_factor 4 --critic_train_steps 2 --perceptual_loss False --cycle_loss_weight 5e0 --ls_reg_weight 9e-6 --Fourier_reg_weight 0.0 --NL_SelfAttention True
+python train-IDEAL-GAN.py --dataset 'GAN-057' --adv_train True --n_G_filters 36 --n_downsamplings 4 --n_res_blocks 2 --encoded_size 128 --epochs 100 --epoch_decay 100 --epoch_ckpt 20 --D_lr_factor 4 --critic_train_steps 2 --perceptual_loss False --cycle_loss_weight 5e0 --ls_reg_weight 9e-6 --Fourier_reg_weight 0.0 --NL_SelfAttention True
