843423dd60bf7227fc3d1a9233c6f05d62c48136
jpmeneses <jpmeneses@uc.cl>
2023-08-20 18:01:26 +1000

    Submit VAE-GAN(v072): Same as v069 but with half of KL-reg weight

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v071-GAN
-#SBATCH --output=out_GAN_071.txt
+#SBATCH --job-name=v072-GAN
+#SBATCH --output=out_GAN_072.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-IDEAL-GAN.py --dataset 'GAN-071' --adv_train False --n_G_filters 36 --n_downsamplings 4 --n_res_blocks 2 --encoded_size 24 --epochs 100 --epoch_decay 100 --epoch_ckpt 20 --perceptual_loss False --cycle_loss_weight 1.0 --ls_reg_weight 5e-6 --Fourier_reg_weight 0.0 --NL_SelfAttention True
+python train-IDEAL-GAN.py --dataset 'GAN-072' --adv_train False --n_G_filters 36 --n_downsamplings 4 --n_res_blocks 2 --encoded_size 24 --epochs 100 --epoch_decay 100 --epoch_ckpt 20 --perceptual_loss False --cycle_loss_weight 1.0 --ls_reg_weight 178e-8 --Fourier_reg_weight 0.0 --NL_SelfAttention True
