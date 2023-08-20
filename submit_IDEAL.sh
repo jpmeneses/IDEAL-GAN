f8fe9c960a8732ed60c52b79bf1ba317ae35865e
jpmeneses <jpmeneses@uc.cl>
2023-08-20 18:05:24 +1000

    Modified KL-reg

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -7,4 +7,4 @@
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-IDEAL-GAN.py --dataset 'GAN-072' --adv_train False --n_G_filters 36 --n_downsamplings 4 --n_res_blocks 2 --encoded_size 24 --epochs 100 --epoch_decay 100 --epoch_ckpt 20 --perceptual_loss False --cycle_loss_weight 1.0 --ls_reg_weight 178e-8 --Fourier_reg_weight 0.0 --NL_SelfAttention True
+python train-IDEAL-GAN.py --dataset 'GAN-072' --adv_train False --n_G_filters 36 --n_downsamplings 4 --n_res_blocks 2 --encoded_size 24 --epochs 100 --epoch_decay 100 --epoch_ckpt 20 --perceptual_loss False --cycle_loss_weight 1.0 --ls_reg_weight 237e-8 --Fourier_reg_weight 0.0 --NL_SelfAttention True
