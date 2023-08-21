5f525250e6fee02d351633709cf5fbd6dc890d55
jpmeneses <jpmeneses@uc.cl>
2023-08-21 14:47:56 +1000

    Submit VAE-GAN(v075): 96 LS-channels, KL-reg weight of 1.1e-6

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v074-GAN
-#SBATCH --output=out_GAN_074.txt
+#SBATCH --job-name=v075-GAN
+#SBATCH --output=out_GAN_075.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-IDEAL-GAN.py --dataset 'GAN-074' --adv_train False --n_G_filters 36 --n_downsamplings 4 --n_res_blocks 2 --encoded_size 96 --epochs 100 --epoch_decay 100 --epoch_ckpt 20 --perceptual_loss False --cycle_loss_weight 1.0 --ls_reg_weight 1e-6 --Fourier_reg_weight 0.0 --NL_SelfAttention True
+python train-IDEAL-GAN.py --dataset 'GAN-075' --adv_train False --n_G_filters 36 --n_downsamplings 4 --n_res_blocks 2 --encoded_size 96 --epochs 100 --epoch_decay 100 --epoch_ckpt 20 --perceptual_loss False --cycle_loss_weight 1.0 --ls_reg_weight 1.1e-6 --Fourier_reg_weight 0.0 --NL_SelfAttention True
