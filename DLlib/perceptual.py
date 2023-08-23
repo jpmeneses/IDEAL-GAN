c722ff71de16c292e09d9986efa9a2af42ae2db9
jpmeneses <jpmeneses@uc.cl>
2023-08-23 20:29:16 +1000

    Add 3rd channel to input array: Change zero-valued array to repeater real-channel

--- DLlib/perceptual.py
+++ DLlib/perceptual.py
@@ -10,7 +10,7 @@ def metric_model(input_shape, layers=[2,5,8,13,18], pad=(16,16)):
 		metric_vgg = keras.Model(inputs=vgg.inputs, outputs=vgg.layers[l].output)
 
 		x = keras.layers.Lambda(lambda x: tf.reshape(x,[-1,x.shape[2],x.shape[3],x.shape[4]]))(inputs)
-		x = keras.layers.Lambda(lambda x: tf.concat([x,tf.zeros_like(x[:,:,:,:1])],axis=-1))(x)
+		x = keras.layers.Lambda(lambda x: tf.concat([x,x[:,:,:,:1]],axis=-1))(x)
 		x = keras.layers.ZeroPadding2D(padding=pad)(x)
 		x = metric_vgg(x)
 
