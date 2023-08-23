3e3a4db1dc7698e2a1e4a207b62f1b28deef2b0b
jpmeneses <jpmeneses@uc.cl>
2023-08-23 20:28:39 +1000

    Submit VAE-GAN(v205): Same as v203, but relaxed KL-reg

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v202-DDPM
-#SBATCH --output=out_DDPM_202.txt
+#SBATCH --job-name=v205-GAN
+#SBATCH --output=out_GAN_205.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-ldm.py --experiment_dir 'GAN-202' --n_timesteps 200 --batch_size 8 --epochs_ldm 200 --epoch_ldm_ckpt 5 --data_augmentation True --lr 1e-4
+python train-IDEAL-GAN.py --dataset 'GAN-205' --adv_train False --n_G_filters 36 --n_downsamplings 4 --n_res_blocks 2 --encoded_size 24 --epochs 100 --epoch_decay 100 --epoch_ckpt 20 --perceptual_loss True --cycle_loss_weight 1e0 --ls_reg_weight 1e-6 --Fourier_reg_weight 0.0 --NL_SelfAttention True
