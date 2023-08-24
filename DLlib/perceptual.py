7bbc3a11e7113701510725917214ea3b35b23c60
jpmeneses <jpmeneses@uc.cl>
2023-08-24 19:15:46 +1000

    Modify perceptual loss: Add magnitude image in the third channel prior to VGG model

--- DLlib/perceptual.py
+++ DLlib/perceptual.py
@@ -8,11 +8,12 @@ def metric_model(input_shape, layers=[2,5,8,13,18], pad=(16,16)):
 	output = list()
 	for l in layers:
 		metric_vgg = keras.Model(inputs=vgg.inputs, outputs=vgg.layers[l].output)
-
 		x = keras.layers.Lambda(lambda x: tf.reshape(x,[-1,x.shape[2],x.shape[3],x.shape[4]]))(inputs)
-		x = keras.layers.Lambda(lambda x: tf.concat([x,x[:,:,:,:1]],axis=-1))(x)
+		x = keras.layers.Lambda(lambda x: tf.concat([x,tf.math.sqrt(tf.reduce_sum(tf.math.square(x),axis=-1,keepdims=True))],axis=-1))(x)
 		x = keras.layers.ZeroPadding2D(padding=pad)(x)
 		x = metric_vgg(x)
-
 		output.append(x)
+	
 	return keras.Model(inputs=inputs, outputs=output)
+
+
