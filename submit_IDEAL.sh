e14aca7415c9e03791135aa271e587bf5aae7b64
jpmeneses <jpmeneses@uc.cl>
2023-08-24 13:55:49 +1000

    Same as v203, without data augmentation

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v206-GAN
-#SBATCH --output=out_GAN_206.txt
+#SBATCH --job-name=v207-GAN
+#SBATCH --output=out_GAN_207.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-IDEAL-GAN.py --dataset 'GAN-206' --adv_train True --n_G_filters 36 --n_downsamplings 4 --n_res_blocks 2 --encoded_size 24 --epochs 100 --epoch_decay 100 --epoch_ckpt 20 --D_lr_factor 4 --critic_train_steps 3 --perceptual_loss False --cycle_loss_weight 1e0 --ls_reg_weight 1e-6 --Fourier_reg_weight 0.0 --NL_SelfAttention True
+python train-IDEAL-GAN.py --dataset 'GAN-207' --adv_train True --n_G_filters 36 --n_downsamplings 4 --n_res_blocks 2 --encoded_size 24 --epochs 100 --epoch_decay 100 --epoch_ckpt 20 --D_lr_factor 4 --data_aug_p 0.0 --critic_train_steps 3 --perceptual_loss False --cycle_loss_weight 1e0 --ls_reg_weight 3e-6 --Fourier_reg_weight 0.0 --NL_SelfAttention True
