8db7d1ef543fa38433ecbef69e432794a7f574fc
jpmeneses <jpmeneses@uc.cl>
2023-08-25 14:12:48 +1000

    Add 255 scaling factor, like RGB image

--- DLlib/perceptual.py
+++ DLlib/perceptual.py
@@ -6,6 +6,7 @@ vgg = keras.applications.vgg19.VGG19()
 def metric_model(input_shape, layers=[2,5,8,13,18], pad=(16,16)):
 	inputs = keras.Input(input_shape)
 	x = keras.layers.Lambda(lambda x: tf.concat([x*0.5+0.5,tf.math.sqrt(tf.reduce_sum(tf.math.square(x),axis=-1,keepdims=True))],axis=-1))(inputs)
+	x = keras.layers.Lambda(lambda x: 255.0*x)(x)
 	x = keras.layers.Lambda(lambda x: tf.reshape(x,[-1,x.shape[2],x.shape[3],x.shape[4]]))(x)
 	x = keras.layers.ZeroPadding2D(padding=pad)(x)
 	x = keras.applications.vgg19.preprocess_input(x)
