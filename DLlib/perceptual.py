738df7f051aa3c3d650446cc3c7d4e0bdfc04fd1
jpmeneses <jpmeneses@uc.cl>
2023-08-25 13:52:56 +1000

    Pre-process input, before computing VGG features

--- DLlib/perceptual.py
+++ DLlib/perceptual.py
@@ -5,14 +5,16 @@ vgg = keras.applications.vgg19.VGG19()
 
 def metric_model(input_shape, layers=[2,5,8,13,18], pad=(16,16)):
 	inputs = keras.Input(input_shape)
+	x = keras.layers.Lambda(lambda x: x*0.5 + 1.0)(inputs)
+	x = keras.layers.Lambda(lambda x: tf.reshape(x,[-1,x.shape[2],x.shape[3],x.shape[4]]))(inputs)
+	x = keras.layers.Lambda(lambda x: tf.concat([x,tf.math.sqrt(tf.reduce_sum(tf.math.square(x),axis=-1,keepdims=True))],axis=-1))(x)
+	x = keras.layers.ZeroPadding2D(padding=pad)(x)
+	x = keras.applications.vgg19.preprocess_input(x)
 	output = list()
 	for l in layers:
 		metric_vgg = keras.Model(inputs=vgg.inputs, outputs=vgg.layers[l].output)
-		x = keras.layers.Lambda(lambda x: tf.reshape(x,[-1,x.shape[2],x.shape[3],x.shape[4]]))(inputs)
-		x = keras.layers.Lambda(lambda x: tf.concat([x,tf.math.sqrt(tf.reduce_sum(tf.math.square(x),axis=-1,keepdims=True))],axis=-1))(x)
-		x = keras.layers.ZeroPadding2D(padding=pad)(x)
-		x = metric_vgg(x)
-		output.append(x)
+		x_l = metric_vgg(x)
+		output.append(x_l)
 	
 	return keras.Model(inputs=inputs, outputs=output)
 
