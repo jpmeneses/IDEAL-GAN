ba09f8d8c506bc19b3ca0018edcc3366e57d9da5
jpmeneses <jpmeneses@uc.cl>
2023-08-27 14:01:49 +1000

    Submit VAE-GAN(v212): Same as v210 (only perceptual loss, increased KL-reg)

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v211-GAN
-#SBATCH --output=out_GAN_211.txt
+#SBATCH --job-name=v212-GAN
+#SBATCH --output=out_GAN_212.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-IDEAL-GAN.py --dataset 'GAN-211' --adv_train True --n_G_filters 36 --n_downsamplings 4 --n_res_blocks 2 --encoded_size 24 --epochs 100 --epoch_decay 100 --epoch_ckpt 20 --D_lr_factor 4 --data_aug_p 0.0 --critic_train_steps 3 --perceptual_loss True --cycle_loss_weight 1e0 --ls_reg_weight 2e-6 --Fourier_reg_weight 0.0 --NL_SelfAttention True
+python train-IDEAL-GAN.py --dataset 'GAN-212' --adv_train False --n_G_filters 36 --n_downsamplings 4 --n_res_blocks 2 --encoded_size 24 --epochs 100 --epoch_decay 100 --epoch_ckpt 20 --data_aug_p 0.0 --perceptual_loss True --cycle_loss_weight 1e0 --ls_reg_weight 5e-6 --Fourier_reg_weight 0.0 --NL_SelfAttention True
