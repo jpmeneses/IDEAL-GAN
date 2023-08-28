cb1f229c912c855d814e3df6d30b2e7483d7df4b
jpmeneses <jpmeneses@uc.cl>
2023-08-28 19:09:41 +1000

    Submit FID-v212

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v209-FID
-#SBATCH --output=out_FID_209.txt
+#SBATCH --job-name=v212-FID
+#SBATCH --output=out_FID_212.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python test-genMetrics.py --experiment_dir 'output/GAN-209'
+python test-genMetrics.py --experiment_dir 'output/GAN-212'
