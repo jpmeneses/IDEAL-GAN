63280c2f22c1bf393929b65767c955dda87e9798
jpmeneses <jpmeneses@uc.cl>
2023-08-28 19:02:07 +1000

    Submit FID-v201: Measure FID for VAE-GAN v201

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v200-FID
-#SBATCH --output=out_FID_200.txt
+#SBATCH --job-name=v201-FID
+#SBATCH --output=out_FID_201.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python test-genMetrics.py --experiment_dir 'output/GAN-200'
+python test-genMetrics.py --experiment_dir 'output/GAN-201'
