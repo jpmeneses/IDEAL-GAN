9a8518abeaec049db9bd903eec3218e588763180
jpmeneses <jpmeneses@uc.cl>
2023-08-31 19:13:57 +1000

    Submit Unsup(v100): Testing in ING-UC cluster

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v218-GAN
-#SBATCH --output=out_GAN_218.txt
+#SBATCH --job-name=v100-unsup
+#SBATCH --output=out_unsup_100.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-IDEAL-GAN.py --dataset 'GAN-218' --adv_train True --n_G_filters 36 --n_downsamplings 4 --n_res_blocks 2 --encoded_size 24 --epochs 100 --epoch_decay 100 --epoch_ckpt 20 --D_lr_factor 4 --data_aug_p 0.0 --critic_train_steps 3 --perceptual_loss True --cycle_loss_weight 1e-2 --ls_reg_weight 4e-8 --Fourier_reg_weight 0.0 --NL_SelfAttention True
+python train-IDEAL-unsup.py --dataset 'Unsup-100' --UQ True --epochs 30 --epoch_decay 30 
