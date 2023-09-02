77d1ae6609931c3b43f5bc9fd12d09e551816760
jpmeneses <jpmeneses@uc.cl>
2023-09-02 21:39:13 +1000

    Submit VAE-GAN(v222): Same as v221, but reactivate B-loss, which is now calculating the MAE between parametric maps only

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v221-GAN
-#SBATCH --output=out_GAN_221.txt
+#SBATCH --job-name=v222-GAN
+#SBATCH --output=out_GAN_222.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-IDEAL-GAN.py --dataset 'GAN-221' --adv_train False --n_G_filters 36 --n_downsamplings 4 --n_res_blocks 2 --encoded_size 24 --epochs 100 --epoch_decay 100 --epoch_ckpt 20 --data_aug_p 0.0 --perceptual_loss True --cycle_loss_weight 1e-1 --B2A2B_weight 0.0 --ls_reg_weight 4e-7 --Fourier_reg_weight 0.0 --NL_SelfAttention True
+python train-IDEAL-GAN.py --dataset 'GAN-222' --adv_train False --n_G_filters 36 --n_downsamplings 4 --n_res_blocks 2 --encoded_size 24 --epochs 100 --epoch_decay 100 --epoch_ckpt 20 --data_aug_p 0.0 --perceptual_loss True --cycle_loss_weight 1e-1 --ls_reg_weight 5e-7 --Fourier_reg_weight 0.0 --NL_SelfAttention True
