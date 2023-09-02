fd963ad962fdba5581511a773cd63a54ef33a56c
jpmeneses <jpmeneses@uc.cl>
2023-09-02 19:28:04 +1000

    Submit VAE-GAN(v220): Same as v217, slightly increased KL-reg

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v219-GAN
-#SBATCH --output=out_GAN_219.txt
+#SBATCH --job-name=v220-GAN
+#SBATCH --output=out_GAN_220.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-IDEAL-GAN.py --dataset 'GAN-219' --adv_train True --n_G_filters 36 --n_downsamplings 4 --n_res_blocks 2 --encoded_size 24 --epochs 100 --epoch_decay 100 --epoch_ckpt 20 --D_lr_factor 4 --data_aug_p 0.0 --critic_train_steps 1 --perceptual_loss True --cycle_loss_weight 1e0 --ls_reg_weight 4e-6 --Fourier_reg_weight 0.0 --NL_SelfAttention True
+python train-IDEAL-GAN.py --dataset 'GAN-220' --adv_train False --n_G_filters 36 --n_downsamplings 3 --n_res_blocks 3 --encoded_size 6 --epochs 100 --epoch_decay 100 --epoch_ckpt 20 --data_aug_p 0.0 --perceptual_loss True --cycle_loss_weight 1e0 --ls_reg_weight 2e-6 --Fourier_reg_weight 0.0 --NL_SelfAttention True
