335eb22ecf2215ac76e27cffd6b8de25f1574287
jpmeneses <jpmeneses@uc.cl>
2023-09-05 09:48:03 +1000

    Re-submit Unsup-v105: Slightly decrease FM-L1 reg

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v107-Unsup
-#SBATCH --output=out_unsup_107.txt
+#SBATCH --job-name=v105-Unsup
+#SBATCH --output=out_unsup_105.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-IDEAL-unsup.py --dataset 'Unsup-107' --out_vars 'FM' --UQ True --k_fold 2 --epochs 35 --epoch_decay 35
+python train-IDEAL-unsup.py --dataset 'Unsup-105' --out_vars 'PM' --UQ True --k_fold 5 --epochs 35 --epoch_decay 35 --FM_L1_weight 5e-1
