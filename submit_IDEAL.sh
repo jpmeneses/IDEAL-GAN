858ec0cb6273328ea3d5dd8cecbba907f1b8db88
jpmeneses <jpmeneses@uc.cl>
2023-09-05 08:24:27 +1000

    Submit Unsup-v105: Run again k-fold No.5, add FM-L1 reg

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v104-Unsup
-#SBATCH --output=out_unsup_104.txt
+#SBATCH --job-name=v105-Unsup
+#SBATCH --output=out_unsup_105.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-IDEAL-unsup.py --dataset 'Unsup-104' --out_vars 'PM' --UQ True --k_fold 5 --epochs 35 --epoch_decay 35
+python train-IDEAL-unsup.py --dataset 'Unsup-105' --out_vars 'PM' --UQ True --k_fold 5 --epochs 35 --epoch_decay 35 --FM_L1_weight 1e-3
