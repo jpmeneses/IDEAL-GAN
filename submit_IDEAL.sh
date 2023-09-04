fdce38ef624619ad3699eb87a3407b4abca16fb4
jpmeneses <jpmeneses@uc.cl>
2023-09-04 20:10:39 +1000

    Submit Unsup-v103: Train FM-model, k-fold No.4

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v102-Unsup
-#SBATCH --output=out_unsup_102.txt
+#SBATCH --job-name=v103-Unsup
+#SBATCH --output=out_unsup_103.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-IDEAL-unsup.py --dataset 'Unsup-102' --out_vars 'PM' --UQ True --k_fold 3 --epochs 35 --epoch_decay 35
+python train-IDEAL-unsup.py --dataset 'Unsup-103' --out_vars 'PM' --UQ True --k_fold 4 --epochs 35 --epoch_decay 35
