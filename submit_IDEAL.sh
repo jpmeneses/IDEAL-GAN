97a79b99d39acc51b6efcaf668b15b2092466f3d
jpmeneses <jpmeneses@uc.cl>
2023-09-05 12:52:57 +1000

    Re-submit Unsup-v106: Train both models up to 100 epochs

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v107-Unsup
-#SBATCH --output=out_unsup_107.txt
+#SBATCH --job-name=v106-Unsup
+#SBATCH --output=out_unsup_106.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-IDEAL-unsup.py --dataset 'Unsup-107' --out_vars 'R2s' --UQ True --k_fold 2 --epochs 40 --epoch_decay 40
+python train-IDEAL-unsup.py --dataset 'Unsup-106' --out_vars 'PM' --UQ True --k_fold 1 --epochs 100 --epoch_decay 100
