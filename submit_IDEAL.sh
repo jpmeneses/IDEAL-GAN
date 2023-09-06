83522e3c376ecc13700ac4446da9106efd8b8847
jpmeneses <jpmeneses@uc.cl>
2023-09-06 10:41:45 +1000

    Submit again Unsup-v110: Run again, divergence in previous attempt

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -7,4 +7,4 @@
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-IDEAL-unsup.py --dataset 'Unsup-110' --out_vars 'R2s' --UQ True --k_fold 5 --epochs 40 --epoch_decay 40
+python train-IDEAL-unsup.py --dataset 'Unsup-110' --out_vars 'FM' --UQ True --k_fold 5 --epochs 35 --epoch_decay 35
