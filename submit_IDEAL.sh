68abc85fd55daf9f0c765f33e236f629c3b12413
jpmeneses <jpmeneses@uc.cl>
2023-09-06 14:12:42 +1000

    Re-submit Unsup-v112: Train R2s model for 5 epochs

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v111-GAN
-#SBATCH --output=out_GAN_111.txt
+#SBATCH --job-name=v112-GAN
+#SBATCH --output=out_GAN_112.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-IDEAL-unsup.py --dataset 'Unsup-111' --out_vars 'R2s' --UQ True --k_fold 1 --epochs 25 --epoch_decay 25
+python train-IDEAL-unsup.py --dataset 'Unsup-112' --out_vars 'R2s' --UQ True --k_fold 2 --epochs 25 --epoch_decay 25
