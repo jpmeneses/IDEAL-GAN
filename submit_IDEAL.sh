49b5edefd14671afb6813fb1c048bb13d583ba7a
jpmeneses <jpmeneses@uc.cl>
2023-09-06 13:00:57 +1000

    Submit again VAE-GAN(v224)

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v112-GAN
-#SBATCH --output=out_GAN_112.txt
+#SBATCH --job-name=v224-GAN
+#SBATCH --output=out_GAN_224.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-IDEAL-unsup.py --dataset 'Unsup-112' --out_vars 'FM' --UQ True --k_fold 2 --epochs 20 --epoch_decay 20
+python train-IDEAL-GAN.py --dataset 'GAN-224' --adv_train False --n_G_filters 36 --n_downsamplings 4 --n_res_blocks 2 --encoded_size 24 --epochs 100 --epoch_decay 100 --epoch_ckpt 20 --data_aug_p 0.0 --perceptual_loss True --cycle_loss_weight 1e-1 --ls_reg_weight 5e-7 --Fourier_reg_weight 0.0 --NL_SelfAttention True
