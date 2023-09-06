Also fix R2s re-scaling for validation plots

train-IDEAL-unsup.py
@@ -64,7 +64,7 @@ os.environ["CUDA_VISIBLE_DEVICES"]="1"
 
 ech_idx = args.n_echoes * 2
 fm_sc = 300.0
-r2_sc = 2*np.pi*fm_sc
+r2_sc = 200.0
 
 ################################################################################
 ######################### DIRECTORIES AND FILENAMES ############################
@@ -600,7 +600,7 @@ for ep in range(args.epochs):
 
                 r2_aux = np.squeeze(A2B[:,2,:,:,1])
                 r2_ok = axs[1,2].imshow(r2_aux*r2_sc, cmap='copper',
-                                        interpolation='none', vmin=0, vmax=fm_sc)
+                                        interpolation='none', vmin=0, vmax=r2_sc)
                 fig.colorbar(r2_ok, ax=axs[1,2])
                 axs[1,2].axis('off')
 
@@ -641,7 +641,7 @@ for ep in range(args.epochs):
 
                 r2n_aux = np.squeeze(B[:,2,:,:,1])
                 r2_unet = axs[2,2].imshow(r2n_aux*r2_sc, cmap='copper',
-                                     interpolation='none', vmin=0, vmax=fm_sc)
+                                     interpolation='none', vmin=0, vmax=r2_sc)
                 fig.colorbar(r2_unet, ax=axs[2,2])
                 axs[2,2].axis('off')
 