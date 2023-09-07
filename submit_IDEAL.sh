a5768dccebe337af3da6aba232c7c0422b645542
jpmeneses <jpmeneses@uc.cl>
2023-09-07 20:41:30 +1000

    Submit Unsup-v117: Train 5-echoes FM model for 20 epochs, k-fold No.2

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v226-GAN
-#SBATCH --output=out_GAN_226.txt
+#SBATCH --job-name=v117-unsup
+#SBATCH --output=out_unsup_117.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-IDEAL-GAN.py --dataset 'GAN-226' --adv_train False --n_G_filters 36 --n_downsamplings 4 --n_res_blocks 2 --encoded_size 24 --epochs 100 --epoch_decay 100 --epoch_ckpt 20 --data_aug_p 0.0 --perceptual_loss True --cycle_loss_weight 1e-1 --B2A2B_weight 8e-1 --ls_reg_weight 3e-7 --Fourier_reg_weight 0.0 --NL_SelfAttention True
+python train-IDEAL-unsup.py --dataset 'Unsup-117' --n_echoes 5 --out_vars 'FM' --UQ True --k_fold 2 --epochs 20 --epoch_decay 20 --epoch_ckpt 10
