f05ca558e747d0c1e24d199a52a9fde8ba110c0c
jpmeneses <jpmeneses@uc.cl>
2023-09-07 11:06:32 +1000

    Re-submit Unsup-v113: Train R2s model for 5 epochs, k-fold No.3

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v115-GAN
-#SBATCH --output=out_GAN_115.txt
+#SBATCH --job-name=v113-GAN
+#SBATCH --output=out_GAN_113.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-IDEAL-unsup.py --dataset 'Unsup-115' --out_vars 'R2s' --UQ True --k_fold 5 --epochs 25 --epoch_decay 25 --epoch_ckpt 5
+python train-IDEAL-unsup.py --dataset 'Unsup-113' --out_vars 'R2s' --UQ True --k_fold 5 --epochs 30 --epoch_decay 30 --epoch_ckpt 5
