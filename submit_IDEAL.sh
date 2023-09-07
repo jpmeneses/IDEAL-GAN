4b19a6f19bfb64f8632f479839a5e98e3eca1d84
jpmeneses <jpmeneses@uc.cl>
2023-09-07 15:18:14 +1000

    Submit VAE-GAN(v225): Decreased B and KL-reg weights

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v113-GAN
-#SBATCH --output=out_GAN_113.txt
+#SBATCH --job-name=v225-GAN
+#SBATCH --output=out_GAN_225.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-IDEAL-unsup.py --dataset 'Unsup-113' --out_vars 'PM' --UQ True --k_fold 3 --epochs 60 --epoch_decay 60 --epoch_ckpt 20
+python train-IDEAL-GAN.py --dataset 'GAN-225' --adv_train False --n_G_filters 36 --n_downsamplings 4 --n_res_blocks 2 --encoded_size 24 --epochs 100 --epoch_decay 100 --epoch_ckpt 20 --data_aug_p 0.0 --perceptual_loss True --cycle_loss_weight 1e-1 --B2A2B_weight 5e-1 --ls_reg_weight 2e-7 --Fourier_reg_weight 0.0 --NL_SelfAttention True
