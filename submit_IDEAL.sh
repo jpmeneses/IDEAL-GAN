09653f48d80cc01d87e3ece7771a3eae846c4922
jpmeneses <jpmeneses@uc.cl>
2023-09-07 10:04:21 +1000

    Re-submit Unsup-v113: Train FM model for 5 more epochs, k-fold No.3

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v115-GAN
-#SBATCH --output=out_GAN_115.txt
+#SBATCH --job-name=v113-GAN
+#SBATCH --output=out_GAN_113.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-IDEAL-unsup.py --dataset 'Unsup-115' --out_vars 'FM' --UQ True --k_fold 5 --epochs 20 --epoch_decay 20 --epoch_ckpt 10
+python train-IDEAL-unsup.py --dataset 'Unsup-113' --out_vars 'FM' --UQ True --k_fold 3 --epochs 25 --epoch_decay 25 --epoch_ckpt 5
