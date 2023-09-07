c681b256f3dc3e1df1348058763c3d9abfb5b084
jpmeneses <jpmeneses@uc.cl>
2023-09-07 12:06:54 +1000

    Re-submit Unsup-v115: Train both models, k-fold No.5

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v114-GAN
-#SBATCH --output=out_GAN_114.txt
+#SBATCH --job-name=v115-GAN
+#SBATCH --output=out_GAN_115.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-IDEAL-unsup.py --dataset 'Unsup-114' --out_vars 'PM' --UQ True --k_fold 4 --epochs 60 --epoch_decay 60 --epoch_ckpt 20
+python train-IDEAL-unsup.py --dataset 'Unsup-115' --out_vars 'PM' --UQ True --k_fold 5 --epochs 60 --epoch_decay 60 --epoch_ckpt 20
