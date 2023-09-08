4754c41543e3211b7e56196f39094a1394ed1c6d
jpmeneses <jpmeneses@uc.cl>
2023-09-08 15:02:34 +1000

    Re-submit VAE-GAN(v227): Train to run adversarial train immediatly after warm-up epochs

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -8,3 +8,4 @@
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
 python train-IDEAL-GAN.py --dataset 'GAN-227' --adv_train False --n_G_filters 36 --n_downsamplings 4 --n_res_blocks 2 --encoded_size 24 --epochs 10 --epoch_decay 10 --epoch_ckpt 5 --data_aug_p 0.0 --perceptual_loss True --cycle_loss_weight 1e-1 --B2A2B_weight 8e-1 --ls_reg_weight 4e-7 --Fourier_reg_weight 0.0 --NL_SelfAttention True
+python train-IDEAL-GAN.py --dataset 'GAN-227' --adv_train True --n_G_filters 36 --n_downsamplings 4 --n_res_blocks 2 --encoded_size 24 --epochs 100 --epoch_decay 100 --epoch_ckpt 20 --data_aug_p 0.0 --perceptual_loss True --cycle_loss_weight 1e-1 --B2A2B_weight 8e-1 --ls_reg_weight 4e-7 --Fourier_reg_weight 0.0 --NL_SelfAttention True
