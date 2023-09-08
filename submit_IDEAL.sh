18690971cd71ca020484826d1226ef152d9a6a9b
jpmeneses <jpmeneses@uc.cl>
2023-09-08 18:25:16 +1000

    Re-submit Unsup-v120: Train 5-echoes R2s model (5 ep) and then both (until 60 ep), k-fold No.5

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-#SBATCH --job-name=v119-unsup
-#SBATCH --output=out_unsup_119.txt
+#SBATCH --job-name=v120-unsup
+#SBATCH --output=out_unsup_120.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-IDEAL-unsup.py --dataset 'Unsup-119' --n_echoes 5 --out_vars 'R2s' --UQ True --k_fold 4 --epochs 30 --epoch_decay 30 --epoch_ckpt 5
-python train-IDEAL-unsup.py --dataset 'Unsup-119' --n_echoes 5 --out_vars 'PM' --UQ True --k_fold 4 --epochs 60 --epoch_decay 60 --epoch_ckpt 20
+python train-IDEAL-unsup.py --dataset 'Unsup-120' --n_echoes 5 --out_vars 'R2s' --UQ True --k_fold 5 --epochs 30 --epoch_decay 30 --epoch_ckpt 5
+python train-IDEAL-unsup.py --dataset 'Unsup-120' --n_echoes 5 --out_vars 'PM' --UQ True --k_fold 5 --epochs 60 --epoch_decay 60 --epoch_ckpt 20
