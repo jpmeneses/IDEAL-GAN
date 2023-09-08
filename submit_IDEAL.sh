5d838cdf3627f46af27bf6040885c45be40da1a5
jpmeneses <jpmeneses@uc.cl>
2023-09-08 17:54:14 +1000

    Edit Unsup-v119: Train 5-echoes FM model during 5 more epochs

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -7,4 +7,4 @@
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-IDEAL-unsup.py --dataset 'Unsup-119' --n_echoes 5 --out_vars 'R2s' --UQ True --k_fold 4 --epochs 25 --epoch_decay 25 --epoch_ckpt 5
+python train-IDEAL-unsup.py --dataset 'Unsup-119' --n_echoes 5 --out_vars 'FM' --UQ True --k_fold 4 --epochs 25 --epoch_decay 25 --epoch_ckpt 5
