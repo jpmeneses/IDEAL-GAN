d09cfefe4cb5868565f704983572587e202aa8a2
jpmeneses <jpmeneses@uc.cl>
2023-09-08 14:56:08 +1000

    Submit Unsup-v120: Train 5-echoes FM model for 20 epochs, k-fold No.5

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v119-unsup
-#SBATCH --output=out_unsup_119.txt
+#SBATCH --job-name=v120-unsup
+#SBATCH --output=out_unsup_120.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-IDEAL-unsup.py --dataset 'Unsup-119' --n_echoes 5 --out_vars 'FM' --UQ True --k_fold 4 --epochs 20 --epoch_decay 20 --epoch_ckpt 10
+python train-IDEAL-unsup.py --dataset 'Unsup-120' --n_echoes 5 --out_vars 'FM' --UQ True --k_fold 5 --epochs 20 --epoch_decay 20 --epoch_ckpt 10
