2197aa3ac6c88425103e894a232ac0f4bf7130a9
jpmeneses <jpmeneses@uc.cl>
2023-09-08 11:11:57 +1000

    Re-submit Unsup-v118: Train both 5-echoes models until 60 epochs, k-fold No.3

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v117-unsup
-#SBATCH --output=out_unsup_117.txt
+#SBATCH --job-name=v118-unsup
+#SBATCH --output=out_unsup_118.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-IDEAL-unsup.py --dataset 'Unsup-117' --n_echoes 5 --out_vars 'PM' --UQ True --k_fold 2 --epochs 60 --epoch_decay 60 --epoch_ckpt 20
+python train-IDEAL-unsup.py --dataset 'Unsup-118' --n_echoes 5 --out_vars 'PM' --UQ True --k_fold 3 --epochs 60 --epoch_decay 60 --epoch_ckpt 20
