eb017f145fa3eb134865dfe4a532ae7da94fbe58
jpmeneses <jpmeneses@uc.cl>
2023-09-09 18:53:23 +1000

    Submit VAE-GAN(v228): Same as v227, but increased B-loss weight and slightly decreased KL-reg

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-#SBATCH --job-name=v120-unsup
-#SBATCH --output=out_unsup_120.txt
+#SBATCH --job-name=v228-GAN
+#SBATCH --output=out_GAN_228.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-IDEAL-unsup.py --dataset 'Unsup-120' --n_echoes 5 --out_vars 'R2s' --UQ True --k_fold 5 --epochs 30 --epoch_decay 30 --epoch_ckpt 5
-python train-IDEAL-unsup.py --dataset 'Unsup-120' --n_echoes 5 --out_vars 'PM' --UQ True --k_fold 5 --epochs 60 --epoch_decay 60 --epoch_ckpt 20
+python train-IDEAL-GAN.py --dataset 'GAN-228' --adv_train False --n_G_filters 36 --n_downsamplings 4 --n_res_blocks 2 --encoded_size 24 --epochs 10 --epoch_decay 10 --epoch_ckpt 5 --data_aug_p 0.0 --perceptual_loss True --cycle_loss_weight 1e-1 --B2A2B_weight 1e0 --ls_reg_weight 3e-7 --Fourier_reg_weight 0.0 --NL_SelfAttention True
+python train-IDEAL-GAN.py --dataset 'GAN-228' --adv_train True --n_G_filters 36 --n_downsamplings 4 --n_res_blocks 2 --encoded_size 24 --epochs 100 --epoch_decay 100 --epoch_ckpt 20 --data_aug_p 0.0 --perceptual_loss True --cycle_loss_weight 1e-1 --B2A2B_weight 1e0 --ls_reg_weight 3e-7 --Fourier_reg_weight 0.0 --NL_SelfAttention True
