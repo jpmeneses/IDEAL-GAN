38e41d887aea0d49c5d361498336ed459ad01ac9
jpmeneses <jpmeneses@uc.cl>
2023-09-12 08:36:49 +1000

    Submit Unsup-v122: Train all stages for 4-echoes model, k-fold No.2

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-#SBATCH --job-name=v121-unsup
-#SBATCH --output=out_unsup_121.txt
+#SBATCH --job-name=v122-unsup
+#SBATCH --output=out_unsup_122.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-IDEAL-unsup.py --dataset 'Unsup-121' --n_echoes 4 --out_vars 'FM' --UQ True --k_fold 1 --epochs 20 --epoch_decay 20 --epoch_ckpt 10
-python train-IDEAL-unsup.py --dataset 'Unsup-121' --n_echoes 4 --out_vars 'R2s' --UQ True --k_fold 1 --epochs 25 --epoch_decay 25 --epoch_ckpt 5
-python train-IDEAL-unsup.py --dataset 'Unsup-121' --n_echoes 4 --out_vars 'PM' --UQ True --k_fold 1 --epochs 60 --epoch_decay 60 --epoch_ckpt 20
+python train-IDEAL-unsup.py --dataset 'Unsup-122' --n_echoes 4 --out_vars 'FM' --UQ True --k_fold 2 --epochs 20 --epoch_decay 20 --epoch_ckpt 10
+python train-IDEAL-unsup.py --dataset 'Unsup-122' --n_echoes 4 --out_vars 'R2s' --UQ True --k_fold 2 --epochs 25 --epoch_decay 25 --epoch_ckpt 5
+python train-IDEAL-unsup.py --dataset 'Unsup-122' --n_echoes 4 --out_vars 'PM' --UQ True --k_fold 2 --epochs 60 --epoch_decay 60 --epoch_ckpt 20
