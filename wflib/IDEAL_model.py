Debug IDEAL gradient: Correct TE shape

wflib/IDEAL_model.py
@@ -162,7 +162,7 @@ def IDEAL_model(out_maps, params):
         ds_dp = tf.linalg.matmul(Wp_d,M) * rho_sc ## (nb,nv,ne,ns) I1
         
         # Xi gradient, considering Taylor approximation
-        dxi = tf.squeeze(tf.linalg.diag(2*np.pi*te_complex)) # (1,ne) --> (ne,ne)
+        dxi = tf.linalg.diag(2*np.pi*tf.squeeze(te_complex,-1)) # (nb,ne,1) --> (nb,ne,ne)
         ds_dxi = tf.linalg.matmul(dxi,Smtx) # (nb,ne,nv)
         ds_dxi = tf.complex(tf.math.real(ds_dxi)*fm_sc,tf.math.imag(ds_dxi)*r2_sc/(2*np.pi))
         ds_dxi = tf.expand_dims(tf.transpose(ds_dxi,perm=[0,2,1]),axis=-1) ## (nb,nv,ne,1) I2