8c6fc7864a1e09d2d5789d7b0424290a120b7848
jpmeneses <jpmeneses@uc.cl>
2023-09-11 12:10:28 +1000

    Submit TE-aug-v101: Second version with adjustable field strength (3.0T)

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v100-TEaug
-#SBATCH --output=out_TEaug_100.txt
+#SBATCH --job-name=v101-TEaug
+#SBATCH --output=out_TEaug_101.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-IDEAL-TEaug.py --dataset 'TEaug-100' --field 1.5 --out_vars 'PM' --epochs 200 --epoch_ckpt 20
+python train-IDEAL-TEaug.py --dataset 'TEaug-101' --field 3.0 --out_vars 'PM' --epochs 200 --epoch_ckpt 20
