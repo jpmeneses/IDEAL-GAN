1e455899066c4d3a6ab21458f30271ecab30103a
jpmeneses <jpmeneses@uc.cl>
2023-09-11 10:47:07 +1000

    Submit TE-aug-v100: First version with adjustable field strength (1.5T)

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v228-GAN
-#SBATCH --output=out_GAN_228.txt
+#SBATCH --job-name=v100-TEaug
+#SBATCH --output=out_TEaug_100.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-IDEAL-GAN.py --dataset 'GAN-228' --adv_train False --n_G_filters 36 --n_downsamplings 4 --n_res_blocks 2 --encoded_size 24 --epochs 10 --epoch_decay 10 --epoch_ckpt 5 --data_aug_p 0.0 --perceptual_loss True --cycle_loss_weight 1e-1 --B2A2B_weight 1e0 --ls_reg_weight 3e-7 --Fourier_reg_weight 0.0 --NL_SelfAttention True
-python train-IDEAL-GAN.py --dataset 'GAN-228' --adv_train True --n_G_filters 36 --n_downsamplings 4 --n_res_blocks 2 --encoded_size 24 --epochs 100 --epoch_decay 100 --epoch_ckpt 20 --D_lr_factor 4 --data_aug_p 0.0 --critic_train_steps 2 --perceptual_loss True --cycle_loss_weight 1e-1 --B2A2B_weight 1e0 --ls_reg_weight 3e-7 --Fourier_reg_weight 0.0 --NL_SelfAttention True
+python train-IDEAL-TEaug.py --dataset 'TEaug-100' --field 1.5 --out_vars 'PM' --epochs 200 --epoch_ckpt 20
