4396bf651851aa4fde2a8156b834c3b619d0b698
jpmeneses <jpmeneses@uc.cl>
2023-09-11 15:44:37 +1000

    Submit VAE-GAN(v229): Same as v228, but slightly decreased KL-reg

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-#SBATCH --job-name=v101-TEaug
-#SBATCH --output=out_TEaug_101.txt
+#SBATCH --job-name=v229-GAN
+#SBATCH --output=out_TEaug_229.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-IDEAL-TEaug.py --dataset 'TEaug-101' --field 3.0 --out_vars 'PM' --epochs 200 --epoch_ckpt 20
+python train-IDEAL-GAN.py --dataset 'GAN-229' --adv_train False --n_G_filters 36 --n_downsamplings 4 --n_res_blocks 2 --encoded_size 24 --epochs 10 --epoch_decay 10 --epoch_ckpt 5 --data_aug_p 0.0 --perceptual_loss True --cycle_loss_weight 1e-1 --B2A2B_weight 1e0 --ls_reg_weight 4e-7 --Fourier_reg_weight 0.0 --NL_SelfAttention True
+python train-IDEAL-GAN.py --dataset 'GAN-229' --adv_train True --n_G_filters 36 --n_downsamplings 4 --n_res_blocks 2 --encoded_size 24 --epochs 100 --epoch_decay 100 --epoch_ckpt 20 --data_aug_p 0.0 --perceptual_loss True --cycle_loss_weight 1e-1 --B2A2B_weight 1e0 --ls_reg_weight 4e-7 --Fourier_reg_weight 0.0 --NL_SelfAttention True
