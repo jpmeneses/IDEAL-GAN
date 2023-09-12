aed36e28f2c46c3f776b513b472da0625ab2f3d5
jpmeneses <jpmeneses@uc.cl>
2023-09-12 10:40:53 +1000

    Submit TE-aug-v103: Field strength 3.0T and variable TEs with non-fixed echo-time shifting

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v102-TEaug
-#SBATCH --output=out_TEaug_102.txt
+#SBATCH --job-name=v103-TEaug
+#SBATCH --output=out_TEaug_103.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-IDEAL-TEaug.py --dataset 'TEaug-102' --field 1.5 --out_vars 'PM' --epochs 100 --epoch_ckpt 20
+python train-IDEAL-TEaug.py --dataset 'TEaug-103' --field 3.0 --out_vars 'PM' --epochs 100 --epoch_ckpt 20
