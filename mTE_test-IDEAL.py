9a3058e379f9a01837070b9d5bfeff5055fc5c86
jpmeneses <jpmeneses@uc.cl>
2023-09-12 10:33:11 +1000

    Enable MEBCRN data format

--- mTE_test-IDEAL.py
+++ mTE_test-IDEAL.py
@@ -2,9 +2,9 @@ import numpy as np
 import tensorflow as tf
 import tensorflow.keras as keras
 
+import tf2lib as tl
 import DLlib as dl
 import pylib as py
-import tf2lib as tl
 import wflib as wf
 import data
 
@@ -19,7 +19,7 @@ from skimage.metrics import structural_similarity
 # ==============================================================================
 
 py.arg('--experiment_dir',default='output/WF-IDEAL')
-py.arg('--dataset', type=str, default='multiTE', choices=['multiTE','phantom'])
+py.arg('--dataset', type=str, default='multiTE', choices=['multiTE','phantom_1p5','phantom_3p0'])
 py.arg('--te_input', type=bool, default=True)
 py.arg('--UQ', type=bool, default=False)
 py.arg('--n_plot',type=int,default=50)
@@ -68,16 +68,14 @@ r2_sc,fm_sc = 200.0,300.0
 dataset_dir = '../../OneDrive - Universidad Católica de Chile/Documents/datasets/'
 dataset_hdf5 = args.dataset + '_GC_192_complex_2D.hdf5'
 testX, testY, TEs =  data.load_hdf5(dataset_dir, dataset_hdf5, ech_idx,
-                                    acqs_data=True, te_data=True,
-                                    complex_data=(args.G_model=='complex'),
-                                    MEBCRN=(args.G_model=='MEBCRN'))
+                                    acqs_data=True, te_data=True, MEBCRN=True)
 
 ################################################################################
 ########################### DATASET PARTITIONS #################################
 ################################################################################
 
 # Overall dataset statistics
-len_dataset,hgt,wdt,n_out = np.shape(testY)
+len_dataset,n_out,hgt,wdt,n_ch = np.shape(testY)
 
 print('Acquisition Dimensions:', hgt,wdt)
 print('Echoes:',args.n_echoes)
@@ -86,6 +84,7 @@ print('Output Maps:',n_out)
 # Input and output dimensions (testing data)
 print('Testing input shape:',testX.shape)
 print('Testing output shape:',testY.shape)
+print('TEs shape:',TEs.shape)
 
 A_B_dataset_test = tf.data.Dataset.from_tensor_slices((testX,TEs,testY))
 A_B_dataset_test.batch(1)
@@ -101,11 +100,10 @@ if args.G_model == 'multi-decod' or args.G_model == 'encod-decod':
                                 R2_self_attention=args.D2_SelfAttention,
                                 FM_self_attention=args.D3_SelfAttention)
     else:
-        G_A2B = dl.PM_Generator(input_shape=(hgt,wdt,ech_idx),
+        G_A2B = dl.PM_Generator(input_shape=(args.n_echoes,hgt,wdt,n_ch),
                                 te_input=args.te_input,
                                 te_shape=(args.n_echoes,),
                                 filters=args.n_G_filters,
-                                dropout=0.0,
                                 R2_self_attention=args.D1_SelfAttention,
                                 FM_self_attention=args.D2_SelfAttention)
 
@@ -145,117 +143,98 @@ tl.Checkpoint(dict(G_A2B=G_A2B), py.join(args.experiment_dir, 'checkpoints')).re
 
 @tf.function
 def sample(A, B, TE=None):
-    indx_B = tf.concat([tf.zeros_like(B[:,:,:,:4],dtype=tf.int32),
-                        tf.ones_like(B[:,:,:,4:],dtype=tf.int32)],axis=-1)
-    indx_B_abs = tf.concat([tf.zeros_like(B[:,:,:,:2],dtype=tf.int32),
-                            tf.ones_like(B[:,:,:,4:],dtype=tf.int32)],axis=-1)
-    indx_PM =tf.concat([tf.zeros_like(B[:,:,:,:1],dtype=tf.int32),
-                        tf.ones_like(B[:,:,:,:1],dtype=tf.int32)],axis=-1)
     # Split B
-    B_WF,B_PM = tf.dynamic_partition(B,indx_B,num_partitions=2)
-    B_WF = tf.reshape(B_WF,B[:,:,:,:4].shape)
-    B_PM = tf.reshape(B_PM,B[:,:,:,4:].shape)
-    # Magnitude of water/fat images
-    B_WF_real = B_WF[:,:,:,0::2]
-    B_WF_imag = B_WF[:,:,:,1::2]
-    B_WF_abs = tf.abs(tf.complex(B_WF_real,B_WF_imag))
-    # Split B param maps
-    B_R2, B_FM = tf.dynamic_partition(B_PM,indx_PM,num_partitions=2)
-    B_R2 = tf.reshape(B_R2,B[:,:,:,:1].shape)
-    B_FM = tf.reshape(B_FM,B[:,:,:,:1].shape)
+    B_WF = B[:,:2,:,:,:]
+    B_PM = B[:,2:,:,:,:]
+    B_WF_abs = tf.math.sqrt(tf.reduce_sum(tf.square(B_WF),axis=-1,keepdims=True))
     # Estimate A2B
     if args.out_vars == 'WF':
         if args.te_input:
             A2B_WF_abs = G_A2B([A,TE], training=True)
         else:
             A2B_WF_abs = G_A2B(A, training=True)
-        A2B_WF_abs = tf.where(A[:,:,:,:2]!=0.0,A2B_WF_abs,0.0)
+        A2B_WF_abs = tf.where(B_WF_abs!=0.0,A2B_WF_abs,0.0)
         A2B_PM = tf.zeros_like(B_PM)
-        # Split A2B param maps
-        A2B_R2, A2B_FM = tf.dynamic_partition(A2B_PM,indx_PM,num_partitions=2)
-        A2B_R2 = tf.reshape(A2B_R2,B[:,:,:,:1].shape)
-        A2B_FM = tf.reshape(A2B_FM,B[:,:,:,:1].shape)
-        A2B_abs = tf.concat([A2B_WF_abs,A2B_PM],axis=-1)
+        A2B_R2 = A2B_PM[:,:,:,:,1:]
+        A2B_FM = A2B_PM[:,:,:,:,:1]
+        A2B = tf.concat([A2B_WF,A2B_PM],axis=1)
         A2B_var = None
-    elif args.out_vars == 'WFc':
-        if args.te_input:
-            A2B_WF = G_A2B([A,te], training=True)
-        else:
-            A2B_WF = G_A2B(A, training=True)
-        A2B_WF = tf.where(B[:,:,:,:4]!=0.0,A2B_WF,0.0)
-        # Magnitude of water/fat images
-        A2B_WF_real = A2B_WF[:,:,:,0::2]
-        A2B_WF_imag = A2B_WF[:,:,:,1::2]
-        A2B_WF_abs = tf.abs(tf.complex(A2B_WF_real,A2B_WF_imag))
-        # Compute zero-valued param maps
-        A2B_PM = tf.zeros_like(A2B_WF_abs)
-        A2B_abs = tf.concat([A2B_WF_abs,A2B_PM],axis=-1)
-        A2B_var = None
+    # elif args.out_vars == 'WFc':
+    #     if args.te_input:
+    #         A2B_WF = G_A2B([A,te], training=True)
+    #     else:
+    #         A2B_WF = G_A2B(A, training=True)
+    #     A2B_WF = tf.where(B[:,:,:,:4]!=0.0,A2B_WF,0.0)
+    #     # Magnitude of water/fat images
+    #     A2B_WF_real = A2B_WF[:,:,:,0::2]
+    #     A2B_WF_imag = A2B_WF[:,:,:,1::2]
+    #     A2B_WF_abs = tf.abs(tf.complex(A2B_WF_real,A2B_WF_imag))
+    #     # Compute zero-valued param maps
+    #     A2B_PM = tf.zeros_like(A2B_WF_abs)
+    #     A2B_abs = tf.concat([A2B_WF_abs,A2B_PM],axis=-1)
+    #     A2B_var = None
     elif args.out_vars == 'PM':
         if args.te_input:
             A2B_PM = G_A2B([A,TE], training=False)
         else:
             A2B_PM = G_A2B(A, training=False)
-        A2B_PM = tf.where(A[:,:,:,:2]!=0.0,A2B_PM,0.0)
-        A2B_R2, A2B_FM = tf.dynamic_partition(A2B_PM,indx_PM,num_partitions=2)
-        A2B_R2 = tf.reshape(A2B_R2,B[:,:,:,:1].shape)
-        A2B_FM = tf.reshape(A2B_FM,B[:,:,:,:1].shape)
+        A2B_PM = tf.where(A[:,:1,:,:,:]!=0.0,A2B_PM,0.0)
+        A2B_R2 = A2B_PM[:,:,:,:,1:]
+        A2B_FM = A2B_PM[:,:,:,:,:1]
         if args.G_model=='U-Net' or args.G_model=='MEBCRN':
             A2B_FM = (A2B_FM - 0.5) * 2
-            A2B_FM = tf.where(B_PM[:,:,:,1:]!=0.0,A2B_FM,0.0)
-            A2B_PM = tf.concat([A2B_R2,A2B_FM],axis=-1)
-        A2B_WF = wf.get_rho(A,A2B_PM,TE)
-        A2B_WF_real = A2B_WF[:,:,:,0::2]
-        A2B_WF_imag = A2B_WF[:,:,:,1::2]
-        A2B_WF_abs = tf.abs(tf.complex(A2B_WF_real,A2B_WF_imag))
-        A2B_abs = tf.concat([A2B_WF_abs,A2B_PM],axis=-1)
+            A2B_FM = tf.where(A[:,:1,:,:,:1]!=0.0,A2B_FM,0.0)
+            A2B_PM = tf.concat([A2B_R2,A2B_FM],axis=1)
+        A2B_WF = wf.get_rho(A, A2B_PM, field=args.field, te=TE)
+        A2B_WF_abs = tf.math.sqrt(tf.reduce_sum(tf.square(A2B_WF),axis=-1,keepdims=True))
+        A2B = tf.concat([A2B_WF,A2B_PM],axis=1)
         A2B_var = None
-    elif args.out_vars == 'WF-PM':
-        B_abs = tf.concat([B_WF_abs,B_PM],axis=-1)
-        if args.te_input:
-            A2B_abs = G_A2B([A,TE], training=True)
-        else:
-            A2B_abs = G_A2B(A, training=True)
-        A2B_abs = tf.where(B_abs!=0.0,A2B_abs,0.0)
-        A2B_WF_abs,A2B_PM = tf.dynamic_partition(A2B_abs,indx_B_abs,num_partitions=2)
-        A2B_WF_abs = tf.reshape(A2B_WF_abs,B[:,:,:,:2].shape)
-        A2B_PM = tf.reshape(A2B_PM,B[:,:,:,4:].shape)
-        A2B_R2, A2B_FM = tf.dynamic_partition(A2B_PM,indx_PM,num_partitions=2)
-        A2B_R2 = tf.reshape(A2B_R2,B[:,:,:,:1].shape)
-        A2B_FM = tf.reshape(A2B_FM,B[:,:,:,:1].shape)
-        if args.G_model=='U-Net' or args.G_model=='MEBCRN':
-            A2B_FM = (A2B_FM - 0.5) * 2
-            A2B_FM = tf.where(B_PM[:,:,:,:1]!=0.0,A2B_FM,0.0)
-            A2B_abs = tf.concat([A2B_WF_abs,A2B_R2,A2B_FM],axis=-1)
-        A2B_var = None
-    elif args.out_vars == 'FM':
-        if args.UQ:
-            _, A2B_FM, A2B_var = G_A2B(A, training=False)
-        else:
-            A2B_FM = G_A2B(A, training=False)
-            A2B_var = None
+    # elif args.out_vars == 'WF-PM':
+    #     B_abs = tf.concat([B_WF_abs,B_PM],axis=-1)
+    #     if args.te_input:
+    #         A2B_abs = G_A2B([A,TE], training=True)
+    #     else:
+    #         A2B_abs = G_A2B(A, training=True)
+    #     A2B_abs = tf.where(B_abs!=0.0,A2B_abs,0.0)
+    #     A2B_WF_abs,A2B_PM = tf.dynamic_partition(A2B_abs,indx_B_abs,num_partitions=2)
+    #     A2B_WF_abs = tf.reshape(A2B_WF_abs,B[:,:,:,:2].shape)
+    #     A2B_PM = tf.reshape(A2B_PM,B[:,:,:,4:].shape)
+    #     A2B_R2, A2B_FM = tf.dynamic_partition(A2B_PM,indx_PM,num_partitions=2)
+    #     A2B_R2 = tf.reshape(A2B_R2,B[:,:,:,:1].shape)
+    #     A2B_FM = tf.reshape(A2B_FM,B[:,:,:,:1].shape)
+    #     if args.G_model=='U-Net' or args.G_model=='MEBCRN':
+    #         A2B_FM = (A2B_FM - 0.5) * 2
+    #         A2B_FM = tf.where(B_PM[:,:,:,:1]!=0.0,A2B_FM,0.0)
+    #         A2B_abs = tf.concat([A2B_WF_abs,A2B_R2,A2B_FM],axis=-1)
+    #     A2B_var = None
+    # elif args.out_vars == 'FM':
+    #     if args.UQ:
+    #         _, A2B_FM, A2B_var = G_A2B(A, training=False)
+    #     else:
+    #         A2B_FM = G_A2B(A, training=False)
+    #         A2B_var = None
         
-        # A2B Masks
-        A2B_FM = tf.where(A[:,:,:,:1]!=0.0,A2B_FM,0.0)
-        if args.UQ:
-            A2B_var = tf.where(A[:,:,:,:1]!=0.0,A2B_var,0.0)
+    #     # A2B Masks
+    #     A2B_FM = tf.where(A[:,:,:,:1]!=0.0,A2B_FM,0.0)
+    #     if args.UQ:
+    #         A2B_var = tf.where(A[:,:,:,:1]!=0.0,A2B_var,0.0)
 
-        # Build A2B_PM array with zero-valued R2*
-        A2B_PM = tf.concat([tf.zeros_like(A2B_FM),A2B_FM], axis=-1)
-        if args.fat_char:
-            A2B_P, A2B2A = fa.acq_to_acq(A,A2B_PM,complex_data=(args.G_model=='complex'))
-            A2B_WF = A2B_P[:,:,:,0:4]
-        else:
-            A2B_WF, A2B2A = wf.acq_to_acq(A,A2B_PM,te=TE,complex_data=(args.G_model=='complex'))
-        A2B = tf.concat([A2B_WF,A2B_PM],axis=-1)
+    #     # Build A2B_PM array with zero-valued R2*
+    #     A2B_PM = tf.concat([tf.zeros_like(A2B_FM),A2B_FM], axis=-1)
+    #     if args.fat_char:
+    #         A2B_P, A2B2A = fa.acq_to_acq(A,A2B_PM,complex_data=(args.G_model=='complex'))
+    #         A2B_WF = A2B_P[:,:,:,0:4]
+    #     else:
+    #         A2B_WF, A2B2A = wf.acq_to_acq(A,A2B_PM,te=TE,complex_data=(args.G_model=='complex'))
+    #     A2B = tf.concat([A2B_WF,A2B_PM],axis=-1)
 
-        # Magnitude of water/fat images
-        A2B_WF_real = A2B_WF[:,:,:,0::2]
-        A2B_WF_imag = A2B_WF[:,:,:,1::2]
-        A2B_WF_abs = tf.abs(tf.complex(A2B_WF_real,A2B_WF_imag))
-        A2B_abs = tf.concat([A2B_WF_abs,A2B_PM],axis=-1)
+    #     # Magnitude of water/fat images
+    #     A2B_WF_real = A2B_WF[:,:,:,0::2]
+    #     A2B_WF_imag = A2B_WF[:,:,:,1::2]
+    #     A2B_WF_abs = tf.abs(tf.complex(A2B_WF_real,A2B_WF_imag))
+    #     A2B_abs = tf.concat([A2B_WF_abs,A2B_PM],axis=-1)
 
-    return A2B_abs, A2B_var
+    return A2B, A2B_var
 
 
 # run
@@ -267,30 +246,35 @@ for A, TE_smp, B in tqdm.tqdm(A_B_dataset_test, desc='Testing Samples Loop', tot
     A = tf.expand_dims(A,axis=0)
     TE_smp = tf.expand_dims(TE_smp,axis=0)
     B = tf.expand_dims(B,axis=0)
-    A2B, A2B_var = sample(A,B,TE_smp)
+    B_WF = B[:,:2,:,:,:]
+    B_WF_abs = tf.math.sqrt(tf.reduce_sum(tf.square(B_WF),axis=-1,keepdims=True))
+    
+    A2B, A2B_var = sample(A, B, TE_smp)
+    A2B_WF = A2B[:,:2,:,:,:]
+    A2B_WF_abs = tf.math.sqrt(tf.reduce_sum(tf.square(A2B_WF),axis=-1,keepdims=True))
 
-    w_aux = np.squeeze(A2B[:,:,:,0])
-    f_aux = np.squeeze(A2B[:,:,:,1])
+    w_aux = np.squeeze(A2B_WF_abs[:,0,:,:,:])
+    f_aux = np.squeeze(A2B_WF_abs[:,1,:,:,:])
     if not(args.UQ):
-        r2_aux = np.squeeze(A2B[:,:,:,2])*r2_sc
+        r2_aux = np.squeeze(A2B[:,2,:,:,1])*r2_sc
         lmax = r2_sc
         cmap = 'copper'
     else:
         r2_aux = np.squeeze(A2B_var)*fm_sc
         lmax = 30
         cmap = 'gnuplot2'
-    field_aux = np.squeeze(A2B[:,:,:,3])
+    field_aux = np.squeeze(A2B[:,2,:,:,0])
     PDFF_aux = f_aux/(w_aux+f_aux)
     PDFF_aux[np.isnan(PDFF_aux)] = 0.0
 
-    wn_aux = np.squeeze(np.abs(tf.complex(B[:,:,:,0],B[:,:,:,1])))
-    fn_aux = np.squeeze(np.abs(tf.complex(B[:,:,:,2],B[:,:,:,3])))
-    r2n_aux = np.squeeze(B[:,:,:,4])
-    fieldn_aux = np.squeeze(B[:,:,:,5])
+    wn_aux = np.squeeze(B_WF_abs[:,0,:,:,:])
+    fn_aux = np.squeeze(B_WF_abs[:,1,:,:,:])
+    r2n_aux = np.squeeze(B[:,2,:,:,1])
+    fieldn_aux = np.squeeze(B[:,2,:,:,0])
     PDFFn_aux = fn_aux/(wn_aux+fn_aux)
     PDFFn_aux[np.isnan(PDFFn_aux)] = 0.0
     
-    if i%args.n_plot == 0 or args.dataset == 'phantom':
+    if i%args.n_plot == 0 or args.dataset != 'multiTE':
         fig,axs=plt.subplots(figsize=(16, 9), nrows=2, ncols=3)
 
         # A2B maps in the first row
@@ -325,7 +309,7 @@ for A, TE_smp, B in tqdm.tqdm(A_B_dataset_test, desc='Testing Samples Loop', tot
         fig.colorbar(field_unet, ax=axs[1,2])
         axs[1,2].axis('off')
 
-        fig.suptitle('TE1/dTE: '+str([TE_smp[0,0].numpy(),np.mean(np.diff(TE_smp))]), fontsize=18)
+        fig.suptitle('TE1/dTE: '+str([TE_smp[0,0,0].numpy(),np.mean(np.diff(TE_smp,axis=1))]), fontsize=18)
 
         # plt.show()
         plt.subplots_adjust(top=1,bottom=0,right=1,left=0,hspace=0.1,wspace=0)
@@ -348,8 +332,8 @@ for A, TE_smp, B in tqdm.tqdm(A_B_dataset_test, desc='Testing Samples Loop', tot
     ws_MSE.write(i+1,3,MSE_r2)
     ws_MSE.write(i+1,4,MSE_fm)
     
-    ws_MSE.write(i+1,5,TE_smp[0,0].numpy())
-    ws_MSE.write(i+1,6,np.mean(np.diff(TE_smp)))
+    ws_MSE.write(i+1,5,TE_smp[0,0,0].numpy())
+    ws_MSE.write(i+1,6,np.mean(np.diff(TE_smp,axis=1)))
 
     # MAE
     MAE_w = np.mean(tf.abs(w_aux-wn_aux), axis=(0,1))
