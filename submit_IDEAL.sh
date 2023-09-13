142cf836d4fa0f6aff7587251303ef200a23784c
jpmeneses <jpmeneses@uc.cl>
2023-09-13 21:54:12 +1000

    Submit TE-aug-v105: Field strength 3.0T, NO random FM scaling, and Ada-IN layers across the overall architecture

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v104-TEaug
-#SBATCH --output=out_TEaug_104.txt
+#SBATCH --job-name=v105-TEaug
+#SBATCH --output=out_TEaug_105.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-IDEAL-TEaug.py --dataset 'TEaug-104' --out_vars 'PM' --epochs 100 --epoch_ckpt 20
+python train-IDEAL-TEaug.py --dataset 'TEaug-105' --field 3.0 --out_vars 'PM' --epochs 100 --epoch_ckpt 20
