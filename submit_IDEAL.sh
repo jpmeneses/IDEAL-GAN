4fe494d8b8806c57b7b368dcfa1b007ac846cce2
jpmeneses <jpmeneses@uc.cl>
2023-09-14 08:52:17 +1000

    Modify VAE-GAN(v231): Edit loss weights - Increase adversarial weight

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -7,4 +7,4 @@
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-IDEAL-GAN.py --dataset 'GAN-231' --adv_train True --cGAN True --n_G_filters 36 --n_downsamplings 4 --n_res_blocks 2 --n_D_filters 72 --encoded_size 24 --epochs 100 --epoch_decay 100 --epoch_ckpt 20 --D_lr_factor 2 --data_aug_p 0.0 --perceptual_loss True --cycle_loss_weight 1e-1 --B2A2B_weight 1e0 --ls_reg_weight 4e-7 --Fourier_reg_weight 0.0 --NL_SelfAttention True
+python train-IDEAL-GAN.py --dataset 'GAN-231' --adv_train True --cGAN True --n_G_filters 36 --n_downsamplings 4 --n_res_blocks 2 --n_D_filters 72 --encoded_size 24 --epochs 100 --epoch_decay 100 --epoch_ckpt 20 --D_lr_factor 2 --data_aug_p 0.0 --perceptual_loss True --cycle_loss_weight 2e-2 --B2A2B_weight 2e0 --ls_reg_weight 1e-7 --Fourier_reg_weight 0.0 --NL_SelfAttention True
