3f93e2537973a4e22b4c0f0784c22fe9201cb94e
jpmeneses <jpmeneses@uc.cl>
2023-09-18 11:14:39 +1000

    Submit TE-aug-v106: Field strength 3.0T, random FM scaling with mean=2, and Ada-IN layers across the overall architecture

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v235-GAN
-#SBATCH --output=out_GAN_235.txt
+#SBATCH --job-name=v106-TEaug
+#SBATCH --output=out_TEaug_106.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-IDEAL-GAN.py --dataset 'GAN-235' --adv_train True --cGAN True --n_G_filters 36 --n_downsamplings 4 --n_res_blocks 2 --n_D_filters 72 --encoded_size 24 --epochs 100 --epoch_decay 100 --epoch_ckpt 20 --data_aug_p 0.0 --perceptual_loss True --cycle_loss_weight 2e-2 --B2A2B_weight 2e0 --ls_reg_weight 2e-7 --Fourier_reg_weight 0.0 --NL_SelfAttention False
+python train-IDEAL-TEaug.py --dataset 'TEaug-106' --field 3.0 --out_vars 'PM' --epochs 100 --epoch_ckpt 20
