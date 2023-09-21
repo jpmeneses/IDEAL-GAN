b7e72d446fae18169ab97d418d0fe7e661b10db0
jpmeneses <jpmeneses@uc.cl>
2023-09-21 17:00:41 +1000

    Submit Sup-v101: Train conventional U-Net to quantify WF, using modified data partitions

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v100-sup
-#SBATCH --output=out_sup_100.txt
+#SBATCH --job-name=v101-sup
+#SBATCH --output=out_sup_101.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-sup.py --dataset 'Sup-100' --out_vars 'PM' --epoch_ckpt 20
+python train-sup.py --dataset 'Sup-101' --out_vars 'WF' --G_model 'U-Net' --epoch_ckpt 20
