1be40b02de26784e42aae8e31f1329faf83e6bd3
jpmeneses <jpmeneses@uc.cl>
2023-09-22 10:49:55 +1000

    Submit Sup-v105: Train 3-echoes MDWF-Net, using modified data partitions

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v104-sup
-#SBATCH --output=out_sup_104.txt
+#SBATCH --job-name=v105-sup
+#SBATCH --output=out_sup_105.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-sup.py --dataset 'Sup-104' --n_echoes 3 --out_vars 'WF' --G_model 'U-Net' --epoch_ckpt 20
+python train-sup.py --dataset 'Sup-105' --n_echoes 3 --out_vars 'WF-PM' --epoch_ckpt 20 --D2_SelfAttention False
