97269413b245d000fe98d266ddc6f875a85baa1a
jpmeneses <jpmeneses@uc.cl>
2023-09-25 19:15:58 +1000

    Submit TE-aug-v107: Field strength 1.5T, synthetic training data, enabled FM augmentation

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v107-TEaug
-#SBATCH --output=out_TEaug_107.txt
+#SBATCH --job-name=v108-TEaug
+#SBATCH --output=out_TEaug_108.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-IDEAL-TEaug.py --dataset 'TEaug-107' --DL_gen True --out_vars 'PM' --epochs 100 --epoch_ckpt 20
+python train-IDEAL-TEaug.py --dataset 'TEaug-108' --DL_gen True --out_vars 'PM' --epochs 100 --epoch_ckpt 20 --FM_aug True
