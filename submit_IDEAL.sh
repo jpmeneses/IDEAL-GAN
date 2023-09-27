424daa3128a3b59482051ada40fc40fe4fa85931
jpmeneses <jpmeneses@uc.cl>
2023-09-27 10:11:03 +1000

    Submit again VAE-GAN(v242): VAE-GAN with magnitude+phase perceptual loss

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -7,4 +7,4 @@
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-IDEAL-GAN.py --dataset 'GAN-242' --adv_train True --cGAN True --n_G_filters 36 --n_downsamplings 4 --n_res_blocks 2 --n_groups_PM 2 --VQ_encoder True --n_D_filters 72 --encoded_size 24 --epochs 100 --epoch_decay 100 --epoch_ckpt 20 --data_aug_p 0.0 --perceptual_loss True --cycle_loss_weight 1e-2 --B2A2B_weight 1e1 --Fourier_reg_weight 0.0 --NL_SelfAttention True
+python train-IDEAL-GAN.py --dataset 'GAN-242' --adv_train True --cGAN True --n_G_filters 36 --n_downsamplings 4 --n_res_blocks 2 --n_groups_PM 2 --n_D_filters 72 --encoded_size 24 --epochs 100 --epoch_decay 100 --epoch_ckpt 20 --data_aug_p 0.0 --perceptual_loss True --cycle_loss_weight 1e-2 --B2A2B_weight 1e1 --ls_reg_weight 1e-7 --Fourier_reg_weight 0.0 --NL_SelfAttention True
