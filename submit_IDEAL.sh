2598a87183f01454e50e7e8cc707371020d4103a
jpmeneses <jpmeneses@uc.cl>
2023-10-02 10:36:53 +1100

    Submit VAE-GAN(v243): No perceptual loss - Only adversarial and B-losses, and KL regularization

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v300-GAN
-#SBATCH --output=out_GAN_300.txt
+#SBATCH --job-name=v243-GAN
+#SBATCH --output=out_GAN_243.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-IDEAL-GAN.py --dataset 'GAN-300' --adv_train True --cGAN True --n_G_filters 36 --n_downsamplings 4 --n_res_blocks 2 --n_groups_PM 2 --VQ_encoder True --n_D_filters 72 --encoded_size 24 --epochs 100 --epoch_decay 100 --epoch_ckpt 20 --data_aug_p 0.0 --perceptual_loss True --cycle_loss_weight 1e-2 --B2A2B_weight 1e1 --Fourier_reg_weight 0.0 --NL_SelfAttention True
+python train-IDEAL-GAN.py --dataset 'GAN-243' --encoded_size 24 --adv_train True --cGAN True --main_loss 'MAE' --perceptual_loss False --A_loss_weight 0.0 --ls_reg_weight 1e-7
