27fb4aa0c74f677a08c28ba8502af029de9cc929
jpmeneses <jpmeneses@uc.cl>
2023-10-02 10:47:28 +1100

    Submit VAE-GAN(v244): Same as reference v238, but without decoder's activations

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v243-GAN
-#SBATCH --output=out_GAN_243.txt
+#SBATCH --job-name=v244-GAN
+#SBATCH --output=out_GAN_244.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-IDEAL-GAN.py --dataset 'GAN-243' --encoded_size 24 --adv_train True --cGAN True --main_loss 'MAE' --perceptual_loss False --A_loss_weight 0.0 --ls_reg_weight 1e-7
+python train-IDEAL-GAN.py --dataset 'GAN-244' --encoded_size 24 --adv_train True --cGAN True
