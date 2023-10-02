05e5167b94a436dcc4bcc812f8539586797ffd20
jpmeneses <jpmeneses@uc.cl>
2023-10-03 09:06:04 +1100

    Submit VQ-VAE(v301): Original perceptual loss, decrease num. of down-samplings

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v243-GAN
-#SBATCH --output=out_GAN_243.txt
+#SBATCH --job-name=v302-GAN
+#SBATCH --output=out_GAN_302.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-IDEAL-GAN.py --dataset 'GAN-243' --encoded_size 24 --adv_train True --cGAN True --main_loss 'MAE' --perceptual_loss False --A_loss_weight 0.0 --B_loss_weight 0.05 --ls_reg_weight 1e-7
+python train-IDEAL-GAN.py --dataset 'GAN-302' --n_downsamplings 3 --n_res_blocks 3 --n_groups_PM 2 --encoded_size 24 --VQ_encoder True --adv_train True --cGAN True --epochs 50 --epoch_decay 50 --epoch_ckpt 10
