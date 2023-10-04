ff5eae7570e25947ebb91670867950384632b6ab
jpmeneses <jpmeneses@uc.cl>
2023-10-04 11:40:31 +1100

    Submit VAE-GAN(v245): No perceptual loss, replace by Fourier A-loss

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v303-GAN
-#SBATCH --output=out_GAN_303.txt
+#SBATCH --job-name=v245-GAN
+#SBATCH --output=out_GAN_245.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-IDEAL-GAN.py --dataset 'GAN-303' --n_downsamplings 3 --n_res_blocks 3 --n_groups_PM 2 --encoded_size 6 --VQ_encoder True --adv_train True --cGAN True --epochs 50 --epoch_decay 50 --epoch_ckpt 10
+python train-IDEAL-GAN.py --dataset 'GAN-245' --encoded_size 24 --adv_train True --cGAN True --perceptual_loss False --A_loss_weight 0.0 --ls_reg_weight 1e-7 --Fourier_reg_weight 1e-4
