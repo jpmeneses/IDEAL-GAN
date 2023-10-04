806fe9fb3a6e2c3e60d889d4d7b3757274b7a725
jpmeneses <jpmeneses@uc.cl>
2023-10-04 12:06:50 +1100

    Submit VAE-GAN(v246): Standard losses (v238), but apply FM re-scaling data augmentation

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v245-GAN
-#SBATCH --output=out_GAN_245.txt
+#SBATCH --job-name=v246-GAN
+#SBATCH --output=out_GAN_246.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-IDEAL-GAN.py --dataset 'GAN-245' --encoded_size 24 --adv_train True --cGAN True --perceptual_loss False --A_loss_weight 0.0 --ls_reg_weight 1e-7 --Fourier_reg_weight 1e-4
+python train-IDEAL-GAN.py --dataset 'GAN-246' --encoded_size 24 --adv_train True --cGAN True --data_aug_p 0.4 --perceptual_loss True
