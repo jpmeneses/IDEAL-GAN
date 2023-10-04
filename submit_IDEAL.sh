e57da3c14025af68ce9b8ebda10ee96d6d43dee1
jpmeneses <jpmeneses@uc.cl>
2023-10-05 10:02:42 +1100

    Submit VQ-DDPM(v303)

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v246-GAN
-#SBATCH --output=out_GAN_246.txt
+#SBATCH --job-name=v303-DDPM
+#SBATCH --output=out_DDPM_303.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-IDEAL-GAN.py --dataset 'GAN-246' --encoded_size 24 --adv_train True --cGAN True --perceptual_loss True --A_loss_weight 3e-3 --B_loss_weight 0.3
+python train-ldm.py --experiment_dir 'GAN-303' --batch_size 8 --epochs_ldm 200 --lr 1e-4
