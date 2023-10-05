5af84d67327e8f80b4e2638e24481e2772928ce9
jpmeneses <jpmeneses@uc.cl>
2023-10-05 14:45:00 +1100

    Try 2D-Net with 384x384 data

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v303-DDPM
-#SBATCH --output=out_DDPM_303.txt
+#SBATCH --job-name=v999-sup
+#SBATCH --output=out_sup_999.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-ldm.py --experiment_dir 'GAN-303' --batch_size 8 --epochs_ldm 200 --lr 1e-4
+python train-sup.py --dataset 'Sup-999' --out_vars 'PM' --n_G_filters 36 --batch_size 1 --lr 0.0001
