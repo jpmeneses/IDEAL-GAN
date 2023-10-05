0cfb4f25603cbb7d55caaadcbef4ea854425b875
jpmeneses <jpmeneses@uc.cl>
2023-10-05 16:39:19 +1100

    Submit VAE-DDPM(v238)

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v999-sup
-#SBATCH --output=out_sup_999.txt
+#SBATCH --job-name=v238-DDPM
+#SBATCH --output=out_DDPM_238.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-sup.py --dataset 'Sup-999' --out_vars 'PM' --n_G_filters 36 --batch_size 1 --epochs 20 --epoch_decay 20 --lr 0.0001
+python train-ldm.py --experiment_dir 'GAN-238' --batch_size 32 --epochs_ldm 200 --lr 5e-4
