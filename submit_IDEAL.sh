5c0a5241f776c5c415ba4b476613551d93ac91f5
jpmeneses <jpmeneses@uc.cl>
2023-10-08 10:50:58 +1100

    Submit VAE-GAN(v248): Same as v247, but slightly decreased B-loss weighting

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v247-GAN
-#SBATCH --output=out_GAN_247.txt
+#SBATCH --job-name=v248-GAN
+#SBATCH --output=out_GAN_248.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-IDEAL-GAN.py --dataset 'GAN-247' --encoded_size 24 --adv_train True --cGAN True --A_loss 'sinGAN'
+python train-IDEAL-GAN.py --dataset 'GAN-248' --encoded_size 24 --adv_train True --cGAN True --A_loss 'sinGAN' --B_loss_weight 0.05
