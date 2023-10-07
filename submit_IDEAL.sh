08e9854e3632ff41c89e6e6a26cd138bc23482ca
jpmeneses <jpmeneses@uc.cl>
2023-10-07 23:36:18 +1100

    Submit VAE-GAN(v247): Try task-specific perceptual loss

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v000-sGAN
-#SBATCH --output=out_sGAN_000.txt
+#SBATCH --job-name=v247-GAN
+#SBATCH --output=out_GAN_247.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-singleGAN.py --K_sc 3
-python train-singleGAN.py --K_sc 2
-python train-singleGAN.py --K_sc 1
-python train-singleGAN.py --K_sc 0
+python train-IDEAL-GAN.py --dataset 'GAN-247' --encoded_size 24 --adv_train True --cGAN True --A_loss 'sinGAN'
