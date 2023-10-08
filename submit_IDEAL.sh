8a4c3e65ed1e681d9342327fd60889230f51b21b
jpmeneses <jpmeneses@uc.cl>
2023-10-09 09:45:33 +1100

    Submit VAE-GAN(v249): Change B-loss to MAE and slightly decrease its weighting

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v248-GAN
-#SBATCH --output=out_GAN_248.txt
+#SBATCH --job-name=v249-GAN
+#SBATCH --output=out_GAN_249.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-IDEAL-GAN.py --dataset 'GAN-248' --encoded_size 24 --adv_train True --cGAN True --A_loss 'sinGAN' --B_loss_weight 0.05
+python train-IDEAL-GAN.py --dataset 'GAN-249' --encoded_size 24 --adv_train True --cGAN True --main_loss 'MAE' --A_loss 'sinGAN' --B_loss_weight 0.01
