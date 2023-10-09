61cd24a9a90c65ad85fed91d86705fef8a52a8b9
jpmeneses <jpmeneses@uc.cl>
2023-10-09 11:50:09 +1100

    Submit TE-aug(v109): Run VET-Net with VAE-GAN-based data augmentation

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v249-GAN
-#SBATCH --output=out_GAN_249.txt
+#SBATCH --job-name=v109-TEaug
+#SBATCH --output=out_TEaug_109.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-IDEAL-GAN.py --dataset 'GAN-249' --encoded_size 24 --adv_train True --cGAN True --main_loss 'MAE' --A_loss 'sinGAN' --B_loss_weight 0.01
+python train-IDEAL-TEaug.py --dataset 'TEaug-109' --DL_aug True --out_vars 'PM' --epochs 100 --epoch_ckpt 20
