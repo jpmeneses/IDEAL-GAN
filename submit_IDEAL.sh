97c46b1628fa154585067b91ee56ef8b03dfdb53
jpmeneses <jpmeneses@uc.cl>
2023-10-09 14:35:43 +1100

    Submit unmasked singleGAN for task-specific perceptual loss

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-#SBATCH --job-name=v251-GAN
-#SBATCH --output=out_GAN_251.txt
+#SBATCH --job-name=v001-sGAN
+#SBATCH --output=out_sGAN_001.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-IDEAL-GAN.py --dataset 'GAN-251' --encoded_size 24 --adv_train True --cGAN True --B_loss_weight 0.05 --FM_loss_weight 2.0
+python train-singleGAN.py --dataset 'sinGAN-wMsk' --K_sc 3
+python train-singleGAN.py --dataset 'sinGAN-wMsk' --K_sc 2
+python train-singleGAN.py --dataset 'sinGAN-wMsk' --K_sc 1
+python train-singleGAN.py --dataset 'sinGAN-wMsk' --K_sc 0
