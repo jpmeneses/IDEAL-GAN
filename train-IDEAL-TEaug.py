ed14a748e6deeeb40569c6a13582be519b67dd5d
jpmeneses <jpmeneses@uc.cl>
2023-10-09 11:49:40 +1100

    Enable VAE-GAN-based data augmentation using both encoder and decoder blocks

--- train-IDEAL-TEaug.py
+++ train-IDEAL-TEaug.py
@@ -22,6 +22,7 @@ from itertools import cycle
 
 py.arg('--dataset', default='WF-IDEAL')
 py.arg('--DL_gen', type=bool, default=False)
+py.arg('--DL_aug', type=bool, default=False)
 py.arg('--DL_experiment_dir', default='output/GAN-238')
 py.arg('--n_per_epoch', type=int, default=10000)
 py.arg('--n_echoes', type=int, default=6)
@@ -71,25 +72,25 @@ r2_sc,fm_sc = 200.0,300.0
 ################################################################################
 dataset_dir = '../datasets/'
 dataset_hdf5_1 = 'JGalgani_GC_192_complex_2D.hdf5'
-out_maps_1 = data.load_hdf5(dataset_dir, dataset_hdf5_1, ech_idx, 
-                            acqs_data=False, te_data=False, MEBCRN=True)
+acqs_1, out_maps_1 = data.load_hdf5(dataset_dir, dataset_hdf5_1, ech_idx, 
+                            acqs_data=True, te_data=False, MEBCRN=True)
 
 if not(args.DL_gen):
     dataset_hdf5_2 = 'INTA_GC_192_complex_2D.hdf5'
-    out_maps_2 = data.load_hdf5(dataset_dir,dataset_hdf5_2, ech_idx,
-                                acqs_data=False, te_data=False, MEBCRN=True)
+    acqs_2, out_maps_2 = data.load_hdf5(dataset_dir,dataset_hdf5_2, ech_idx,
+                                acqs_data=True, te_data=False, MEBCRN=True)
 
     dataset_hdf5_3 = 'INTArest_GC_192_complex_2D.hdf5'
-    out_maps_3 = data.load_hdf5(dataset_dir,dataset_hdf5_3, ech_idx,
-                                acqs_data=False, te_data=False, MEBCRN=True)
+    acqs_3, out_maps_3 = data.load_hdf5(dataset_dir,dataset_hdf5_3, ech_idx,
+                                acqs_data=True, te_data=False, MEBCRN=True)
 
     dataset_hdf5_4 = 'Volunteers_GC_192_complex_2D.hdf5'
-    out_maps_4 = data.load_hdf5(dataset_dir,dataset_hdf5_4, ech_idx,
-                                acqs_data=False, te_data=False, MEBCRN=True)
+    acqs_4, out_maps_4 = data.load_hdf5(dataset_dir,dataset_hdf5_4, ech_idx,
+                                acqs_data=True, te_data=False, MEBCRN=True)
 
     dataset_hdf5_5 = 'Attilio_GC_192_complex_2D.hdf5'
-    out_maps_5 = data.load_hdf5(dataset_dir,dataset_hdf5_5, ech_idx,
-                                acqs_data=False, te_data=False, MEBCRN=True)
+    acqs_5, out_maps_5 = data.load_hdf5(dataset_dir,dataset_hdf5_5, ech_idx,
+                                acqs_data=True, te_data=False, MEBCRN=True)
 
 ################################################################################
 ########################### DATASET PARTITIONS #################################
@@ -100,9 +101,11 @@ if not(args.DL_gen):
 # n4_div = 434
 
 if args.DL_gen:
-    trainY = np.zeros((args.n_per_epoch,1,1,1,1),dtype=np.float32)
+    trainX = trainY = np.zeros((args.n_per_epoch,1,1,1,1),dtype=np.float32)
 else:
+    trainX = np.concatenate((acqs_2,acqs_3,acqs_4,acqs_5),axis=0)
     trainY  = np.concatenate((out_maps_2,out_maps_3,out_maps_4,out_maps_5),axis=0)
+valX    = acqs_1
 valY    = out_maps_1
 
 # Overall dataset statistics
@@ -116,10 +119,10 @@ print('Echoes:',echoes)
 # Input and output dimensions (validations data)
 print('Output shape:',valY.shape)
 
-B_dataset = tf.data.Dataset.from_tensor_slices(trainY)
-B_dataset = B_dataset.batch(args.batch_size).shuffle(len(trainY))
-B_dataset_val = tf.data.Dataset.from_tensor_slices(valY)
-B_dataset_val.batch(1)
+A_B_dataset = tf.data.Dataset.from_tensor_slices((trainX,trainY))
+A_B_dataset = A_B_dataset.batch(args.batch_size).shuffle(len(trainY))
+A_B_dataset_val = tf.data.Dataset.from_tensor_slices((valX,valY))
+A_B_dataset_val.batch(1)
 
 # ==============================================================================
 # =                                   models                                   =
@@ -171,7 +174,15 @@ elif args.G_model == 'MEBCRN':
 else:
     raise(NameError('Unrecognized Generator Architecture'))
 
-if args.DL_gen:
+if args.DL_gen or args.DL_aug:
+    enc= dl.encoder(input_shape=(DL_args.n_echoes,hgt,wdt,n_ch),
+                    encoded_dims=DL_args.encoded_size,
+                    filters=DL_args.n_G_filters,
+                    num_layers=DL_args.n_downsamplings,
+                    num_res_blocks=DL_args.n_res_blocks,
+                    kl_reg=False,
+                    NL_self_attention=DL_args.NL_SelfAttention
+                    )
     dec_w =  dl.decoder(encoded_dims=DL_args.encoded_size,
                         output_2D_shape=(hgt,wdt),
                         filters=DL_args.n_G_filters,
@@ -194,7 +205,7 @@ if args.DL_gen:
                         num_res_blocks=DL_args.n_res_blocks,
                         NL_self_attention=DL_args.NL_SelfAttention
                         )
-    tl.Checkpoint(dict(dec_w=dec_w, dec_f=dec_f, dec_xi=dec_xi), py.join(args.DL_experiment_dir, 'checkpoints')).restore()
+    tl.Checkpoint(dict(enc=enc,dec_w=dec_w,dec_f=dec_f,dec_xi=dec_xi), py.join(args.DL_experiment_dir, 'checkpoints')).restore()
 
 IDEAL_op = wf.IDEAL_Layer(field=args.field)
 
@@ -208,11 +219,20 @@ G_optimizer = keras.optimizers.Adam(learning_rate=G_lr_scheduler, beta_1=args.be
 # ==============================================================================
 
 @tf.function
-def train_G(B, te=None):
+def train_G(A, B, te=None):
     with tf.GradientTape() as t:
         ##################### B Cycle #####################
-        B2A = IDEAL_op(B, te=te, training=False)
-        B2A = keras.layers.GaussianNoise(stddev=0.1)(B2A)
+        if not(args.DL_aug):
+            B2A = IDEAL_op(B, te=te, training=False)
+            B2A = keras.layers.GaussianNoise(stddev=0.1)(B2A)
+        else:
+            A2Z = enc(A, training=False)
+            A2Z = keras.layers.GaussianNoise(stddev=0.05)(A2Z)
+            A2Z2B_w = dec_w(A2Z, training=False)
+            A2Z2B_f = dec_f(A2Z, training=False)
+            A2Z2B_xi= dec_xi(A2Z, training=False)
+            B = tf.concat([A2Z2B_w,A2Z2B_f,A2Z2B_xi],axis=1)
+
         B_WF_abs = tf.math.sqrt(tf.reduce_sum(tf.square(B[:,:2,:,:,:]),axis=-1,keepdims=True))
         B_PM = B[:,2:,:,:,:]
 
@@ -337,13 +357,21 @@ def train_G(B, te=None):
             'L1_FM': FM_L1}
 
 
-def train_step(B, te=None):
-    G_loss_dict = train_G(B, te)
+def train_step(A, B, te=None):
+    G_loss_dict = train_G(A, B, te)
     return G_loss_dict
 
 
 @tf.function
-def sample(B, te=None):
+def sample(A, B, te=None):
+    if args.DL_aug:
+        A2Z = enc(A, training=False)
+        A2Z = keras.layers.GaussianNoise(stddev=0.05)(A2Z)
+        A2Z2B_w = dec_w(A2Z, training=False)
+        A2Z2B_f = dec_f(A2Z, training=False)
+        A2Z2B_xi= dec_xi(A2Z, training=False)
+        B = tf.concat([A2Z2B_w,A2Z2B_f,A2Z2B_xi],axis=1)
+
     # Split B
     B_WF_abs = tf.math.sqrt(tf.reduce_sum(tf.square(B[:,:2,:,:,:]),axis=-1,keepdims=True))
     B_PM = B[:,2:,:,:,:]
@@ -427,8 +455,8 @@ def sample(B, te=None):
                         'R2_loss': R2_loss,
                         'FM_loss': FM_loss}
 
-def validation_step(B, TE):
-    B2A, B2A2B_abs, val_B2A2B_dict = sample(B, TE)
+def validation_step(A, B, TE):
+    B2A, B2A2B_abs, val_B2A2B_dict = sample(A, B, TE)
     return B2A, B2A2B_abs, val_B2A2B_dict
 
 if args.DL_gen:
@@ -464,7 +492,7 @@ train_summary_writer = tf.summary.create_file_writer(py.join(output_dir, 'summar
 val_summary_writer = tf.summary.create_file_writer(py.join(output_dir, 'summaries', 'validation'))
 
 # sample
-val_iter = cycle(B_dataset_val)
+val_iter = cycle(A_B_dataset_val)
 sample_dir = py.join(output_dir, 'samples_training')
 py.mkdir(sample_dir)
 n_div = np.ceil(total_steps/len(valY))
@@ -478,7 +506,7 @@ for ep in range(args.epochs):
     ep_cnt.assign_add(1)
 
     # train for an epoch
-    for B in B_dataset:
+    for A, B in A_B_dataset:
         if args.DL_gen:
             hls = hgt//(2**(DL_args.n_downsamplings))
             wls = wdt//(2**(DL_args.n_downsamplings))
@@ -490,17 +518,22 @@ for ep in range(args.epochs):
         # ==============================================================================
         p = np.random.rand()
         if p <= 0.4:
+            A = tf.reshape(tf.transpose(A,perm=[0,2,3,1,4]),[A.shape[0],hgt,wdt,args.n_echoes*n_ch])
             B = tf.reshape(tf.transpose(B,perm=[0,2,3,1,4]),[B.shape[0],hgt,wdt,n_out*n_ch])
 
             # Random 90 deg rotations
+            A = tf.image.rot90(A,k=np.random.randint(3))
             B = tf.image.rot90(B,k=np.random.randint(3))
 
             # Random horizontal reflections
+            A = tf.image.random_flip_left_right(A)
             B = tf.image.random_flip_left_right(B)
 
             # Random vertical reflections
+            A = tf.image.random_flip_up_down(A)
             B = tf.image.random_flip_up_down(B)
 
+            A = tf.transpose(tf.reshape(A,[A.shape[0],hgt,wdt,args.n_echoes,n_ch]),[0,3,1,2,4])
             B = tf.transpose(tf.reshape(B,[B.shape[0],hgt,wdt,n_out,n_ch]),[0,3,1,2,4])
 
             # Random off-resonance field-map scaling factor
@@ -519,7 +552,7 @@ for ep in range(args.epochs):
         else:
             te_var = wf.gen_TEvar(args.n_echoes, bs=B.shape[0])
 
-        G_loss_dict = train_step(B, te=te_var)
+        G_loss_dict = train_step(A, B, te=te_var)
 
         # # summary
         with train_summary_writer.as_default():
@@ -529,7 +562,9 @@ for ep in range(args.epochs):
 
         # sample
         if (G_optimizer.iterations.numpy() % n_div == 0) or (G_optimizer.iterations.numpy() < 200):
+            A = next(val_iter)
             B = next(val_iter)
+            A = tf.expand_dims(A, axis=0)
             B = tf.expand_dims(B, axis=0)
             B_WF = B[:,:2,:,:,:]
             B_WF_abs = tf.math.sqrt(tf.reduce_sum(tf.square(B_WF),axis=-1,keepdims=True))
@@ -539,7 +574,7 @@ for ep in range(args.epochs):
             else:
                 TE_valid = wf.gen_TEvar(args.n_echoes, 1)
             
-            B2A, B2A2B, val_A2B_dict = validation_step(B, TE_valid)
+            B2A, B2A2B, val_A2B_dict = validation_step(A, B, TE_valid)
             B2A2B_WF = B2A2B[:,:2,:,:,:]
             B2A2B_WF_abs = tf.math.sqrt(tf.reduce_sum(tf.square(B2A2B_WF),axis=-1,keepdims=True))
 
