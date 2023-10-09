b9cd19da1fab1a27f0ac69b6d657d49df8522c15
jpmeneses <jpmeneses@uc.cl>
2023-10-09 16:03:01 +1100

    Merge wDisc and wGen architectures into sGAN

--- DLlib/module.py
+++ DLlib/module.py
@@ -178,38 +178,26 @@ def PatchGAN(input_shape,
         return keras.Model(inputs=inputs, outputs=h)
 
 
-def WGen(input_shape,
+def sGAN(input_shape,
+        gen_mode=False,
         num_filters=32,
-        num_layers=5):
+        num_layers=5,
+        kernel_size=3
+        ):
     x = inputs = keras.Input(shape=input_shape)
+    h = list()
     for _ in range(num_layers-1):
         x = keras.layers.Conv2D(num_filters, 3, padding='same', kernel_initializer='he_normal')(x)
         x = keras.layers.BatchNormalization()(x)
         x = tf.nn.leaky_relu(x)
+        h.append(x)
     x = keras.layers.Conv2D(input_shape[-1], 3, padding='same')(x)
+    h.append(x)
     output = keras.layers.add([inputs,x])
-    return keras.Model(inputs=inputs, outputs=output)
-
-
-def WDisc(input_shape,
-        n_out_ch=1,
-        num_filters=32,
-        num_layers=5,
-        kernel_size=3,
-        ):
-    h = inputs = keras.Input(shape=input_shape)
-    h = keras.layers.Conv2D(num_filters, kernel_size, kernel_initializer='he_normal')(h)
-    h = keras.layers.BatchNormalization()(h)
-    x_h = h = tf.nn.leaky_relu(h)
-    for _ in range(num_layers-2):
-        num_filters /= 2
-        h = keras.layers.Conv2D(num_filters, kernel_size, kernel_initializer='he_normal')(h)
-        h = keras.layers.BatchNormalization()(h)
-        h = tf.nn.leaky_relu(h)
-    x_t = keras.layers.Conv2D(num_filters, kernel_size, kernel_initializer='he_normal')(h)
-    x_t = keras.layers.BatchNormalization()(x_t)
-    x_t = tf.nn.leaky_relu(x_t)
-    return keras.Model(inputs=inputs, outputs=[x_h, h, x_t])
+    if gen_mode:
+        return keras.Model(inputs=inputs, outputs=output)
+    else:
+        return keras.Model(inputs=inputs, outputs=h)
 
 
 # ==============================================================================
