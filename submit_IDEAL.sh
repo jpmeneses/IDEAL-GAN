d5ebdc9cb6449b998e93b2b28a4e36e4a2c66e38
jpmeneses <jpmeneses@uc.cl>
2023-10-11 09:48:14 +1100

    Submit VAE-GAN(v252): Same as v250, but discard finest-level task-specific perceptual discriminator

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v250-GAN
-#SBATCH --output=out_GAN_250.txt
+#SBATCH --job-name=v252-GAN
+#SBATCH --output=out_GAN_252.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-IDEAL-GAN.py --dataset 'GAN-250' --encoded_size 24 --adv_train True --cGAN True --A_loss 'sinGAN' --B_loss_weight 0.02 --FM_loss_weight 2.0
+python train-IDEAL-GAN.py --dataset 'GAN-252' --encoded_size 24 --adv_train True --cGAN True --A_loss 'sinGAN' --B_loss_weight 0.02 --FM_loss_weight 2.0
