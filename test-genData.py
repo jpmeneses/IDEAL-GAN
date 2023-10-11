6e06262ed31f0ef535f01de4f64e99d4f0e3aabc
jpmeneses <jpmeneses@uc.cl>
2023-10-12 09:36:59 +1100

    Add adjustable VQ num. of embedding

--- test-genData.py
+++ test-genData.py
@@ -63,7 +63,7 @@ dec_xi = dl.decoder(encoded_dims=args.encoded_size,
                     )
 
 IDEAL_op = wf.IDEAL_Layer()
-vq_op = dl.VectorQuantizer(args.encoded_size,256,0.5)
+vq_op = dl.VectorQuantizer(args.encoded_size,args.VQ_num_embed,args.VQ_commit_cost)
 
 tl.Checkpoint(dict(dec_w=dec_w, dec_f=dec_f, dec_xi=dec_xi, vq_op=vq_op), py.join(args.experiment_dir, 'checkpoints')).restore()
 
@@ -99,7 +99,7 @@ TE = wf.gen_TEvar(args.n_echoes,orig=False)
 
 for k in range(args.n_samples):
 	if args.VQ_encoder:
-		Z = tf.random.uniform(z_shape[:-1],minval=0,maxval=255,seed=0,dtype=tf.int32)
+		Z = tf.random.uniform(z_shape[:-1],minval=0,maxval=args.VQ_num_embed,seed=0,dtype=tf.int32)
 	else:
 		Z = tf.random.normal(z_shape,seed=0,dtype=tf.float32)
 	Z2B, Z2B2A = sample(Z)
