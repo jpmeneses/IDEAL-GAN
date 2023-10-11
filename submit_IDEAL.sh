b4818af54fa2988744044c1c68e9b686ac243bef
jpmeneses <jpmeneses@uc.cl>
2023-10-11 11:08:36 +1100

    Submit unmasked WF-sep singleGAN

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-#SBATCH --job-name=v252-GAN
-#SBATCH --output=out_GAN_252.txt
+#SBATCH --job-name=v002-sGAN
+#SBATCH --output=out_sGAN_002.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-IDEAL-GAN.py --dataset 'GAN-252' --encoded_size 24 --adv_train True --cGAN True --A_loss 'sinGAN' --B_loss_weight 0.02 --FM_loss_weight 2.0
+python train-singleGAN.py --dataset 'sinGAN-WF' --K_sc 3
+python train-singleGAN.py --dataset 'sinGAN-WF' --K_sc 2
+python train-singleGAN.py --dataset 'sinGAN-WF' --K_sc 1
+python train-singleGAN.py --dataset 'sinGAN-WF' --K_sc 0
