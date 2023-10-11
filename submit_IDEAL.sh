7b540a33b016537ae7672a7ea4692abb97c7a325
jpmeneses <jpmeneses@uc.cl>
2023-10-11 14:28:04 +1100

    Submit VQ-VAE(v303): Significantly decrease embedding dimensions

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v252-GAN
-#SBATCH --output=out_GAN_252.txt
+#SBATCH --job-name=v304-GAN
+#SBATCH --output=out_GAN_304.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-IDEAL-GAN.py --dataset 'GAN-252' --encoded_size 24 --adv_train True --cGAN True --A_loss 'sinGAN' --B_loss_weight 0.02 --FM_loss_weight 2.0
+python train-IDEAL-GAN.py --dataset 'GAN-304' --n_downsamplings 3 --n_res_blocks 3 --encoded_size 6 --VQ_encoder True --VQ_num_embed 32 --adv_train True --cGAN True --epochs 50 --epoch_decay 50 --epoch_ckpt 10
