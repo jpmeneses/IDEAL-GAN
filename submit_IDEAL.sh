f3278dda588cbabdc6076484adeea8f6f6e56c92
jpmeneses <jpmeneses@uc.cl>
2023-10-13 09:12:33 +1100

    Submit VQ-VAE(v306): Same as v306, but decrease encoded dims

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v253-GAN
-#SBATCH --output=out_GAN_253.txt
+#SBATCH --job-name=v306-GAN
+#SBATCH --output=out_GAN_306.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-IDEAL-GAN.py --dataset 'GAN-253' --encoded_size 24 --adv_train True --cGAN True --A_loss 'sinGAN' --B_loss_weight 0.005 --FM_loss_weight 2.0
+python train-IDEAL-GAN.py --dataset 'GAN-306' --n_downsamplings 3 --n_res_blocks 3 --encoded_size 2 --VQ_encoder True --VQ_num_embed 32 --adv_train True --cGAN True --epochs 80 --epoch_decay 80 --epoch_ckpt 20 --ls_reg_weight 1e1
