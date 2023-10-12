896571190ea4883d36355fd0917b831d261f787b
jpmeneses <jpmeneses@uc.cl>
2023-10-13 09:38:37 +1100

    Submit VAE-GAN(v253): MSE-based task-specific perceptual loss

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v306-GAN
-#SBATCH --output=out_GAN_306.txt
+#SBATCH --job-name=v254-GAN
+#SBATCH --output=out_GAN_254.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-IDEAL-GAN.py --dataset 'GAN-306' --n_downsamplings 3 --n_res_blocks 3 --encoded_size 2 --VQ_encoder True --VQ_num_embed 32 --adv_train True --cGAN True --epochs 80 --epoch_decay 80 --epoch_ckpt 20 --ls_reg_weight 1e1
+python train-IDEAL-GAN.py --dataset 'GAN-254' --encoded_size 24 --adv_train True --cGAN True --A_loss 'sinGAN' --A_loss_weight 0.1 --B_loss_weight 0.005 --FM_loss_weight 2.0
