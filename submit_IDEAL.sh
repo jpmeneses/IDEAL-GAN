93831f6cb9f75d14cba82d70b5379a71bc94f6bc
jpmeneses <jpmeneses@uc.cl>
2023-10-12 12:24:32 +1100

    Submit WF-sep singleGAN with 64 filters at discriminator

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-#SBATCH --job-name=v305-GAN
-#SBATCH --output=out_GAN_305.txt
+#SBATCH --job-name=v003-sGAN
+#SBATCH --output=out_sGAN_003.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-IDEAL-GAN.py --dataset 'GAN-305' --n_downsamplings 3 --n_res_blocks 3 --encoded_size 6 --VQ_encoder True --VQ_num_embed 32 --adv_train True --cGAN True --epochs 50 --epoch_decay 50 --epoch_ckpt 10 --ls_reg_weight 1e1
+python train-singleGAN.py --dataset 'sinGAN-WF-64f' --K_sc 3
+python train-singleGAN.py --dataset 'sinGAN-WF-64f' --K_sc 2
+python train-singleGAN.py --dataset 'sinGAN-WF-64f' --K_sc 1
+python train-singleGAN.py --dataset 'sinGAN-WF-64f' --K_sc 0
