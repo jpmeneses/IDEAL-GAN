84f13d0fd6dd466ad6efb53a7a26a6cfb33e4cc0
jpmeneses <jpmeneses@uc.cl>
2023-10-15 22:14:07 +1100

    Re-submit VQ-VAE(v307): Relax LS-regularization (but still larger than v306)

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -7,4 +7,4 @@
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-IDEAL-GAN.py --dataset 'GAN-307' --n_downsamplings 3 --n_res_blocks 3 --encoded_size 2 --VQ_encoder True --VQ_num_embed 32 --adv_train True --cGAN True --epochs 80 --epoch_decay 80 --epoch_ckpt 20 --ls_reg_weight 1e2
+python train-IDEAL-GAN.py --dataset 'GAN-307' --n_downsamplings 3 --n_res_blocks 3 --encoded_size 2 --VQ_encoder True --VQ_num_embed 32 --adv_train True --cGAN True --epochs 80 --epoch_decay 80 --epoch_ckpt 20 --ls_reg_weight 2e1
