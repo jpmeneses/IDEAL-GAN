8851e8b95f7059970f21bbbe69354f90764131ae
jpmeneses <jpmeneses@uc.cl>
2023-10-15 22:25:51 +1100

    Re-submit VQ-VAE(v306): Train for 20 more epochs

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v307-GAN
-#SBATCH --output=out_GAN_307.txt
+#SBATCH --job-name=v306-GAN
+#SBATCH --output=out_GAN_306.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-IDEAL-GAN.py --dataset 'GAN-307' --n_downsamplings 3 --n_res_blocks 3 --encoded_size 2 --VQ_encoder True --VQ_num_embed 32 --adv_train True --cGAN True --epochs 80 --epoch_decay 80 --epoch_ckpt 20 --ls_reg_weight 2e1
+python train-IDEAL-GAN.py --dataset 'GAN-306' --n_downsamplings 3 --n_res_blocks 3 --encoded_size 2 --VQ_encoder True --VQ_num_embed 32 --adv_train True --cGAN True --epochs 100 --epoch_decay 100 --epoch_ckpt 20 --ls_reg_weight 1e1
