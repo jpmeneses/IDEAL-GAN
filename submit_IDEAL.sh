a78f9a33a7ed06c535df25b652067010d8ddefbf
jpmeneses <jpmeneses@uc.cl>
2023-10-15 22:38:19 +1100

    Submit VQ-VAE(v308): Try with smaller latent space

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v306-GAN
-#SBATCH --output=out_GAN_306.txt
+#SBATCH --job-name=v308-GAN
+#SBATCH --output=out_GAN_308.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-IDEAL-GAN.py --dataset 'GAN-306' --n_downsamplings 3 --n_res_blocks 3 --encoded_size 2 --VQ_encoder True --VQ_num_embed 32 --adv_train True --cGAN True --epochs 100 --epoch_decay 100 --epoch_ckpt 20 --ls_reg_weight 1e1
+python train-IDEAL-GAN.py --dataset 'GAN-308' --encoded_size 6 --VQ_encoder True --VQ_num_embed 32 --adv_train True --cGAN True --epochs 80 --epoch_decay 80 --epoch_ckpt 20 --ls_reg_weight 1e1
