387595e228138059f69935f860dd82e45f49d671
jpmeneses <jpmeneses@uc.cl>
2023-10-16 13:19:34 +1100

    Submit VAE-GAN(v255): Using standard loss and weights, try Bayesian output layer at PM-decoder

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v308-GAN
-#SBATCH --output=out_GAN_308.txt
+#SBATCH --job-name=v255-GAN
+#SBATCH --output=out_GAN_255.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-IDEAL-GAN.py --dataset 'GAN-308' --encoded_size 6 --VQ_encoder True --VQ_num_embed 32 --adv_train True --cGAN True --epochs 80 --epoch_decay 80 --epoch_ckpt 20 --ls_reg_weight 1e1
+python train-IDEAL-GAN.py --dataset 'GAN-255' --PM_bayes_layer True --encoded_size 24 --adv_train True --cGAN True --B_loss_weight 0.05 --FM_loss_weight 2.0
