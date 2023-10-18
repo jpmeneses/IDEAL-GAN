9c12844aab9147fe13cafdb0a6ad22d814eb98bc
jpmeneses <jpmeneses@uc.cl>
2023-10-19 09:34:23 +1100

    Submit Sup-202: U-Net trained with 384x384 data

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v201-sup
-#SBATCH --output=out_sup_201.txt
+#SBATCH --job-name=v202-sup
+#SBATCH --output=out_sup_202.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-sup.py --dataset Sup-201 --n_echoes 3 --out_vars 'PM' --n_G_filters 36 --batch_size 1 --epoch_ckpt 20 --lr 0.0001
+python train-sup.py --dataset Sup-202 --out_vars 'WF' --G_model 'U-Net' --n_G_filters 36 --batch_size 1 --epoch_ckpt 20 --lr 0.0001
