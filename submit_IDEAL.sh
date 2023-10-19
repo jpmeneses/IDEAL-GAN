d034fb1beb93caea9094b29a62e1e0263d249102
jpmeneses <jpmeneses@uc.cl>
2023-10-19 15:48:43 +1100

    Submit VAE-GAN(v255): Using standard loss and weights, try new stochastic output layer at PM-decoder

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v202-sup
-#SBATCH --output=out_sup_202.txt
+#SBATCH --job-name=v257-GAN
+#SBATCH --output=out_GAN_257.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-sup.py --dataset Sup-202 --out_vars 'WF' --G_model 'U-Net' --n_G_filters 36 --batch_size 1 --epoch_ckpt 20 --lr 0.0001
+python train-IDEAL-GAN.py --dataset 'GAN-257' --PM_bayes_layer True --encoded_size 24 --adv_train True --cGAN True --B_loss_weight 0.05 --FM_loss_weight 2.0
