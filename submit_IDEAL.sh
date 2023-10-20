eb80a28c187deb9af45bc702df38fca2d498203d
jpmeneses <jpmeneses@uc.cl>
2023-10-20 16:15:42 +1100

    Re-submit VAE-000: Return to original KL-reg weight and reduce LS-dims to 24

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -7,4 +7,4 @@
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-IDEAL-VAE.py --dataset VAE-000 --ls_reg_weight 1e-8
+python train-IDEAL-VAE.py --dataset VAE-000 --encoded_size 24
