e358789c44ff6a712e7e026b01560bab3fc3327d
jpmeneses <jpmeneses@uc.cl>
2023-10-23 08:58:20 +1100

    Submit Sup-205: 3-echoes MDWF-Net trained with 384x384 data

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v004-VAE
-#SBATCH --output=out_VAE_004.txt
+#SBATCH --job-name=v205-sup
+#SBATCH --output=out_sup_205.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-IDEAL-VAE.py --dataset VAE-004 --encoded_size 24 --A_loss_weight 1e-6
+python train-sup.py --dataset Sup-205 --n_echoes 3 --out_vars 'WF-PM' --n_G_filters 36 --batch_size 1 --epoch_ckpt 20 --lr 0.0001
