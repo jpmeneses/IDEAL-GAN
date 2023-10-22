4c7cdd4453af455a1fa88ea0a1c35b4e8fc75878
jpmeneses <jpmeneses@uc.cl>
2023-10-22 20:26:07 +1100

    Submit VAE-002: Original setting, no adversarial loss, and MSE-based perceptual loss

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v001-VAE
-#SBATCH --output=out_VAE_001.txt
+#SBATCH --job-name=v002-VAE
+#SBATCH --output=out_VAE_002.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-IDEAL-VAE.py --dataset VAE-001 --encoded_size 24 --adv_train True
+python train-IDEAL-VAE.py --dataset VAE-002 --encoded_size 24 --A_loss_weight 0.1
