ebe0b48f3e78654e2448fcdd66fbb8309ab4c36f
jpmeneses <jpmeneses@uc.cl>
2023-10-22 22:28:19 +1100

    Re-submit VAE-002: Increase A-loss weight

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v003-VAE
-#SBATCH --output=out_VAE_003.txt
+#SBATCH --job-name=v002-VAE
+#SBATCH --output=out_VAE_002.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-IDEAL-VAE.py --dataset VAE-003 --encoded_size 24 --A_loss 'sinGAN' --A_loss_weight 0.1
+python train-IDEAL-VAE.py --dataset VAE-002 --encoded_size 24 --A_loss_weight 0.0001
