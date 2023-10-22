ba8f5d2fde1d4869b0a864c613307cc68b64c685
jpmeneses <jpmeneses@uc.cl>
2023-10-23 08:24:06 +1100

    Submit VAE-004: MSE A-loss with highly decreased weighting

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v003-VAE
-#SBATCH --output=out_VAE_003.txt
+#SBATCH --job-name=v004-VAE
+#SBATCH --output=out_VAE_004.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-IDEAL-VAE.py --dataset VAE-003 --encoded_size 24 --A_loss_weight 0.01
+python train-IDEAL-VAE.py --dataset VAE-004 --encoded_size 24 --A_loss_weight 1e-6
