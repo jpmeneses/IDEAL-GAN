f5abaaf002bbfead3912aa73eb48f50bf61d809d
jpmeneses <jpmeneses@uc.cl>
2023-10-23 21:27:05 +1100

    Re-submit VAE-GAN(v258): Strongly decrease A-loss weight

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v005-VAE
-#SBATCH --output=out_VAE_005.txt
+#SBATCH --job-name=v258-GAN
+#SBATCH --output=out_GAN_258.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-IDEAL-VAE.py --dataset VAE-005 --encoded_size 24 --A_loss_weight 1e-7
+python train-IDEAL-GAN.py --dataset 'GAN-258' --PM_bayes_layer True --encoded_size 24 --adv_train True --cGAN True --A_loss_weight 1e-7 --B_loss_weight 0.05 --FM_loss_weight 1.0
