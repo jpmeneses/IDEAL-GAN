82e742a12152880ccdf918137f9244cf2c51344c
jpmeneses <jpmeneses@uc.cl>
2023-10-23 14:54:18 +1100

    Submit KL-LDM (v238)

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v205-sup
-#SBATCH --output=out_sup_205.txt
+#SBATCH --job-name=v238-DDPM
+#SBATCH --output=out_DDPM_238.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-sup.py --dataset Sup-205 --n_echoes 3 --out_vars 'WF-PM' --n_G_filters 36 --batch_size 1 --epoch_ckpt 20 --lr 0.0001
+python train-ldm.py --experiment_dir 'GAN-238' --n_timesteps 1000 --batch_size 8 --epochs_ldm 200 --lr 1e-4
