e3a34889f34ceaf3d9ace86d4bc2c17c4fb502d7
jpmeneses <jpmeneses@uc.cl>
2023-10-23 15:09:00 +1100

    Adjust imshow scales for validation samples

--- train-ldm.py
+++ train-ldm.py
@@ -322,25 +322,25 @@ for ep in range(args.epochs_ldm):
     # A2B maps in the second row
     w_aux = np.squeeze(np.abs(tf.complex(Z2B[:,0,:,:,0],Z2B[:,0,:,:,1])))
     W_ok =  axs[1,1].imshow(w_aux, cmap='bone',
-                            interpolation='none', vmin=0, vmax=1)
+                            interpolation='none', vmin=0, vmax=10)
     fig.colorbar(W_ok, ax=axs[1,1])
     axs[1,1].axis('off')
 
     f_aux = np.squeeze(np.abs(tf.complex(Z2B[:,1,:,:,0],Z2B[:,1,:,:,1])))
     F_ok =  axs[1,2].imshow(f_aux, cmap='pink',
-                            interpolation='none', vmin=0, vmax=1)
+                            interpolation='none', vmin=0, vmax=10)
     fig.colorbar(F_ok, ax=axs[1,2])
     axs[1,2].axis('off')
 
     r2_aux = np.squeeze(Z2B[:,2,:,:,1])
     r2_ok = axs[1,3].imshow(r2_aux*r2_sc, cmap='copper',
-                            interpolation='none', vmin=0, vmax=fm_sc)
+                            interpolation='none', vmin=0, vmax=10*fm_sc)
     fig.colorbar(r2_ok, ax=axs[1,3])
     axs[1,3].axis('off')
 
     field_aux = np.squeeze(Z2B[:,2,:,:,0])
     field_ok =  axs[1,4].imshow(field_aux*fm_sc, cmap='twilight',
-                                interpolation='none', vmin=-fm_sc/2, vmax=fm_sc/2)
+                                interpolation='none', vmin=-5*fm_sc, vmax=5*fm_sc)
     fig.colorbar(field_ok, ax=axs[1,4])
     axs[1,4].axis('off')
     fig.delaxes(axs[1,0])
