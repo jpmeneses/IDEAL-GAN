e0ef12166f3c1d77ae8d9b791558d2bac2f932d7
jpmeneses <jpmeneses@uc.cl>
2023-10-24 13:23:41 +1100

    Re-submit VAE-GAN(v258):

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v006-VAE
-#SBATCH --output=out_VAE_006.txt
+#SBATCH --job-name=v258-GAN
+#SBATCH --output=out_GAN_258.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-IDEAL-VAE.py --dataset VAE-006 --encoded_size 24 --adv_train True --A_loss_weight 1e-8 --ls_reg_weight 2e-7
+python train-IDEAL-GAN.py --dataset 'GAN-258' --PM_bayes_layer True --encoded_size 24 --adv_train True --cGAN True --A_loss_weight 1e-7 --B_loss_weight 0.05 --FM_loss_weight 1.0
