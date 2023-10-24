761ffe82e7892f3d86e5a1942e4f7bd3c00fd8e7
jpmeneses <jpmeneses@uc.cl>
2023-10-24 13:13:02 +1100

    Re-submit VAE-006: Add adversarial loss and increase KL-reg

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -7,4 +7,4 @@
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-IDEAL-VAE.py --dataset VAE-006 --encoded_size 24 --A_loss_weight 1e-8
+python train-IDEAL-VAE.py --dataset VAE-006 --encoded_size 24 --adv_train True --A_loss_weight 1e-8 --ls_reg_weight 2e-7
