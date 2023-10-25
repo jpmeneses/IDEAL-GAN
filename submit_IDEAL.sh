45737a14f306b3fe7aa945b2eea44115cc4ec911
jpmeneses <jpmeneses@uc.cl>
2023-10-26 09:43:14 +1100

    Submit KL-LDM (v259): Try with larger batch-size and unfinished Auto-Encoder

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v238-DDPM
-#SBATCH --output=out_DDPM_238.txt
+#SBATCH --job-name=v259-DDPM
+#SBATCH --output=out_DDPM_259.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-ldm.py --experiment_dir 'GAN-238' --n_timesteps 1000 --batch_size 12 --epochs_ldm 200 --lr 1e-4
+python train-ldm.py --experiment_dir 'GAN-259' --n_timesteps 1000 --batch_size 16 --epochs_ldm 200 --lr 1e-4
