31743a161494448687d0a338a0a4c35a4e0971aa
jpmeneses <jpmeneses@uc.cl>
2023-10-25 14:50:55 +1100

    Debug 'dataset' argument

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -7,4 +7,4 @@
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-IDEAL-GAN.py --experiment_dir 'GAN-259' --n_downsamplings 3 --encoded_size 6 --adv_train True --cGAN True --ls_reg_weight 0.9e-7
+python train-IDEAL-GAN.py --dataset 'GAN-259' --n_downsamplings 3 --encoded_size 6 --adv_train True --cGAN True --ls_reg_weight 0.9e-7
