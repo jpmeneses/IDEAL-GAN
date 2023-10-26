673d83ce4d29702420ffa965cf09d0909c3dc696
jpmeneses <jpmeneses@uc.cl>
2023-10-27 08:41:54 +1100

    Re-submit VAE-007: Increase A-loss weight

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v259-DDPM
-#SBATCH --output=out_DDPM_259.txt
+#SBATCH --job-name=v007-VAE
+#SBATCH --output=out_VAE_007.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-ldm.py --experiment_dir 'GAN-259' --n_timesteps 1000 --batch_size 16 --epochs_ldm 200 --lr 1e-4
+python train-IDEAL-VAE.py --dataset VAE-007 --encoded_size 24 --adv_train True --batch_size 64 --lr 0.001 --D_lr_factor 5 --A_loss_weight 1e-2
