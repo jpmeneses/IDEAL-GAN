0e0f8c3de25d83c7b4dda5fa664ab72173f94524
jpmeneses <jpmeneses@uc.cl>
2023-10-26 16:27:55 +1100

    Submit VAE-007: Much larger batch-size and increased D-LR factor

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v261-GAN
-#SBATCH --output=out_GAN_261.txt
+#SBATCH --job-name=v007-VAE
+#SBATCH --output=out_VAE_007.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-IDEAL-GAN.py --dataset 'GAN-261' --n_downsamplings 3 --encoded_size 6 --adv_train True --cGAN True --batch_size 8 --lr 0.0005 --ls_reg_weight 1e-7
+python train-IDEAL-VAE.py --dataset VAE-007 --encoded_size 24 --adv_train True --batch_size 64 --lr 0.001 --D_lr_factor 5 --A_loss_weight 1e-7
