0d5e1d94eb02c55514bced5dafbf47c343b8e549
jpmeneses <jpmeneses@uc.cl>
2023-10-27 10:16:17 +1100

    Submit VAE-GAN(v400): Start new series with increased batch-size - 1st iteration with reference parameters

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v007-VAE
-#SBATCH --output=out_VAE_007.txt
+#SBATCH --job-name=v400-GAN
+#SBATCH --output=out_GAN_400.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-IDEAL-VAE.py --dataset VAE-007 --encoded_size 24 --adv_train True --batch_size 64 --lr 0.001 --D_lr_factor 5 --A_loss_weight 1e-2
+ython train-IDEAL-GAN.py --dataset 'GAN-400' --encoded_size 24 --adv_train True --cGAN True --batch_size 8 --lr 0.001
