500594db5fa574f7ddde0c05f0e4aeb398538dac
jpmeneses <jpmeneses@uc.cl>
2023-10-26 19:33:05 +1100

    Submit KL-LDM (v259)

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v261-GAN
-#SBATCH --output=out_GAN_261.txt
+#SBATCH --job-name=v259-DDPM
+#SBATCH --output=out_DDPM_259.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-IDEAL-GAN.py --dataset 'GAN-261' --n_downsamplings 3 --encoded_size 6 --adv_train True --cGAN True --batch_size 8 --lr 0.0005 --ls_reg_weight 2e-8
+python train-ldm.py --experiment_dir 'GAN-259' --n_timesteps 1000 --batch_size 16 --epochs_ldm 200 --lr 1e-4
