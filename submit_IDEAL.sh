4aa1a28a2295764a0d2691caf7155a6b86403802
jpmeneses <jpmeneses@uc.cl>
2023-10-27 20:31:13 +1100

    Submit VAE-GAN(v500): New series - magnitude-based generation

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v400-GAN
-#SBATCH --output=out_GAN_400.txt
+#SBATCH --job-name=v500-GAN
+#SBATCH --output=out_GAN_500.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-IDEAL-GAN.py --dataset 'GAN-400' --encoded_size 24 --adv_train True --cGAN True --batch_size 8 --lr 0.001
+python train-IDEAL-GAN.py --dataset 'GAN-500' --n_groups_PM 1 --encoded_size 24 --adv_train True --cGAN True --batch_size 16 --lr 0.001
