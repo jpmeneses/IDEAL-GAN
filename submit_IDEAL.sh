e8e91940e309761e379928eb413f9459d9ef1568
jpmeneses <jpmeneses@uc.cl>
2023-10-30 07:22:53 +1100

    Submit VAE-GAN(v401): Same as v400, but increased KL-reg weight

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v500-GAN
-#SBATCH --output=out_GAN_500.txt
+#SBATCH --job-name=v401-GAN
+#SBATCH --output=out_GAN_401.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-IDEAL-GAN.py --dataset 'GAN-500' --only_mag True --n_groups_PM 1 --encoded_size 24 --adv_train True --cGAN True --batch_size 8 --lr 0.001 --ls_reg_weight 1e-8
+python train-IDEAL-GAN.py --dataset 'GAN-401' --encoded_size 24 --adv_train True --cGAN True --batch_size 8 --lr 0.001 --ls_reg_weight 1e-6
