d21774ef80000ad473fc072a00c0bc8081050c13
jpmeneses <jpmeneses@uc.cl>
2023-10-30 12:21:32 +1100

    Statistical CoV: Keep dims when averaging prod diff matrices

--- DLlib/metrics.py
+++ DLlib/metrics.py
@@ -196,5 +196,5 @@ class CoVar(tf.keras.layers.Layer):
         x_mu = tf.reduce_mean(x,axis=0,keepdims=True)
         x_dif = keras.layers.Lambda(lambda z: tf.expand_dims(z,axis=-1))(x - x_mu)
         cov = keras.layers.Lambda(lambda a: tf.linalg.matmul(a,a,transpose_b=True))(x_dif)
-        cov_res = keras.layers.Lambda(lambda z: tf.reduce_mean(z,axis=0))(cov)
+        cov_res = keras.layers.Lambda(lambda z: tf.reduce_mean(z,axis=0,keepdims=True))(cov)
         return cov_res
