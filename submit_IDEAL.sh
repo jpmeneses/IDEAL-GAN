4ab1cf1b7f1beb9fe3606a6c98a459552eef2613
jpmeneses <jpmeneses@uc.cl>
2023-10-30 11:39:06 +1100

    Submit VAE-007: Increased KL-reg weight, batch-size, and cov-reg for monitoring

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v401-GAN
-#SBATCH --output=out_GAN_401.txt
+#SBATCH --job-name=v008-VAE
+#SBATCH --output=out_VAE_008.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-IDEAL-GAN.py --dataset 'GAN-401' --encoded_size 24 --adv_train True --cGAN True --batch_size 8 --lr 0.001 --ls_reg_weight 1e-6
+python train-IDEAL-VAE.py --dataset VAE-008 --encoded_size 24 --adv_train True --batch_size 64 --lr 0.001 --D_lr_factor 5 --A_loss_weight 1e-2 --ls_reg_weight 1e-6
