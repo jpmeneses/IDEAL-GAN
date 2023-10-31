1c5619d3e339b1a201cbe8cae7c4e29cc28438ec
jpmeneses <jpmeneses@uc.cl>
2023-10-31 11:52:23 +1100

    Submit KL-LDM (v401): Unfinished KL-VAE, trying with Ep-80 ckpt

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v010-VAE
-#SBATCH --output=out_VAE_010.txt
+#SBATCH --job-name=v401-LDM
+#SBATCH --output=out_LDM_401.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-IDEAL-VAE.py --dataset VAE-010 --encoded_size 24 --adv_train True --batch_size 64 --lr 0.001 --D_lr_factor 5 --A_loss_weight 1e-2 --ls_reg_weight 1e-4
+python train-ldm.py --experiment_dir 'GAN-401' --n_timesteps 1000 --batch_size 8 --epochs_ldm 200 --lr 1e-4
