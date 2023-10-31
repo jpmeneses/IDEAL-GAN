89197561e40eb1993a3d311921521808eaac7d5a
jpmeneses <jpmeneses@uc.cl>
2023-10-31 16:14:56 +1100

    Submit VAE-GAN(v402): Same as v401, but without NL Self-Attention

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v401-GAN
-#SBATCH --output=out_GAN_401.txt
+#SBATCH --job-name=v402-GAN
+#SBATCH --output=out_GAN_402.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-IDEAL-GAN.py --dataset 'GAN-401' --encoded_size 24 --adv_train True --cGAN True --batch_size 8 --epochs 160 --epoch_decay 160 --lr 0.001 --ls_reg_weight 1e-6
+python train-IDEAL-GAN.py --dataset 'GAN-402' --encoded_size 24 --adv_train True --cGAN True --batch_size 8 --epochs 100 --epoch_decay 100 --lr 0.001 --ls_reg_weight 1e-6 --NL_SelfAttention False
