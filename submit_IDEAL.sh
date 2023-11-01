facae85720638810291bb0382fac443e9fba06b5
jpmeneses <jpmeneses@uc.cl>
2023-11-01 18:56:01 +1100

    Submit VAE-GAN(v405): In-between of KL-reg weightings of v401 and v403

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v012-VAE
-#SBATCH --output=out_VAE_012.txt
+#SBATCH --job-name=v405-GAN
+#SBATCH --output=out_GAN_405.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-IDEAL-VAE.py --dataset VAE-012 --encoded_size 24 --adv_train True --batch_size 64 --lr 0.001 --D_lr_factor 5 --A_loss_weight 1e-3 --ls_reg_weight 1e-3
+python train-IDEAL-GAN.py --dataset 'GAN-405' --encoded_size 24 --adv_train True --cGAN True --batch_size 8 --epochs 100 --epoch_decay 100 --lr 0.001 --ls_reg_weight 5e-6
