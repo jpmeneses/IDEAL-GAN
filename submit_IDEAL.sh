5a6929464734339b347d4310ddfb2671d087f266
jpmeneses <jpmeneses@uc.cl>
2023-11-03 08:05:32 +1100

    Submit VAE-GAN(v407): Decreased KL-reg and slightly increased CoV-reg

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v406-GAN
-#SBATCH --output=out_GAN_406.txt
+#SBATCH --job-name=v407-GAN
+#SBATCH --output=out_GAN_407.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-IDEAL-GAN.py --dataset 'GAN-406' --encoded_size 24 --adv_train True --cGAN True --batch_size 8 --epochs 100 --epoch_decay 100 --lr 0.001 --ls_reg_weight 4e-6 --cov_reg_weight 1e-4
+python train-IDEAL-GAN.py --dataset 'GAN-407' --encoded_size 24 --adv_train True --cGAN True --batch_size 8 --epochs 100 --epoch_decay 100 --lr 0.001 --ls_reg_weight 2e-6 --cov_reg_weight 2e-4
