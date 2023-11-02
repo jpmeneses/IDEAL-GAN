e22fd9b5fc65d85b0d2272ed3b0774182b4fa392
jpmeneses <jpmeneses@uc.cl>
2023-11-02 11:03:47 +1100

    Submit VAE-GAN(v406): Add CoV-regularization

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v405-GAN
-#SBATCH --output=out_GAN_405.txt
+#SBATCH --job-name=v406-GAN
+#SBATCH --output=out_GAN_406.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-IDEAL-GAN.py --dataset 'GAN-405' --encoded_size 24 --adv_train True --cGAN True --batch_size 8 --epochs 100 --epoch_decay 100 --lr 0.001 --ls_reg_weight 5e-6
+python train-IDEAL-GAN.py --dataset 'GAN-406' --encoded_size 24 --adv_train True --cGAN True --batch_size 8 --epochs 100 --epoch_decay 100 --lr 0.001 --ls_reg_weight 4e-6 --cov_reg_weight 1e-4
