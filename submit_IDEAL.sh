12f9c1e11a8e595ffdcc9048b9004e2a1ae9514f
jpmeneses <jpmeneses@uc.cl>
2023-11-06 18:19:09 +1100

    Re-submit KL-LDM (v408): Try with fewer time-steps and a smaller LR

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v407-GAN
-#SBATCH --output=out_GAN_407.txt
+#SBATCH --job-name=v408-LDM
+#SBATCH --output=out_LDM_408.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-IDEAL-GAN.py --dataset 'GAN-407' --encoded_size 24 --adv_train True --cGAN True --batch_size 8 --epochs 200 --epoch_decay 200 --lr 0.001 --ls_reg_weight 2e-6
+python train-ldm.py --experiment_dir 'GAN-408' --n_timesteps 200 --batch_size 8 --epochs_ldm 400 --lr 5e-5
