d7734fc310279d1d41641a9447cf8e33004ca00d
jpmeneses <jpmeneses@uc.cl>
2023-11-09 12:42:46 +1100

    Re-submit KL-LDM (v408): Customized initial and final beta values

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v409-GAN
-#SBATCH --output=out_GAN_409.txt
+#SBATCH --job-name=v408-LDM
+#SBATCH --output=out_LDM_408.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-IDEAL-GAN.py --dataset 'GAN-409' --n_downsamplings 3 --encoded_size 3 --adv_train True --cGAN True --batch_size 8 --epochs 100 --epoch_decay 100 --lr 0.001 --ls_reg_weight 2e-6
+python train-ldm.py --experiment_dir 'GAN-408' --n_timesteps 1000 --beta_start 0.0015 --beta_end 0.0195 --batch_size 8 --epochs_ldm 1000 --lr 1e-5
