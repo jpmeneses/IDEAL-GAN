db89f2c4195db9787a08ddc65f55167b7e68dd2d
jpmeneses <jpmeneses@uc.cl>
2023-11-13 10:10:50 +1100

    Re-submit VAE-GAN(v409): Try again with a lower initial beta size

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v408-LDM
-#SBATCH --output=out_LDM_408.txt
+#SBATCH --job-name=v409-LDM
+#SBATCH --output=out_LDM_409.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-ldm.py --experiment_dir 'GAN-408' --n_timesteps 500 --beta_start 0.005 --beta_end 0.1 --batch_size 8 --epochs_ldm 600 --lr 1e-5
+python train-ldm.py --experiment_dir 'GAN-409' --n_timesteps 500 --beta_start 0.001 --beta_end 0.1 --batch_size 8 --epochs_ldm 600 --lr 1e-5
