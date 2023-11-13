17c25607fca25e36c8a74a1aba896d05075febd9
jpmeneses <jpmeneses@uc.cl>
2023-11-13 15:56:15 +1100

    submit KL-LDM (v408): Train for 2000 epochs with a LR of 4e-5

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v408-LDM
-#SBATCH --output=out_LDM_408.txt
+#SBATCH --job-name=v408a-LDM
+#SBATCH --output=out_LDM_408a.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-ldm.py --experiment_dir 'GAN-408' --n_timesteps 500 --beta_start 0.002 --beta_end 0.1 --batch_size 8 --epochs_ldm 2000 --epoch_ldm_ckpt 100
+python train-ldm.py --experiment_dir 'GAN-408a' --n_timesteps 500 --beta_start 0.002 --beta_end 0.1 --batch_size 8 --epochs_ldm 2000 --epoch_ldm_ckpt 100 --lr 0.00004
