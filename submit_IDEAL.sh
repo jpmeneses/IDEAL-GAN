aca580e50c1e840767847adbb9192535e535685a
jpmeneses <jpmeneses@uc.cl>
2023-11-15 09:54:22 +1100

    Re-submit KL-LDM (v408c): Train for 2000 epochs with a LR of 1e-4

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -7,4 +7,4 @@
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-ldm.py --experiment_dir 'GAN-408c' --n_timesteps 500 --beta_start 0.002 --beta_end 0.1 --batch_size 8 --epochs_ldm 2000 --epoch_ldm_ckpt 100 --lr 0.00002
+python train-ldm.py --experiment_dir 'GAN-408c' --n_timesteps 500 --beta_start 0.002 --beta_end 0.1 --batch_size 8 --epochs_ldm 2000 --epoch_ldm_ckpt 100 --lr 0.0001
