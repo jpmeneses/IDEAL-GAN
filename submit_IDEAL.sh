8e0363479aa55db3a90b8e635f4affd2fca41ecd
jpmeneses <jpmeneses@uc.cl>
2023-11-17 10:17:01 +1100

    Submit TEaug-200: Train VET-Net with 384x384 data

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v500-GAN
-#SBATCH --output=out_GAN_500.txt
+#SBATCH --job-name=v200-TEaug
+#SBATCH --output=out_TEaug_200.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-IDEAL-GAN.py --dataset 'GAN-500' --n_downsamplings 5 --encoded_size 24 --adv_train True --cGAN True --batch_size 2 --epochs 140 --epoch_decay 140 --lr 0.00025 --ls_reg_weight 2e-6
+python train-IDEAL-TEaug.py --dataset 'TEaug-200' --data_size 384 --out_vars PM
