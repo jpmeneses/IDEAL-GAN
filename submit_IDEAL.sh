840c79abdabace300a5d2203527a8c73d8cf3380
jpmeneses <jpmeneses@uc.cl>
2023-11-17 10:46:25 +1100

    Submit TEaug-201: 3-echo VET-Net with 384x384 data

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v200-TEaug
-#SBATCH --output=out_TEaug_200.txt
+#SBATCH --job-name=v201-TEaug
+#SBATCH --output=out_TEaug_201.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-IDEAL-TEaug.py --dataset 'TEaug-200' --data_size 384 --out_vars PM
+python train-IDEAL-TEaug.py --dataset 'TEaug-201' --data_size 384 --n_echoes 3 --out_vars PM
