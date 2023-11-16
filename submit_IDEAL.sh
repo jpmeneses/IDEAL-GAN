4a176821c36aac0ff0131a145457025492cf26b8
jpmeneses <jpmeneses@uc.cl>
2023-11-16 12:09:41 +1100

    Submit KL-LDM (v408d): Set LR to 8e-5

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v408e-LDM
-#SBATCH --output=out_LDM_408e.txt
+#SBATCH --job-name=v408f-LDM
+#SBATCH --output=out_LDM_408f.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-ldm.py --experiment_dir 'GAN-408e' --n_timesteps 500 --beta_start 0.002 --beta_end 0.1 --batch_size 8 --epochs_ldm 2000 --epoch_ldm_ckpt 100 --lr 0.00008
+python train-ldm.py --experiment_dir 'GAN-408f' --n_timesteps 500 --beta_start 0.002 --beta_end 0.1 --batch_size 8 --epochs_ldm 2000 --epoch_ldm_ckpt 100 --lr 0.00007
