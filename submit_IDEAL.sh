fc965ca368b74080f89c9c4b419c727c17dee2a6
jpmeneses <jpmeneses@uc.cl>
2023-11-20 13:16:09 +1100

    Submit TEaug-203: 3-echo VET-Net with 384x384 data and reduced base number of filters to 36

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v202-TEaug
-#SBATCH --output=out_TEaug_202.txt
+#SBATCH --job-name=v203-TEaug
+#SBATCH --output=out_TEaug_203.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-IDEAL-TEaug.py --dataset 'TEaug-202' --data_size 384 --out_vars PM --n_G_filters 36
+python train-IDEAL-TEaug.py --dataset 'TEaug-203' --data_size 384 --n_echoes 3 --out_vars PM --n_G_filters 36
