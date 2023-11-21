8b017b7b1e504b6837ff437f54f77e00d139c53c
jpmeneses <jpmeneses@uc.cl>
2023-11-21 15:04:01 +1100

    Submit KL-LDM (v408h): Try with cosine beta scheduler

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v408g-LDM
-#SBATCH --output=out_LDM_408g.txt
+#SBATCH --job-name=v408h-LDM
+#SBATCH --output=out_LDM_408h.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-ldm.py --experiment_dir 'GAN-408g' --n_timesteps 1000 --beta_start 0.0005 --beta_end 0.05 --batch_size 8 --epochs_ldm 2000 --epoch_ldm_ckpt 100 --lr 0.00007
+python train-ldm.py --experiment_dir 'GAN-408h' --scheduler cosine --n_timesteps 1000 --batch_size 8 --epochs_ldm 2000 --epoch_ldm_ckpt 100 --lr 0.00007
