517fda7f3e8739b8ecca70760140119b3238615c
jpmeneses <jpmeneses@uc.cl>
2023-11-21 21:23:33 +1100

    Submit VAE-GAN(v502): Train with large-scale data and 3 down-samplings

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v501-GAN
-#SBATCH --output=out_GAN_501.txt
+#SBATCH --job-name=v502-GAN
+#SBATCH --output=out_GAN_502.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-IDEAL-GAN.py --dataset 'GAN-501' --n_downsamplings 4 --encoded_size 3 --adv_train True --cGAN True --batch_size 2 --epochs 100 --epoch_decay 100 --lr 0.00025 --ls_reg_weight 2e-6
+python train-IDEAL-GAN.py --dataset 'GAN-502' --n_downsamplings 3 --encoded_size 3 --adv_train True --cGAN True --batch_size 2 --epochs 100 --epoch_decay 100 --lr 0.00025 --ls_reg_weight 1e-5
