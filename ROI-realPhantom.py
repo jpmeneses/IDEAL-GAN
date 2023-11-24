7d42ef9352ea28c6dc95b09fd585eb8387afb97c
jpmeneses <jpmeneses@uc.cl>
2023-11-24 17:53:14 +1100

    Enable MEBCRN and non-MEBCRN data loading

--- ROI-realPhantom.py
+++ ROI-realPhantom.py
@@ -20,10 +20,16 @@ py.arg('--experiment_dir',default='output/WF-sep')
 py.arg('--dataset', type=str, default='phantom_1p5', choices=['phantom_1p5','phantom_3p0'])
 py.arg('--map',default='PDFF',choices=['PDFF','R2s','Water'])
 py.arg('--te_input', type=bool, default=True)
+py.arg('--ME_layer', type=bool, default=False)
 py.arg('--batch_size', type=int, default=1)
 test_args = py.args()
 args = py.args_from_yaml(py.join(test_args.experiment_dir, 'settings.yml'))
 args.__dict__.update(test_args.__dict__)
+
+if not(hasattr(args,'field')):
+  py.arg('--field', type=float, default=1.5)
+  ds_args = py.args()
+  args.__dict__.update(ds_args.__dict__)
 
 # Excel file for saving ROIs values
 workbook = xlsxwriter.Workbook(py.join(args.experiment_dir,args.map+'_phantom_ROIs.xlsx'))
@@ -35,7 +41,7 @@ r2_sc,fm_sc = 200.0,300.0
 ######################### DIRECTORIES AND FILENAMES ############################
 ################################################################################
 dataset_dir = '../../OneDrive - Universidad Católica de Chile/Documents/datasets/'
-dataset_hdf5 = args.dataset + '_GC_192_complex_2D.hdf5'
+dataset_hdf5 = args.dataset + '_GC_192_128_complex_2D.hdf5'
 testX, testY, TEs =  data.load_hdf5(dataset_dir, dataset_hdf5, ech_idx,
                                     acqs_data=True, te_data=True, MEBCRN=True)
 
@@ -63,9 +69,13 @@ A_B_dataset_test.batch(1)
 #################################################################################
 
 # model
+if args.ME_layer:
+  input_shape = (args.n_echoes,None,None,n_ch)
+else:
+  input_shape = (None,None,ech_idx)
 if args.G_model == 'multi-decod' or args.G_model == 'encod-decod':
   if args.out_vars == 'WF-PM':
-    G_A2B = dl.MDWF_Generator(input_shape=(hgt,wdt,ech_idx),
+    G_A2B = dl.MDWF_Generator(input_shape=input_shape,
                               te_input=args.te_input,
                               te_shape=(args.n_echoes,),
                               filters=args.n_G_filters,
@@ -73,7 +83,8 @@ if args.G_model == 'multi-decod' or args.G_model == 'encod-decod':
                               R2_self_attention=args.D2_SelfAttention,
                               FM_self_attention=args.D3_SelfAttention)
   else:
-    G_A2B = dl.PM_Generator(input_shape=(args.n_echoes,hgt,wdt,n_ch),
+    G_A2B = dl.PM_Generator(input_shape=input_shape,
+                            ME_layer=args.ME_layer,
                             te_input=args.te_input,
                             te_shape=(args.n_echoes,),
                             filters=args.n_G_filters,
@@ -84,7 +95,7 @@ elif args.G_model == 'U-Net':
     n_out = 4
   else:
     n_out = 2
-  G_A2B = dl.UNet(input_shape=(hgt,wdt,ech_idx),
+  G_A2B = dl.UNet(input_shape=input_shape,
                   n_out=n_out,
                   filters=args.n_G_filters,
                   te_input=args.te_input,
@@ -108,6 +119,10 @@ tl.Checkpoint(dict(G_A2B=G_A2B), py.join(args.experiment_dir, 'checkpoints')).re
 
 @tf.function
 def sample(A, B, TE=None):
+  # Back-up A
+  A_ME = A
+  if not(args.ME_layer):
+    A = data.A_from_MEBCRN(A)
   # Split B
   B_WF = B[:,:2,:,:,:]
   B_PM = B[:,2:,:,:,:]
@@ -115,12 +130,15 @@ def sample(A, B, TE=None):
   # Estimate A2B
   if args.out_vars == 'WF':
     if args.te_input:
-      A2B_WF_abs = G_A2B([A,TE], training=True)
+      if TE is None:
+        TE = wf.gen_TEvar(args.n_echoes, bs=A.shape[0], orig=True) # (nb,ne,1)
+      A2B_WF = G_A2B([A,TE], training=True)
     else:
-      A2B_WF_abs = G_A2B(A, training=True)
-    A2B_WF_abs = tf.where(A[:,:,:,:2]!=0.0,A2B_WF_abs,0.0)
+      A2B_WF = G_A2B(A, training=True)
+    A2B_WF = data.B_to_MEBCRN(A2B_WF,mode='WF')
+    A2B_WF = tf.where(A_ME[:,:2,:,:,:]!=0.0,A2B_WF,0.0)
     A2B_PM = tf.zeros_like(B_PM)
-    A2B_abs = tf.concat([A2B_WF_abs,A2B_PM],axis=-1)
+    A2B = tf.concat([A2B_WF,A2B_PM],axis=1)
   elif args.out_vars == 'WFc':
     A2B_WF = G_A2B(A, training=False)
     A2B_WF = tf.where(B[:,:,:,:4]!=0,A2B_WF,0.0)
@@ -131,39 +149,44 @@ def sample(A, B, TE=None):
     A2B_abs = tf.concat([A2B_WF_abs,A2B_PM],axis=-1)
   elif args.out_vars == 'PM':
     if args.te_input:
-      A2B_PM = G_A2B([A,TE], training=False)
+      if TE is None:
+        TE = wf.gen_TEvar(args.n_echoes, bs=A.shape[0], orig=True) # (nb,ne,1)
+      A2B_PM = G_A2B([A,TE], training=True)
     else:
-      A2B_PM = G_A2B(A, training=False)
-    A2B_PM = tf.where(A[:,:1,:,:,:]!=0.0,A2B_PM,0.0)
+      A2B_PM = G_A2B(A, training=True)
+    if not(args.ME_layer):
+      A2B_PM = data.B_to_MEBCRN(A2B_PM,mode='PM')
+    A2B_PM = tf.where(A_ME[:,:1,:,:,:]!=0.0,A2B_PM,0.0)
     A2B_R2 = A2B_PM[:,:,:,:,1:]
     A2B_FM = A2B_PM[:,:,:,:,:1]
     if args.G_model=='U-Net' or args.G_model=='MEBCRN':
       A2B_FM = (A2B_FM - 0.5) * 2
-      A2B_FM = tf.where(A[:,:1,:,:,1:]!=0.0,A2B_FM,0.0)
-      A2B_PM = tf.concat([A2B_R2,A2B_FM],axis=-1)
-    A2B_WF = wf.get_rho(A, A2B_PM, field=args.field, te=TE)
+      A2B_FM = tf.where(A_ME[:,:1,:,:,:1]!=0.0,A2B_FM,0.0)
+      A2B_PM = tf.concat([A2B_FM,A2B_R2],axis=-1)
+    A2B_WF = wf.get_rho(A_ME, A2B_PM, field=args.field, te=TE)
     A2B = tf.concat([A2B_WF,A2B_PM],axis=1)
   elif args.out_vars == 'WF-PM':
-    B_abs = tf.concat([B_WF_abs,B_PM],axis=-1)
     if args.te_input:
+      if TE is None:
+        TE = wf.gen_TEvar(args.n_echoes, bs=A.shape[0], orig=True) # (nb,ne,1)
       A2B_abs = G_A2B([A,TE], training=True)
     else:
       A2B_abs = G_A2B(A, training=True)
-    A2B_abs = tf.where(B_abs!=0.0,A2B_abs,0.0)
-    A2B_WF_abs,A2B_PM = tf.dynamic_partition(A2B_abs,indx_B_abs,num_partitions=2)
-    A2B_WF_abs = tf.reshape(A2B_WF_abs,B[:,:,:,:2].shape)
-    A2B_PM = tf.reshape(A2B_PM,B[:,:,:,4:].shape)
-    A2B_R2, A2B_FM = tf.dynamic_partition(A2B_PM,indx_PM,num_partitions=2)
-    A2B_R2 = tf.reshape(A2B_R2,B[:,:,:,:1].shape)
-    A2B_FM = tf.reshape(A2B_FM,B[:,:,:,:1].shape)
+    A2B = data.B_to_MEBCRN(A2B_abs,mode='WF-PM')
+    A2B = tf.where(A_ME[:,:3,:,:,:]!=0.0,A2B,0.0)
+    A2B_WF = A2B[:,:2,:,:,:]
+    A2B_PM = A2B[:,2:,:,:,:]
+    A2B_R2 = A2B_PM[:,:,:,:,1:]
+    A2B_FM = A2B_PM[:,:,:,:,:1]
     if args.G_model=='U-Net' or args.G_model=='MEBCRN':
       A2B_FM = (A2B_FM - 0.5) * 2
-      A2B_FM = tf.where(B_PM[:,:,:,:1]!=0.0,A2B_FM,0.0)
-      A2B_abs = tf.concat([A2B_WF_abs,A2B_R2,A2B_FM],axis=-1)
+      A2B_FM = tf.where(A_ME[:,:1,:,:,:1]!=0.0,A2B_FM,0.0)
+      A2B_PM = tf.concat([A2B_FM,A2B_R2],axis=-1)
+      A2B = tf.concat([A2B_WF,A2B_PM],axis=1)
 
   return A2B
 
-all_test_ans = np.zeros((len_dataset,n_out,hgt,wdt,n_ch))
+all_test_ans = np.zeros((len_dataset,hgt,wdt,3))
 i = 0
 
 t1 = process_time()
@@ -174,29 +197,26 @@ for A, B, TE in tqdm.tqdm(A_B_dataset_test, desc='Testing Samples Loop', total=l
   A2B = sample(A,B,TE)
   # A2B = tf.expand_dims(A2B,axis=0)
 
-  all_test_ans[i,:,:,:,:] = A2B
+  A2B_WF_abs = tf.math.sqrt(tf.reduce_sum(tf.square(A2B[:,:2,:,:,:]),axis=-1))
+  A2B_WF_abs = tf.transpose(A2B_WF_abs,perm=[0,2,3,1])
+  A2B_R2 = A2B[:,2,:,:,1:]
+  A2B = tf.concat([A2B_WF_abs,A2B_R2],axis=-1)
+
+  all_test_ans[i,:,:,:] = A2B
   i += 1
 
 t2 = process_time()
 print("Elapsed time during the whole program in seconds:",t2-t1) 
 print("Time per slice:",(t2-t1)/len_dataset)
 
-wf_ans_abs = np.sqrt(np.sum(np.square(all_test_ans[:,:2,:,:,:]),axis=-1,keepdims=False))
-wf_ans_abs = np.transpose(wf_ans_abs,axes=(0,2,3,1))
-w_all_ans = wf_ans_abs[:,:,:,0]
-f_all_ans = wf_ans_abs[:,:,:,1]
-
-pm_ans = all_test_ans[:,2,:,:,:]
-r2_all_ans = pm_ans[:,:,:,1]*r2_sc
+w_all_ans = all_test_ans[:,:,:,0]
+f_all_ans = all_test_ans[:,:,:,1]
+r2_all_ans = all_test_ans[:,:,:,2]*r2_sc
 
 # Ground truth
-wf_gt_abs = np.sqrt(np.sum(np.square(testY[:,:2,:,:,:]),axis=-1,keepdims=False))
-wf_gt_abs = np.transpose(wf_gt_abs,axes=(0,2,3,1))
-w_all_gt = wf_gt_abs[:,:,:,0]
-f_all_gt = wf_gt_abs[:,:,:,1]
-
-pm_gt = testY[:,2,:,:,:]
-r2_all_gt = pm_gt[:,:,:,1]*r2_sc
+w_all_gt = np.sqrt(np.sum(testY[:,0,:,:,:]**2,axis=-1))
+f_all_gt = np.sqrt(np.sum(testY[:,1,:,:,:]**2,axis=-1))
+r2_all_gt = testY[:,2,:,:,1]*r2_sc
 
 #################################################################################
 #################################################################################
@@ -224,13 +244,10 @@ elif args.map == 'Water':
 else:
   raise TypeError('The selected map is not available')
 
-if args.dataset == 'phantom_1p5':
-  npy_file = 'phantom_slices_crops.npy'
-elif args.dataset == 'phantom_3p0':
-  npy_file = 'phantom_slices_crops_3p0.npy'
+npy_file = py.join('ROI_files', args.dataset + '_slices_crops.npy')
 
 fig, ax = plt.subplots(1, 1)
-tracker = IndexTracker_phantom(fig, ax, X, bool_PDFF, lims, npy_file)
+tracker = IndexTracker_phantom(fig, ax, X, bool_PDFF, lims, npy_file=npy_file)
 
 fig.canvas.mpl_connect('scroll_event', tracker.onscroll)
 fig.canvas.mpl_connect('button_press_event', tracker.button_press)
