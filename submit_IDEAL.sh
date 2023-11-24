cf8563007439c553bcd40be4afa79d47e04dc668
jpmeneses <jpmeneses@uc.cl>
2023-11-24 17:49:27 +1100

    Submit GEN-v503: Generate synthetic CSE-MR data

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v409-LDM
-#SBATCH --output=out_LDM_409.txt
+#SBATCH --job-name=v503-GEN
+#SBATCH --output=out_GEN_503.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-ldm.py --experiment_dir 'GAN-409' --scheduler cosine --n_timesteps 1000 --batch_size 8 --epochs_ldm 2000 --epoch_ldm_ckpt 100 --lr 0.00007
+python test-genData.py --experiment_dir output/GAN-503
