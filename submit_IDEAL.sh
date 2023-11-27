3ed8e16cb77572c49496488bb6a604fe75481e64
jpmeneses <jpmeneses@uc.cl>
2023-11-27 13:26:38 +1100

    Submit VQ-VAE(v601): Increased num. of embedding (16)

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v600-GAN
-#SBATCH --output=out_600_GAN.txt
+#SBATCH --job-name=v601-GAN
+#SBATCH --output=out_601_GAN.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-IDEAL-GAN.py --dataset GAN-600 --n_downsamplings 3 --encoded_size 3 --VQ_encoder True --VQ_num_embed 8 --adv_train True --cGAN True --batch_size 8 --lr 0.001 --ls_reg_weight 1.0
+python train-IDEAL-GAN.py --dataset GAN-601 --n_downsamplings 3 --encoded_size 3 --VQ_encoder True --VQ_num_embed 16 --adv_train True --cGAN True --batch_size 8 --lr 0.001 --ls_reg_weight 1.0
