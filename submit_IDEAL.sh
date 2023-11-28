417c57ea054adc905495fde9b69b4a1f9acf3c8c
jpmeneses <jpmeneses@uc.cl>
2023-11-29 10:45:28 +1100

    Submit VQ-VAE(v603): Same as v602, increased num. of embedding to 32

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v601-LDM
-#SBATCH --output=out_601_LDM.txt
+#SBATCH --job-name=v603-GAN
+#SBATCH --output=out_603_GAN.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-ldm.py --experiment_dir GAN-601 --scheduler cosine --n_timesteps 1000 --batch_size 8 --epochs_ldm 2000 --epoch_ldm_ckpt 100
+python train-IDEAL-GAN.py --dataset GAN-603 --n_downsamplings 3 --encoded_size 3 --VQ_encoder True --VQ_num_embed 32 --adv_train True --cGAN True --batch_size 8 --lr 0.001 --ls_reg_weight 5.0
