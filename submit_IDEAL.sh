47dd39c1a0f08b557b3d6023b1f5b4af4b1c8db9
jpmeneses <jpmeneses@uc.cl>
2023-11-30 22:54:06 +1000

    Re-submit VQ-VAE(v603): Train for 60 more epochs

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -7,4 +7,4 @@
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-IDEAL-GAN.py --dataset GAN-603 --n_downsamplings 3 --encoded_size 3 --VQ_encoder True --VQ_num_embed 32 --adv_train True --cGAN True --batch_size 8 --lr 0.001 --ls_reg_weight 5.0
+python train-IDEAL-GAN.py --dataset GAN-603 --n_downsamplings 3 --encoded_size 3 --VQ_encoder True --VQ_num_embed 32 --adv_train True --cGAN True --batch_size 8 --epochs 160 --epoch_decay 160 --lr 0.001 --ls_reg_weight 5.0
