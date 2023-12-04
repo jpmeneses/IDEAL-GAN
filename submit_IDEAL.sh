eb1fa2b03d9c24aeaedf6f6cc6ceebfffc019d86
jpmeneses <jpmeneses@uc.cl>
2023-12-04 12:12:15 +1100

    Re-submit VAE-GAN(v503): Train for 60 more epochs

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v603-LDM
-#SBATCH --output=out_603_LDM.txt
+#SBATCH --job-name=v503-GAN
+#SBATCH --output=out_503_GAN.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-ldm.py --experiment_dir GAN-603 --scheduler cosine --n_timesteps 1000 --batch_size 8 --epochs_ldm 2000 --epoch_ldm_ckpt 100
+python train-IDEAL-GAN.py --dataset GAN-503 --data_size 384 --n_downsamplings 5 --encoded_size 24 --adv_train True --cGAN True --batch_size 2 --epochs 160 --epoch_decay 160 --lr 0.00025 --ls_reg_weight 2e-6 
