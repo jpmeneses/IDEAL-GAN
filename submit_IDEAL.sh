6a746665601fc99630d5ec1adc8e974eb39b2a3b
jpmeneses <jpmeneses@uc.cl>
2023-12-06 10:50:26 +1100

    Submit GEN-v503: Generate synthetic CSE-MR data

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v204-TEaug
-#SBATCH --output=out_204_TEaug.txt
+#SBATCH --job-name=v503-GEN
+#SBATCH --output=out_503_GEN.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-IDEAL-TEaug.py --dataset TEaug-204 --data_size 384 --field 3.0 --out_vars PM --n_G_filters 36
+python test-genData.py --experiment_dir output/GAN-503 --n_samples 100
