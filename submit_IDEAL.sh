adfd2c64ad66c518f6f2c379c18badabd8c7a5d8
jpmeneses <jpmeneses@uc.cl>
2023-12-05 18:17:31 +1100

    Submit TEaug-204: 3T VET-Net with 384x384 data and reduced base number of filters to 36

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v503-GAN
-#SBATCH --output=out_503_GAN.txt
+#SBATCH --job-name=v204-TEaug
+#SBATCH --output=out_204_TEaug.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-IDEAL-GAN.py --dataset GAN-503 --data_size 384 --n_downsamplings 5 --encoded_size 24 --adv_train True --cGAN True --batch_size 2 --epochs 160 --epoch_decay 160 --lr 0.00025 --ls_reg_weight 2e-6 
+python train-IDEAL-TEaug.py --dataset TEaug-204 --data_size 384 --field 3.0 --out_vars PM --n_G_filters 36
