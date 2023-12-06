a744d4ccd55ceda14fd7ebacc3911e1f67f31567
jpmeneses <jpmeneses@uc.cl>
2023-12-06 15:01:27 +1100

    Submit TEaug-205: Train VET-Net with synthetically generated data (VAE-GAN v503)

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v410-GAN
-#SBATCH --output=out_410_GAN.txt
+#SBATCH --job-name=v205-TEaug
+#SBATCH --output=out_TEaug_205.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-IDEAL-GAN.py --dataset GAN-410 --rand_ne True --encoded_size 24 --adv_train True --cGAN True --batch_size 8 --lr 0.001 --ls_reg_weight 2e-6
+python train-IDEAL-TEaug.py --dataset TEaug-205 --data_size 384 --DL_gen True --DL_experiment_dir output/GAN-503 --out_vars PM --n_G_filters 36
