3f9101aa4126551ef2baeecde878d39a90a15132
jpmeneses <jpmeneses@uc.cl>
2023-12-08 09:06:43 +1100

    Submit VAE-GAN(v412): Train with 192x192 data, variable number of echoes and small LS (4 DS)

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v411-GAN
-#SBATCH --output=out_GAN_411.txt
+#SBATCH --job-name=v412-GAN
+#SBATCH --output=out_GAN_412.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-IDEAL-GAN.py --dataset GAN-411 --rand_ne True --n_downsamplings 3 --div_decod True --encoded_size 3 --adv_train True --cGAN True --batch_size 8 --epochs 140 --epoch_decay 140 --lr 0.001 --ls_reg_weight 1e-6
+python train-IDEAL-GAN.py --dataset GAN-412 --rand_ne True --div_decod True --encoded_size 24 --adv_train True --cGAN True --batch_size 8 --epochs 140 --epoch_decay 140 --lr 0.001 --ls_reg_weight 4e-6
