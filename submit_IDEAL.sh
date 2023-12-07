df03477bcfd92655aaaca3f23ead5b1315aa95e3
jpmeneses <jpmeneses@uc.cl>
2023-12-07 15:22:25 +1100

    Re-submit VAE-GAN(v411): Use variable num of echoes and smaller decoders

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -7,4 +7,4 @@
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-IDEAL-GAN.py --dataset GAN-411 --rand_ne True --n_downsamplings 3 --encoded_size 3 --adv_train True --cGAN True --batch_size 8 --epochs 140 --epoch_decay 140 --lr 0.001 --ls_reg_weight 1e-6
+python train-IDEAL-GAN.py --dataset GAN-411 --rand_ne True --n_downsamplings 3 --div_decod True --encoded_size 3 --adv_train True --cGAN True --batch_size 8 --epochs 140 --epoch_decay 140 --lr 0.001 --ls_reg_weight 1e-6
