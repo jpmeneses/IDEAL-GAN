edf06ee70edfd99e49b8ecf3f16423ecf7d95ae2
jpmeneses <jpmeneses@uc.cl>
2023-12-08 09:11:56 +1100

    Submit VQ-VAE(v604): Same as v603, reduced size decoders and limited R2s IDEAL op

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v412-GAN
-#SBATCH --output=out_GAN_412.txt
+#SBATCH --job-name=v604-GAN
+#SBATCH --output=out_GAN_604.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-IDEAL-GAN.py --dataset GAN-412 --rand_ne True --div_decod True --encoded_size 24 --adv_train True --cGAN True --batch_size 8 --epochs 140 --epoch_decay 140 --lr 0.001 --ls_reg_weight 4e-6
+python train-IDEAL-GAN.py --dataset GAN-604 --rand_ne True --n_downsamplings 3 --encoded_size 3 --VQ_encoder True --VQ_num_embed 32 --adv_train True --cGAN True --batch_size 8 --epochs 160 --epoch_decay 160 --lr 0.001 --ls_reg_weight 10.0
