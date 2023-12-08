6633c08e3031686e34b6e477a354f0894a2023b4
jpmeneses <jpmeneses@uc.cl>
2023-12-08 20:42:37 +1100

    Re-submit KL-LDM(v411): Train with ckpt-140

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v603-LDM
-#SBATCH --output=out_LDM_603.txt
+#SBATCH --job-name=v411-LDM
+#SBATCH --output=out_LDM_411.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-ldm.py --experiment_dir GAN-603 --scheduler cosine --n_timesteps 1000 --batch_size 8 --epochs_ldm 2000 --epoch_ldm_ckpt 100
+python train-ldm.py --experiment_dir GAN-411 --scheduler cosine --n_timesteps 1000 --batch_size 8 --epochs_ldm 2000 --epoch_ldm_ckpt 100
