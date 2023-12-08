f9199315a8947e3605db7fed95d1c583e5d1a2df
jpmeneses <jpmeneses@uc.cl>
2023-12-08 21:17:46 +1100

    Submit VAE-GAN(v505): Train with 384x384 data, variable number of echoes, small LS (12x12x24) without activation

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v411-LDM
-#SBATCH --output=out_LDM_411.txt
+#SBATCH --job-name=v505-GAN
+#SBATCH --output=out_GAN_505.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-ldm.py --experiment_dir GAN-411 --scheduler cosine --n_timesteps 1000 --batch_size 8 --epochs_ldm 2000 --epoch_ldm_ckpt 100
+python train-IDEAL-GAN.py --dataset GAN-505 --data_size 384 --rand_ne True --div_decod True --encoded_size 24 --ls_mean_activ None --adv_train True --cGAN True --batch_size 2 --epochs 140 --epoch_decay 140 --lr 0.00025 --ls_reg_weight 4e-6
