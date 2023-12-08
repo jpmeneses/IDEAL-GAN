e98f97fcf0683e36796f1a7617758e9b57effbc3
jpmeneses <jpmeneses@uc.cl>
2023-12-08 21:59:08 +1100

    Re-submit VAE-GAN(v505): Decrease KL regularization weight

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -7,4 +7,4 @@
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-IDEAL-GAN.py --dataset GAN-505 --data_size 384 --rand_ne True --div_decod True --encoded_size 24 --ls_mean_activ None --adv_train True --cGAN True --batch_size 2 --epochs 140 --epoch_decay 140 --lr 0.00025 --ls_reg_weight 4e-6
+python train-IDEAL-GAN.py --dataset GAN-505 --data_size 384 --rand_ne True --div_decod True --encoded_size 24 --ls_mean_activ None --adv_train True --cGAN True --batch_size 2 --epochs 140 --epoch_decay 140 --lr 0.00025 --ls_reg_weight 2e-6
