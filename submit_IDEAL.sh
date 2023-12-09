267f7f1b816a9072fadfa10ce9e3ab80183fe7aa
jpmeneses <jpmeneses@uc.cl>
2023-12-09 18:55:09 +1100

    Submit VQ-LDM(v603): Train LDM with cosine beta scheduler and 100 time steps for 2000 epochs

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v505-GAN
-#SBATCH --output=out_GAN_505.txt
+#SBATCH --job-name=v604-LDM
+#SBATCH --output=out_LDM_604.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-IDEAL-GAN.py --dataset GAN-505 --data_size 384 --rand_ne True --div_decod True --encoded_size 24 --ls_mean_activ None --adv_train True --cGAN True --batch_size 2 --epochs 140 --epoch_decay 140 --lr 0.00025 --ls_reg_weight 2e-6
+python train-ldm.py --experiment_dir GAN-604 --scheduler cosine --n_timesteps 1000 --batch_size 8 --epochs_ldm 2000 --epoch_ldm_ckpt 100
