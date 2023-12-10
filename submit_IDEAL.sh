2f1006beb4029f312c0357fc52e32270fde29d62
jpmeneses <jpmeneses@uc.cl>
2023-12-11 10:30:19 +1100

    Re-submit VAE-GAN(v700): Turn-off adversarial and perceptual losses

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -7,4 +7,4 @@
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-IDEAL-GAN.py --dataset GAN-700 --rand_ne True --only_mag True --div_decod True --encoded_size 24 --ls_mean_activ None --adv_train True --cGAN True --batch_size 8 --epochs 140 --epoch_decay 140 --lr 0.001 --ls_reg_weight 4e-6
+python train-IDEAL-GAN.py --dataset GAN-700 --rand_ne True --only_mag True --div_decod True --encoded_size 24 --ls_mean_activ None --batch_size 8 --epochs 140 --epoch_decay 140 --lr 0.001 --A_loss pix-wise --ls_reg_weight 4e-6
