da9f633b9b0a34f236a0c2a40c72f48cc9dbd205
jpmeneses <jpmeneses@uc.cl>
2023-12-10 22:50:51 +1100

    Submit VAE-GAN(v700): Train standard VAE-GAN with mag-and-phase decoders

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v412-GAN
-#SBATCH --output=out_GAN_412.txt
+#SBATCH --job-name=v700-GAN
+#SBATCH --output=out_GAN_700.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl	
 
-python train-IDEAL-GAN.py --dataset GAN-412 --rand_ne True --n_downsamplings 3 --div_decod True --encoded_size 3 --ls_mean_activ None --adv_train True --cGAN True --batch_size 8 --epochs 140 --epoch_decay 140 --lr 0.001 --ls_reg_weight 4e-6
+python train-IDEAL-GAN.py --dataset GAN-700 --rand_ne True --only_mag True --div_decod True --encoded_size 24 --ls_mean_activ None --adv_train True --cGAN True --batch_size 8 --epochs 140 --epoch_decay 140 --lr 0.001 --ls_reg_weight 4e-6
