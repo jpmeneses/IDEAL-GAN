8e5ea9cda4d3cb8ebe5e39b9bd724251d5706451
jpc <jpmenesescasanova@gmail.com>
2023-12-11 21:04:16 +1100

    Submit VAE-GAN(v506): Train standard VAE-GAN with 12x12x24 LS

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v700-GAN
-#SBATCH --output=out_GAN_700.txt
+#SBATCH --job-name=v506-GAN
+#SBATCH --output=out_GAN_506.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
-#SBATCH --mail-user=jpmeneses@uc.cl	
+#SBATCH --mail-user=jpmeneses@uc.cl
 
-python train-IDEAL-GAN.py --dataset GAN-700 --rand_ne True --only_mag True --div_decod True  --encoded_size 24 --ls_mean_activ None --adv_train True --cGAN True  --batch_size 8 --epochs 140 --epoch_decay 140 --lr 0.001 --A_loss pix-wise --ls_reg_weight 4e-6
+python train-IDEAL-GAN.py --dataset GAN-506 --data_size 384 --rand_ne True --n_downsamplings 5 --div_decod True  --encoded_size 24 --ls_mean_activ None --adv_train True --cGAN True  --batch_size 2 --epochs 140 --epoch_decay 140 --lr 0.00025 --ls_reg_weight 3e-6
