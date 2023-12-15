88333de86952be4f7d4790c7bcfb8b6df72d0119
jpmen <jpmeneses@uc.cl>
2023-12-15 12:42:38 +1100

    Submit VAE-GAN(v508): Train VAE-GAN with large-scale data and a 48x48x3 LS

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v507-GAN
-#SBATCH --output=out_GAN_507.txt
+#SBATCH --job-name=v508-GAN
+#SBATCH --output=out_GAN_508.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl
 
-python train-IDEAL-GAN.py --dataset GAN-507 --data_size 384 --rand_ne True --n_downsamplings 4 --div_decod True  --encoded_size 3 --ls_mean_activ None --adv_train True --cGAN True  --batch_size 2 --epochs 140 --epoch_decay 140 --lr 0.00025 --ls_reg_weight 1e-6
+python train-IDEAL-GAN.py --dataset GAN-508 --data_size 384 --rand_ne True --n_downsamplings 3 --div_decod True  --encoded_size 3 --ls_mean_activ None --adv_train True --cGAN True  --batch_size 2 --epochs 140 --epoch_decay 140 --lr 0.00025 --ls_reg_weight 5e-6
