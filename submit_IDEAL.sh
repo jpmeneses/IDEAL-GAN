c7520dc4758d2293b7b010235393d2fdd63c2d3e
jpmen <jpmeneses@uc.cl>
2023-12-16 19:11:59 +1100

    Submit VAE-GAN(v704): Train with full loss, decreased B-loss weight and decreased phase-decoder size

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v508-GAN
-#SBATCH --output=out_GAN_508.txt
+#SBATCH --job-name=v704-GAN
+#SBATCH --output=out_GAN_704.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl
 
-python train-IDEAL-GAN.py --dataset GAN-508 --data_size 384 --rand_ne True --n_downsamplings 3 --div_decod True  --encoded_size 3 --ls_mean_activ None --adv_train True --cGAN True  --batch_size 2 --epochs 140 --epoch_decay 140 --lr 0.00025 --ls_reg_weight 5e-6
+python train-IDEAL-GAN.py --dataset GAN-704 --rand_ne True --only_mag True --div_decod True  --encoded_size 24 --ls_mean_activ None --adv_train True --cGAN True --batch_size 8 --epochs 140 --epoch_decay 140 --lr 0.001 --B_loss_weight 0.05 --ls_reg_weight 2e-6
