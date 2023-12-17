a96ab4fa643c73bf84fb7ec8714b53c4c33ef3a2
jpmen <jpmeneses@uc.cl>
2023-12-17 23:14:29 +1100

    Submit KL-LDM(v508): Train LDM with cosine beta scheduler and 100 time steps for 2000 epochs

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v507-LDM
-#SBATCH --output=out_LDM_507.txt
+#SBATCH --job-name=v508-LDM
+#SBATCH --output=out_LDM_508.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl
 
-python train-ldm.py --experiment_dir GAN-507 --scheduler cosine --n_timesteps 1000 --batch_size 2 --epochs_ldm 2000 --epoch_ldm_ckpt 100
+python train-ldm.py --experiment_dir GAN-508 --scheduler cosine --n_timesteps 1000 --batch_size 2 --epochs_ldm 2000 --epoch_ldm_ckpt 100
