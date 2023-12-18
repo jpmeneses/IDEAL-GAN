273e124c4ecac7634c478eb23f0f22e288ca99a9
jpmen <jpmeneses@uc.cl>
2023-12-18 20:06:44 +1100

    Submit VAE-GAN(v707): Run with magnitude and phase decoders, and customized smaller num of filters

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v508-LDM
-#SBATCH --output=out_LDM_508.txt
+#SBATCH --job-name=v707-GAN
+#SBATCH --output=out_GAN_707.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl
 
-python train-ldm.py --experiment_dir GAN-508 --scheduler cosine --n_timesteps 1000 --batch_size 2 --epochs_ldm 2000 --epoch_ldm_ckpt 100 --lr 2e-5
+python train-IDEAL-GAN.py --dataset GAN-707 --rand_ne True --only_mag True --n_G_filt_list 36,72,144,144,288 --div_decod True  --encoded_size 24 --ls_mean_activ None --adv_train True --cGAN True --batch_size 8 --epochs 140 --epoch_decay 140 --lr 0.001 --ls_reg_weight 2e-6
