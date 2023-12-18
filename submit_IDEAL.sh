fd642196c82e81566f64739ee2c40737e53d06fd
jpmen <jpmeneses@uc.cl>
2023-12-19 09:22:34 +1100

    Submit VAE-GAN(v708): Enable phase-augmentation and slightly decrease KL-reg weight

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v707-GAN
-#SBATCH --output=out_GAN_707.txt
+#SBATCH --job-name=v708-GAN
+#SBATCH --output=out_GAN_708.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl
 
-python train-IDEAL-GAN.py --dataset GAN-707 --rand_ne True --only_mag True --n_G_filt_list 36,72,144,144,288 --div_decod True  --encoded_size 24 --ls_mean_activ None --adv_train True --cGAN True --batch_size 8 --epochs 140 --epoch_decay 140 --lr 0.001 --FM_loss_weight 0.5 --ls_reg_weight 2e-6
+python train-IDEAL-GAN.py --dataset GAN-708 --rand_ne True --rand_ph_offset True --only_mag True --n_G_filt_list 36,72,144,144,288 --div_decod True  --encoded_size 24 --ls_mean_activ None --adv_train True --cGAN True --batch_size 8 --epochs 140 --epoch_decay 140 --lr 0.001 --FM_loss_weight 0.5 --ls_reg_weight 1e-6
