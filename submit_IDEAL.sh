6f3882988ddf3224958392207be357884ea66a2c
jpmen <jpmeneses@uc.cl>
2023-12-18 20:12:33 +1100

    Re-submit VAE-GAN(v707): Decrease phase-only B-loss weight

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -7,4 +7,4 @@
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl
 
-python train-IDEAL-GAN.py --dataset GAN-707 --rand_ne True --only_mag True --n_G_filt_list 36,72,144,144,288 --div_decod True  --encoded_size 24 --ls_mean_activ None --adv_train True --cGAN True --batch_size 8 --epochs 140 --epoch_decay 140 --lr 0.001 --ls_reg_weight 2e-6
+python train-IDEAL-GAN.py --dataset GAN-707 --rand_ne True --only_mag True --n_G_filt_list 36,72,144,144,288 --div_decod True  --encoded_size 24 --ls_mean_activ None --adv_train True --cGAN True --batch_size 8 --epochs 140 --epoch_decay 140 --lr 0.001 --FM_loss_weight 0.5 --ls_reg_weight 2e-6
