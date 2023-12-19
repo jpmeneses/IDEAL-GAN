c8053ad4e057ae1c356e32fdfa48ec1c12c03fa9
jpmen <jpmeneses@uc.cl>
2023-12-20 10:30:26 +1100

    Submit VAE-GAN(v709): Same as v708, but with WeightNorm enabled

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v509-GAN
-#SBATCH --output=out_GAN_509.txt
+#SBATCH --job-name=v709-GAN
+#SBATCH --output=out_GAN_709.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl
 
-python train-IDEAL-GAN.py --dataset GAN-509 --data_size 384 --rand_ne True --rand_ph_offset True --n_G_filt_list 36,72,72,144,144,288 --n_downsamplings 5 --div_decod True  --encoded_size 24 --ls_mean_activ None --adv_train True --cGAN True  --batch_size 2 --epochs 140 --epoch_decay 140 --lr 0.00025 --ls_reg_weight 2e-6
+python train-IDEAL-GAN.py --dataset GAN-709 --rand_ne True --rand_ph_offset True --only_mag True --n_G_filt_list 36,72,144,144,288 --div_decod True  --encoded_size 24 --ls_mean_activ None --adv_train True --cGAN True --batch_size 8 --epochs 140 --epoch_decay 140 --lr 0.001 --FM_loss_weight 0.5 --ls_reg_weight 1e-6
