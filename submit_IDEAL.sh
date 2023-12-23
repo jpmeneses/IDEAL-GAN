09d4afe9a53e581d142c510004038e90ff891ab0
jpmen <jpmeneses@uc.cl>
2023-12-23 22:45:18 +1100

    Re-submit VAE-GAN(v712): Debug num. of filters

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v714-GAN
-#SBATCH --output=out_GAN_714.txt
+#SBATCH --job-name=v712-GAN
+#SBATCH --output=out_GAN_712.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl
 
- python train-IDEAL-GAN.py --dataset GAN-714 --data_size 384 --rand_ne True --rand_ph_offset True --only_mag True --n_G_filt_list 36,72,72,144 --n_downsamplings 4 --div_decod True  --encoded_size 3 --ls_mean_activ None --adv_train True --cGAN True --batch_size 2 --epochs 140 --epoch_decay 140 --lr 0.001 --FM_loss_weight 0.5 --ls_reg_weight 1e-6
+python train-IDEAL-GAN.py --dataset GAN-712 --data_size 384 --rand_ne True --rand_ph_offset True --only_mag True --n_G_filt_list 36,72,72,144,144,288 --n_downsamplings 5 --div_decod True  --encoded_size 24 --ls_mean_activ None --adv_train True --cGAN True --batch_size 2 --epochs 140 --epoch_decay 140 --lr 0.001 --FM_loss_weight 0.5 --ls_reg_weight 1.5e-6
