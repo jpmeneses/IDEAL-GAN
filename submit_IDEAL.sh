9b1dfcdda176bad7a1471da0e1720ab7f6512bd0
jpmen <jpmeneses@uc.cl>
2024-01-15 16:10:08 +1100

    Submit Sup-301: Train with synthetically generated data for TEs (initial/step) of 2.3/2.3 ms

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v300-sup
-#SBATCH --output=out_sup_300.txt
+#SBATCH --job-name=v301-sup
+#SBATCH --output=out_sup_301.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl
 
-python train-sup.py --dataset Sup-300 --data_size 384 --DL_gen True --DL_experiment_dir output/GAN-715 --n_per_epoch 5000 --G_model U-Net
+python train-sup.py --dataset Sup-301 --data_size 384 --DL_gen True --DL_experiment_dir output/GAN-715 --n_per_epoch 5000 --TE1 0.0023 --dTE 0.0023 --G_model U-Net
