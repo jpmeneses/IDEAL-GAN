eb9ffc8b4bdf306b5ea98631cc5d2bb9385b90a6
jpmen <jpmeneses@uc.cl>
2024-01-15 13:57:33 +1100

    Submit VAE-GAN(v720): Same as v713, but with smaller decoders

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v719-GAN
-#SBATCH --output=out_GAN_719.txt
+#SBATCH --job-name=v720-GAN
+#SBATCH --output=out_GAN_720.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl
 
-python train-IDEAL-GAN.py --dataset GAN-719 --data_size 384 --rand_ne True --rand_ph_offset True --only_mag True --n_G_filt_list 36,72,72,144,144,288 --n_downsamplings 5 --div_decod True  --encoded_size 24 --ls_mean_activ None --adv_train True --cGAN True --batch_size 2 --epochs 140 --epoch_decay 140 --lr 0.001 --A_loss_weight 0.008 --FM_loss_weight 0.5 --ls_reg_weight 2e-6 --Fourier_reg_weight 0.002
+python train-IDEAL-GAN.py --dataset GAN-720 --data_size 384 --rand_ne True --rand_ph_offset True --only_mag True --n_G_filt_list 36,72,72,144 --n_downsamplings 3 --div_decod True  --encoded_size 3 --ls_mean_activ None --adv_train True --cGAN True --batch_size 2 --epochs 140 --epoch_decay 140 --lr 0.001 --FM_loss_weight 0.5 --ls_reg_weight 3e-6
