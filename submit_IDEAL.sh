65dcfd5baf5e820a3e488ccbd9a4ae2077674649
jpmen <jpmeneses@uc.cl>
2024-01-15 14:15:04 +1100

    Submit Sup-300: Train with synthetically generated data and standard TEs

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v720-GAN
-#SBATCH --output=out_GAN_720.txt
+#SBATCH --job-name=v300-sup
+#SBATCH --output=out_sup_300.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl
 
-python train-IDEAL-GAN.py --dataset GAN-720 --data_size 384 --rand_ne True --rand_ph_offset True --only_mag True --n_G_filt_list 36,72,72,144 --n_downsamplings 3 --div_decod True  --encoded_size 3 --ls_mean_activ None --adv_train True --cGAN True --batch_size 2 --epochs 140 --epoch_decay 140 --lr 0.001 --FM_loss_weight 0.5 --ls_reg_weight 3e-6
+python train-sup.py --dataset Sup-300 --data_size 384 --DL_gen True --DL_experiment_dir output/GAN-715 --n_per_epoch 5000 --G_model U-Net
