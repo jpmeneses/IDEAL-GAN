ab412830f567b674fe5b7f5c08373467998f0304
jpmen <jpmeneses@uc.cl>
2024-01-15 16:44:22 +1100

    Re-submit VAE-GAN(v719): Return to reference A-loss weight and significantly decrease Fourier loss weight

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v301-sup
-#SBATCH --output=out_sup_301.txt
+#SBATCH --job-name=v719-GAN
+#SBATCH --output=out_GAN_719.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl
 
-python train-sup.py --dataset Sup-301 --data_size 384 --DL_gen True --DL_experiment_dir output/GAN-715 --n_per_epoch 5000 --TE1 0.0023 --dTE 0.0023 --G_model U-Net
+python train-IDEAL-GAN.py --dataset GAN-719 --data_size 384 --rand_ne True --rand_ph_offset True --only_mag True --n_G_filt_list 36,72,72,144,144,288 --n_downsamplings 5 --div_decod True  --encoded_size 24 --ls_mean_activ None --adv_train True --cGAN True --batch_size 2 --epochs 140 --epoch_decay 140 --lr 0.001 --FM_loss_weight 0.5 --ls_reg_weight 2e-6 --Fourier_reg_weight 2e-4
