b301c3e54b46f5b304c3803d527bbb2958364c13
jpmen <jpmeneses@uc.cl>
2024-01-16 18:32:21 +1100

    Submit Sup-303: Same as Sup-302, but for TE1(dTE) of 1.4(2.2) ms

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v302-sup
-#SBATCH --output=out_sup_302.txt
+#SBATCH --job-name=v303-sup
+#SBATCH --output=out_sup_303.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl
 
-python train-sup.py --dataset Sup-302 --data_size 384 --DL_gen True --DL_experiment_dir output/GAN-715 --n_per_epoch 5000 --TE1 0.0023 --dTE 0.0023 --G_model U-Net --epochs 60 --epoch_decay 60
+python train-sup.py --dataset Sup-303 --data_size 384 --DL_gen True --DL_experiment_dir output/GAN-715 --n_per_epoch 5000 --TE1 0.0014 --dTE 0.0022 --G_model U-Net --epochs 60 --epoch_decay 60
