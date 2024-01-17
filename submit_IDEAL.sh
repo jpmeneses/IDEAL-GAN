a95bca9215a3e3e6b0f65eab4d81f6a0c79ddac4
jpmen <jpmeneses@uc.cl>
2024-01-17 14:16:31 +1100

    Submit KL-LDM(v720): Train ckpt-100 using mag-and-phase encod-decod and cosine beta scheduler

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v721-GAN
-#SBATCH --output=out_GAN_721.txt
+#SBATCH --job-name=v720-LDM
+#SBATCH --output=out_LDM_720.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl
 
-python train-IDEAL-GAN.py --dataset GAN-721 --data_size 384 --rand_ne True --rand_ph_offset True --only_mag True --rem_R2 True --n_G_filt_list 36,72,72,144,144,288 --n_downsamplings 5 --encoded_size 24 --ls_mean_activ None --adv_train True --cGAN True --batch_size 2 --epochs 140 --epoch_decay 140 --lr 0.001 --FM_loss_weight 0.5 --ls_reg_weight 2e-6 --Fourier_reg_weight 2e-4
+python train-ldm.py --experiment_dir GAN-720 --scheduler cosine --n_timesteps 1000 --batch_size 8 --epochs_ldm 2000 --epoch_ldm_ckpt 100 --lr 7e-5
