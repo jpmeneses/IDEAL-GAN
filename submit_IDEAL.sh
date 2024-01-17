fd414e5c8896af8b720f49c69fb97a69ee6297bf
jpmen <jpmeneses@uc.cl>
2024-01-18 10:36:03 +1100

    Re-submit KL-LDM(v716): Corrected ckpt, mag-and-phase encod-decod, and cosine beta scheduler

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v720-LDM
-#SBATCH --output=out_LDM_720.txt
+#SBATCH --job-name=v716-LDM
+#SBATCH --output=out_LDM_716.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl
 
-python train-ldm.py --experiment_dir GAN-720 --scheduler cosine --n_timesteps 1000 --batch_size 8 --epochs_ldm 2000 --epoch_ldm_ckpt 100 --lr 7e-5
+python train-ldm.py --experiment_dir output/GAN-716 --scheduler cosine --n_timesteps 1000 --batch_size 8 --epochs_ldm 2000 --epoch_ldm_ckpt 100 --lr 7e-5
