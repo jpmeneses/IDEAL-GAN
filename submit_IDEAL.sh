edb1fc7771cf8719583a406c1e6e29b8cc063d77
jpmen <jpmeneses@uc.cl>
2024-01-18 18:18:43 +1100

    Re-submit Sup-203: 3-echo U-Net trained with 384x384 data

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v204-sup
-#SBATCH --output=out_sup_204.txt
+#SBATCH --job-name=v203-sup
+#SBATCH --output=out_sup_203.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl
 
-python train-sup.py --dataset Sup-204 --out_vars 'WF-PM' --n_G_filters 36 --batch_size 1 --epoch_ckpt 20 --lr 0.0001
+python train-sup.py --dataset Sup-203 --n_echoes 3 --out_vars 'WF' --G_model 'U-Net' --n_G_filters 36 --batch_size 1 --epoch_ckpt 20 --lr 0.0001
