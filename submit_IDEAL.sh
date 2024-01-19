d257c162059fb109fd020736d28eef838b573ad9
jpmen <jpmeneses@uc.cl>
2024-01-19 17:51:28 +1100

    Submit KL-LDM(v716c): Variation of v716 - Decreased beta-scheduler time-steps

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v716b-LDM
-#SBATCH --output=out_LDM_716b.txt
+#SBATCH --job-name=v716c-LDM
+#SBATCH --output=out_LDM_716c.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl
 
-python train-ldm.py --experiment_dir output/GAN-716b --scheduler cosine --n_timesteps 1000 --batch_size 8 --epochs_ldm 500 --epoch_ldm_ckpt 100 --lr 1e-4
+python train-ldm.py --experiment_dir output/GAN-716c --scheduler cosine --n_timesteps 500 --batch_size 8 --epochs_ldm 500 --epoch_ldm_ckpt 100 --lr 7e-5
