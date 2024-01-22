d9941e4d6270b35ada92207743cee3d2cdf446e5
jpmen <jpmeneses@uc.cl>
2024-01-22 12:19:16 +1100

    Debug - Enable LDM-based data generation

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -7,4 +7,4 @@
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl
 
-python train-sup.py --dataset Sup-304 --data_size 384 --DL_gen True --DL_experiment_dir output/GAN-716c --n_per_epoch 5000 --TE1 0.0014 --dTE 0.0022 --G_model U-Net --batch_size 16 --epochs 50 --epoch_decay 50
+python train-sup.py --dataset Sup-304 --data_size 384 --DL_gen True --DL_LDM True --DL_experiment_dir output/GAN-716c --n_per_epoch 5000 --TE1 0.0014 --dTE 0.0022 --G_model U-Net --batch_size 16 --epochs 50 --epoch_decay 50
