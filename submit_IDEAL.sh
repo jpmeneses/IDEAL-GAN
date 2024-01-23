418b7265274d67f8cee60c7e33c6ca4268e3e42b
jpmen <jpmeneses@uc.cl>
2024-01-23 15:42:13 +1100

    Submit KL-LDM(v722): LDM with 500 diffusion time-steps

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v723-GAN
-#SBATCH --output=out_GAN_723.txt
+#SBATCH --job-name=v722-LDM
+#SBATCH --output=out_LDM_722.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl
 
-python train-IDEAL-GAN.py --dataset GAN-723 --data_size 384 --rand_ne True --rand_ph_offset True --only_mag True --n_G_filt_list 36,72,72,144,144 --n_downsamplings 4 --div_decod True  --encoded_size 3 --ls_mean_activ None --adv_train True --cGAN True --batch_size 2 --epochs 200 --epoch_decay 200 --lr 0.001 --A_loss_weight 0.005 --FM_loss_weight 0.667 --ls_reg_weight 5e-7
+python train-ldm.py --experiment_dir output/GAN-722 --scheduler cosine --n_timesteps 500 --batch_size 8 --epochs_ldm 500 --epoch_ldm_ckpt 100 --lr 7e-5
