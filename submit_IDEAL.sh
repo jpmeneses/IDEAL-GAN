789e7b283ca9c486777f4982bc9e7d246550b15c
jpmen <jpmeneses@uc.cl>
2024-01-23 12:41:42 +1100

    Submit VAE-GAN(v723): LS-shape of 24x24x3, decreased A-loss and KL weights

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v305-sup
-#SBATCH --output=out_sup_305.txt
+#SBATCH --job-name=v723-GAN
+#SBATCH --output=out_GAN_723.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl
 
-python train-sup.py --dataset Sup-305 --data_size 384 --DL_gen True --DL_LDM True --DL_experiment_dir output/GAN-716c --n_per_epoch 5000 --TE1 0.0013 --dTE 0.0021 --G_model U-Net --batch_size 16 --epochs 50 --epoch_decay 50 --lr 0.001
+python train-IDEAL-GAN.py --dataset GAN-723 --data_size 384 --rand_ne True --rand_ph_offset True --only_mag True --n_G_filt_list 36,72,72,144,144 --n_downsamplings 4 --div_decod True  --encoded_size 3 --ls_mean_activ None --adv_train True --cGAN True --batch_size 2 --epochs 200 --epoch_decay 200 --lr 0.001 --A_loss_weight 0.005 --FM_loss_weight 0.667 --ls_reg_weight 5e-7
