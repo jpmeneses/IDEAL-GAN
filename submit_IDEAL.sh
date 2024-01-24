4e5822e3a734d807a72120c34a095860733938f9
jpmen <jpmeneses@uc.cl>
2024-01-24 22:19:23 +1100

    Submit VAE-GAN(v723): Same as v716, with 3 down-samplings, but decreased KL-reg weight

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v722-LDM
-#SBATCH --output=out_LDM_722.txt
+#SBATCH --job-name=v724-GAN
+#SBATCH --output=out_GAN_724.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl
 
-python train-ldm.py --experiment_dir output/GAN-722 --scheduler cosine --n_timesteps 500 --batch_size 8 --epochs_ldm 500 --epoch_ldm_ckpt 100 --lr 7e-5
+python train-IDEAL-GAN.py --dataset GAN-724 --data_size 384 --rand_ne True --rand_ph_offset True --only_mag True --n_G_filt_list 36,72,72,144 --n_downsamplings 3 --div_decod True  --encoded_size 3 --ls_mean_activ None --adv_train True --cGAN True --batch_size 2 --epochs 140 --epoch_decay 140 --lr 0.001 --FM_loss_weight 0.5 --ls_reg_weight 5e-7
