93d6c1f3aa250713f1ecc0f2b5e8ae634fcbc451
jpmen <jpmeneses@uc.cl>
2024-01-25 13:44:19 +1100

    Submit KL-LDM(v716c): Variation of v716 - Linear beta-scheduler

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v306-sup
-#SBATCH --output=out_sup_306.txt
+#SBATCH --job-name=v716d-LDM
+#SBATCH --output=out_LDM_716d.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl
 
-python train-sup.py --dataset Sup-306 --data_size 384 --DL_gen True --DL_experiment_dir output/GAN-722b --n_per_epoch 5000 --DL_LDM True --DDIM True --TE1 0.0013 --dTE 0.0021 --G_model U-Net --batch_size 16 --epochs 50 --epoch_decay 50 --lr 0.001
+python train-ldm.py --experiment_dir output/GAN-716d --n_timesteps 500 --batch_size 8 --epochs_ldm 300 --epoch_ldm_ckpt 100 --lr 7e-5
