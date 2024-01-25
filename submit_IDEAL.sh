a9ec9d3eb0aedc59f483cf7c7db5c7842954b1bf
jpmen <jpmeneses@uc.cl>
2024-01-25 22:40:32 +1100

    Submit Sup-307: U-Net trained for TE base (step) of 2.3 (2.3) ms using LDM and DDIM-based data generation

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v716d-LDM
-#SBATCH --output=out_LDM_716d.txt
+#SBATCH --job-name=v307-sup
+#SBATCH --output=out_sup_307.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl
 
-python train-ldm.py --experiment_dir output/GAN-716d --n_timesteps 500 --batch_size 8 --epochs_ldm 300 --epoch_ldm_ckpt 100 --lr 7e-5
+python train-sup.py --dataset Sup-307 --data_size 384 --DL_gen True --DL_experiment_dir output/GAN-722b --n_per_epoch 5000 --DL_LDM True --DDIM True --TE1 0.0023 --dTE 0.0023 --G_model U-Net --batch_size 16 --epochs 50 --epoch_decay 50 --lr 0.001
