547714c86a46c9a93c84759a19a73dc2f2ff9852
jpmen <jpmeneses@uc.cl>
2024-03-01 16:00:58 -0300

    Submit VAE-GAN(v725): Try with fully-unsupervised training and no R2s signal decay

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v801-GAN
-#SBATCH --output=out_GAN_801.txt
+#SBATCH --job-name=v725-GAN
+#SBATCH --output=out_GAN_725.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl
 
-python train-IDEAL-GAN.py --dataset GAN-801 --data_size 384 --rand_ne False --rand_ph_offset False --only_mag True --n_G_filt_list 36,72,72,144 --n_downsamplings 3 --div_decod True  --encoded_size 3 --adv_train True --cGAN True --batch_size 2 --epochs 200 --epoch_decay 200 --epoch_ckpt 50 --lr 0.001 --FM_loss_weight 0.5 --ls_reg_weight 1e-6
+python train-IDEAL-GAN.py --dataset GAN-725 --data_size 384 --rand_ne True --rand_ph_offset True --only_mag True --no_GC True --rem_R2 True --n_G_filt_list 36,72,72,144 --n_downsamplings 3 --div_decod True  --encoded_size 3 --adv_train True --cGAN True --batch_size 2 --epochs 20 --epoch_decay 20 --epoch_ckpt 5 --lr 0.001 --FM_loss_weight 0.5 --ls_reg_weight 1e-6
