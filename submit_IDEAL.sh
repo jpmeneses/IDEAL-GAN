2dec41d471344ee60b92b3230a035828ff26c63d
jpmen <jpmeneses@uc.cl>
2024-03-01 13:26:32 -0300

    Submit VAE-GAN(v800): Train VAE with 3-DS and enabled NE and phase augmentation

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v307-sup
-#SBATCH --output=out_sup_307.txt
+#SBATCH --job-name=v800-GAN
+#SBATCH --output=out_GAN_800.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl
 
-python train-sup.py --dataset Sup-307 --data_size 384 --DL_gen True --DL_experiment_dir output/GAN-722b --n_per_epoch 5000 --DL_LDM True --DDIM True --TE1 0.0023 --dTE 0.0023 --G_model U-Net --batch_size 16 --epochs 50 --epoch_decay 50 --lr 0.001
+python train-IDEAL-GAN.py --dataset GAN-800 --data_size 384 --rand_ne True --rand_ph_offset True --only_mag True --n_G_filt_list 36,72,72,144 --n_downsamplings 3 --div_decod True  --encoded_size 3 --adv_train True --cGAN True --batch_size 2 --epochs 200 --epoch_decay 200 --epoch_ckpt 50 --lr 0.001 --FM_loss_weight 0.5 --ls_reg_weight 1e-6
