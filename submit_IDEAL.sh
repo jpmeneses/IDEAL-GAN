1fddd684c39496b51be98e3975c4acb589d01f4c
jpmen <jpmeneses@uc.cl>
2024-03-13 12:43:57 -0300

    Re-submit Sup-204: Train 6-echo MDWF-Net with LS data

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v203-sup
-#SBATCH --output=out_sup_203.txt
+#SBATCH --job-name=v204-sup
+#SBATCH --output=out_sup_204.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl
 
-python train-sup.py --dataset Sup-203 --data_size 384 --n_echoes 3 --G_model U-Net --n_G_filters 36 --lr 0.0001
+python train-sup.py --dataset Sup-204 --data_size 384 --out_vars WF-PM --n_G_filters 36 --lr 0.0001
