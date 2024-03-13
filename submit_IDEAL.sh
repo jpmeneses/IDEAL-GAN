fbef8270a20a9c53c79e85335d9336076da7bddb
jpmen <jpmeneses@uc.cl>
2024-03-13 12:26:00 -0300

    Re-submit Sup-202: Train 6-echo U-Net with LS data

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v803-GAN
-#SBATCH --output=out_GAN_803.txt
+#SBATCH --job-name=v202-sup
+#SBATCH --output=out_sup_202.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl
 
-python train-IDEAL-GAN.py --dataset GAN-803 --data_size 384 --rand_ne False --rand_ph_offset True --only_mag True --n_G_filt_list 36,72,72,144 --n_downsamplings 3 --div_decod True  --encoded_size 3 --adv_train True --cGAN True --batch_size 2 --epochs 200 --epoch_decay 200 --epoch_ckpt 50 --lr 0.001 --FM_loss_weight 0.5 --ls_reg_weight 1e-6
+python train-sup.py --dataset Sup-202 --data_size 384 --G_model U-Net --n_G_filters 36 --lr 0.0001
