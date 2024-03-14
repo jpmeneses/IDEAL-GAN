a857aee0be0fc72707c24ab0994649d584b8af15
jpmen <jpmeneses@uc.cl>
2024-03-14 10:56:41 -0300

    Re-submit Sup-200: Train 6-echo 2D-Net with LS data

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v205-sup
-#SBATCH --output=out_sup_205.txt
+#SBATCH --job-name=v200-sup
+#SBATCH --output=out_sup_200.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl
 
-python train-sup.py --dataset Sup-205 --data_size 384 --n_echoes 3 --out_vars WF-PM --n_G_filters 36 --lr 0.0001
+python train-sup.py --dataset Sup-200 --data_size 384 --out_vars PM --n_G_filters 36 --lr 0.0001
