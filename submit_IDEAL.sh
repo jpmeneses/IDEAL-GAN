4cd4090c980fcc6597d437f36bf53ed2037d2446
jpmen <jpmeneses@uc.cl>
2024-03-19 17:47:12 -0300

    Re-submit KL-LDM(v806a)

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v806-LDM
-#SBATCH --output=out_LDM_806.txt
+#SBATCH --job-name=v806a-LDM
+#SBATCH --output=out_LDM_806a.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl
 
-python train-ldm.py --experiment_dir output/GAN-806 --n_timesteps 500 --batch_size 8 --epochs_ldm 300 --epoch_ldm_ckpt 100 --lr 7e-5
+python train-ldm.py --experiment_dir output/GAN-806a --n_timesteps 500 --batch_size 8 --epochs_ldm 300 --epoch_ldm_ckpt 100 --lr 7e-5
