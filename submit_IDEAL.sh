f5e0590c43b12148229137dc79f86b418c356dc5
jpmen <jpmeneses@uc.cl>
2024-03-22 10:22:01 -0300

    Submit KL-LDM(v807a): LDM from ep-150 PI-VAE with linear beta scheduler

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v807-LDM
-#SBATCH --output=out_LDM_807.txt
+#SBATCH --job-name=v807a-LDM
+#SBATCH --output=out_LDM_807a.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl
 
-python train-ldm.py --experiment_dir output/GAN-807 --n_timesteps 500 --batch_size 8 --epochs_ldm 300 --epoch_ldm_ckpt 100 --lr 7e-5
+python train-ldm.py --experiment_dir output/GAN-807a --n_timesteps 500 --batch_size 8 --epochs_ldm 300 --epoch_ldm_ckpt 100 --lr 7e-5
