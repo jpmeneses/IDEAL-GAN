9d3c78796b6694e67b6db94b736f26d1e73def61
jpmen <jpmeneses@uc.cl>
2024-03-26 16:20:38 -0300

    Submit DS-GEN(v807): Generate CSE-MRI and WF samples using PI-LDM(v807)

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v810-GAN
-#SBATCH --output=out_GAN_810.txt
+#SBATCH --job-name=v807-DS
+#SBATCH --output=out_DS_807.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl
 
-python train-IDEAL-GAN.py --dataset GAN-810 --data_size 384 --rand_ne True --rand_ph_offset False --only_mag True --n_G_filt_list 36,72,72,144,144 --n_downsamplings 4 --div_decod True  --encoded_size 3 --adv_train True --cGAN True --batch_size 2 --epochs 200 --epoch_decay 200 --epoch_ckpt 50 --lr 0.001 --FM_loss_weight 0.5 --ls_reg_weight 5e-7
+python gen_LDM_dataset.py --experiment_dir output/GAN-807 --batch_size 12 --n_samples 2400
