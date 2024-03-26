ecbcc3b33a0904a22867254f28484b451210e509
jpmen <jpmeneses@uc.cl>
2024-03-26 10:27:17 -0300

    Submit VAE-GAN(v810): Same as v807 (unwrapped phase, KLw, NE-aug) but 4 down-samplings

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v809-GAN
-#SBATCH --output=out_GAN_809.txt
+#SBATCH --job-name=v810-GAN
+#SBATCH --output=out_GAN_810.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl
 
-python train-IDEAL-GAN.py --dataset GAN-809 --data_size 384 --rand_ne False --rand_ph_offset False --only_mag True --n_G_filt_list 36,72,72,144 --n_downsamplings 3 --div_decod True  --encoded_size 3 --adv_train True --cGAN True --batch_size 2 --epochs 200 --epoch_decay 200 --epoch_ckpt 50 --lr 0.001 --FM_loss_weight 0.5 --ls_reg_weight 1e-6
+python train-IDEAL-GAN.py --dataset GAN-810 --data_size 384 --rand_ne True --rand_ph_offset False --only_mag True --n_G_filt_list 36,72,72,144,144 --n_downsamplings 4 --div_decod True  --encoded_size 3 --adv_train True --cGAN True --batch_size 2 --epochs 200 --epoch_decay 200 --epoch_ckpt 50 --lr 0.001 --FM_loss_weight 0.5 --ls_reg_weight 5e-7
