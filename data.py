85d21e5acfc0a9cdb7c6220983bb56bc75a40c30
jpmen <jpmeneses@uc.cl>
2024-03-26 15:45:26 -0300

    Add TFRecord feature processing functinos

--- data.py
+++ data.py
@@ -304,3 +304,20 @@ def B_to_MEBCRN(B,mode='WF-PM'):
         B_PM= tf.expand_dims(B_PM,axis=1)
 
         return tf.concat([B_W,B_F,B_PM],axis=1)
+
+
+def _bytes_feature(value):
+    """Returns a bytes_list from a string / byte."""
+    if isinstance(value, type(tf.constant(0))):
+        value = value.numpy() # BytesList won't unpack a string from an EagerTensor.
+    return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
+
+
+def _float_feature(value):
+    """Returns a float_list from a float / double."""
+    return tf.train.Feature(float_list=tf.train.FloatList(value=value))
+
+
+def _int64_feature(value):
+    """Returns an int64_list from a bool / enum / int / uint."""
+    return tf.train.Feature(int64_list=tf.train.Int64List(value=value))
