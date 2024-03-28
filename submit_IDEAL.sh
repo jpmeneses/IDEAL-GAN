edcf6a59484961f8252da5b13c3c168356f8b94b
jpmen <jpmeneses@uc.cl>
2024-03-28 16:48:45 -0300

    Re-submit Sup-308: U-Net trained for standard TEs using previously generated LDM data with non-MEBCRN format

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v807-DS
-#SBATCH --output=out_DS_807.txt
+#SBATCH --job-name=v308-sup
+#SBATCH --output=out_sup_308.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl
 
-python gen_LDM_dataset.py --experiment_dir output/GAN-807 --ds_filename 'LDM_ds_noMEBCRN' --MEBCRN False --batch_size 10 --n_samples 3330
+python train-sup.py --dataset Sup-308 --data_size 384 --DL_gen True --DL_filename LDM_ds_noMEBCRN --G_model U-Net --batch_size 16 --epochs 50 --epoch_decay 50 --lr 0.001
