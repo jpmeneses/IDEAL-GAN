fc62ef6f0dab2c9c3576526def7ad3fd2747356c
jpmen <jpmeneses@uc.cl>
2024-03-29 11:21:26 -0300

    Submit Sup-309: Same as v308, but with 5% Gaussian noise addition

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v308-sup
-#SBATCH --output=out_sup_308.txt
+#SBATCH --job-name=v309-sup
+#SBATCH --output=out_sup_309.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl
 
-python train-sup.py --dataset Sup-308 --data_size 384 --DL_gen True --DL_filename LDM_ds_noMEBCRN --G_model U-Net --batch_size 16 --epochs 50 --epoch_decay 50 --lr 0.001
+python train-sup.py --dataset Sup-309 --data_size 384 --DL_gen True --DL_filename LDM_ds_noMEBCRN --sigma_noise 0.05 --G_model U-Net --batch_size 16 --epochs 50 --epoch_decay 50 --lr 0.001
