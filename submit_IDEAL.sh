a582e184923a7ab198b26617607c407f5a65f0dd
jpmen <jpmeneses@uc.cl>
2024-04-01 12:14:40 -0300

    Submit Sup-310: WF-U-Net for standard protocol with 2% noise-addition

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v310-sup
-#SBATCH --output=out_sup_310.txt
+#SBATCH --job-name=v311-sup
+#SBATCH --output=out_sup_311.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl
 
-python train-sup.py --dataset Sup-310 --data_size 384 --DL_gen True --DL_filename LDM_ds_noMEBCRN --TE1 0.0023 --dTE 0.0023 --G_model U-Net --batch_size 16 --epochs 50 --epoch_decay 50 --lr 0.001
+python train-sup.py --dataset Sup-311 --data_size 384 --DL_gen True --DL_filename LDM_ds_noMEBCRN --sigma_noise 0.02 --G_model U-Net --batch_size 16 --epochs 40 --epoch_decay 40 --epoch_ckpt 5 --lr 0.001
