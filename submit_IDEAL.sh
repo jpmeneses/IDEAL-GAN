a4d897bf4d5e09116fb28985e6c603dd3c11340c
jpmen <jpmeneses@uc.cl>
2024-04-01 10:02:30 -0300

    Submit VAE-GAN(v810): Same as v810 (unwrapped phase, 4-DS, NE-aug) but relaxed KL-reg

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v309-sup
-#SBATCH --output=out_sup_309.txt
+#SBATCH --job-name=v810-GAN
+#SBATCH --output=out_GAN_810.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl
 
-python train-sup.py --dataset Sup-309 --data_size 384 --DL_gen True --DL_filename LDM_ds_noMEBCRN --sigma_noise 0.05 --G_model U-Net --batch_size 16 --epochs 50 --epoch_decay 50 --lr 0.001
+python train-IDEAL-GAN.py --dataset GAN-810 --data_size 384 --rand_ne True --rand_ph_offset False --only_mag True --n_G_filt_list 36,72,72,144,144 --n_downsamplings 4 --div_decod True  --encoded_size 3 --adv_train True --cGAN True --batch_size 2 --epochs 200 --epoch_decay 200 --epoch_ckpt 50 --lr 0.001 --FM_loss_weight 0.5 --ls_reg_weight 2e-7
