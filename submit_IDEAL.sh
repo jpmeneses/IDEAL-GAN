2c7ea98cbb8bf3bc8d9d561b00721b5413a4b2a1
jpmen <jpmeneses@uc.cl>
2024-04-02 12:45:49 -0300

    Re-submit Sup-311

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v811-GAN
-#SBATCH --output=out_GAN_811.txt
+#SBATCH --job-name=v311-sup
+#SBATCH --output=out_sup_311.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl
 
-python train-IDEAL-GAN.py --dataset GAN-811 --data_size 384 --rand_ne True --rand_ph_offset False --only_mag True --n_G_filt_list 36,72,72,144,144 --n_downsamplings 4 --div_decod True  --encoded_size 3 --adv_train True --cGAN True --batch_size 2 --epochs 200 --epoch_decay 200 --epoch_ckpt 50 --lr 0.001 --FM_loss_weight 0.5 --ls_reg_weight 2e-7
+python train-sup.py --dataset Sup-311 --data_size 384 --DL_gen True --DL_filename LDM_ds_noMEBCRN --sigma_noise 0.02 --G_model U-Net --batch_size 16 --epochs 40 --epoch_decay 40 --epoch_ckpt 5 --lr 0.001
