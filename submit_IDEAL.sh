92ab5a266ef998ea8aa934ba5710c71b364cee28
jpmen <jpmeneses@uc.cl>
2024-04-03 10:32:42 -0300

    Submit Sup-312: Same as v308 (no added noise), but train for more epochs

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v310-sup
-#SBATCH --output=out_sup_310.txt
+#SBATCH --job-name=v312-sup
+#SBATCH --output=out_sup_312.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl
 
-python train-sup.py --dataset Sup-310 --data_size 384 --DL_gen True --DL_filename LDM_ds_noMEBCRN --sigma_noise 0.02 --TE1 0.0023 --dTE 0.0023 --G_model U-Net --batch_size 16 --lr 0.001
+python train-sup.py --dataset Sup-312 --data_size 384 --DL_gen True --DL_filename LDM_ds_noMEBCRN --G_model U-Net --batch_size 16 --lr 0.001
