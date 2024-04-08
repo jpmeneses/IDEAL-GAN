19d62340903ff02188b94ffa27e6090f29fd29fe
jpmen <jpmeneses@uc.cl>
2024-04-08 13:30:49 -0400

    Submit Unsup-101: Train FM-only model with corrected std-weighted loss for 40 epochs

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v316-sup
-#SBATCH --output=out_sup_316.txt
+#SBATCH --job-name=v101-uns
+#SBATCH --output=out_uns_101.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl
 
-python train-sup.py --dataset Sup-316 --data_size 384 --DL_gen True --DL_filename LDM_ds_noMEBCRN --TE1 0.0012 --dTE 0.0020 --G_model U-Net --batch_size 16 --lr 0.001
+python train-IDEAL-unsup.py --dataset Unsup-101 --UQ True --epochs 40 
