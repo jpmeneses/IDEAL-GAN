ee16122108d3b83e307f927dd02f67add642b5e6
jpmen <jpmeneses@uc.cl>
2024-04-12 12:26:16 -0400

    Submit Sup-317: Train WF U-Net with mixed data (real-and-synth) with 3330 samples

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v103-uns
-#SBATCH --output=out_uns_103.txt
+#SBATCH --job-name=v318-sup
+#SBATCH --output=out_sup_318.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl
 
-python train-IDEAL-unsup.py --dataset Unsup-103 --UQ True 
+python train-sup.py --dataset Sup-318 --data_size 384 --DL_gen True --DL_partial_real True --DL_filename LDM_ds_noMEBCRN_3130 --G_model U-Net --batch_size 16 --lr 0.001
