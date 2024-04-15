27f6b2b6af5d1b550bd7dbb86a35a60ffa531449
jpmen <jpmeneses@uc.cl>
2024-04-15 16:48:38 -0400

    Submit Sup-318: Train WF U-Net with 6660 synthetic samples

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v807-DS
-#SBATCH --output=out_DS_807.txt
+#SBATCH --job-name=v319-sup
+#SBATCH --output=out_sup_319.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl
 
-python gen_LDM_dataset.py --experiment_dir output/GAN-807 --ds_filename 'LDM_ds_noMEBCRN' --MEBCRN False --batch_size 16 --n_samples 6660
+python train-sup.py --dataset Sup-319 --data_size 384 --DL_gen True --DL_filename LDM_ds_noMEBCRN_6660 --G_model U-Net --batch_size 16 --lr 0.001
