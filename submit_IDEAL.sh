c559a04e6b6f3cc0ec0112ccc1c57f16883fb615
jpmen <jpmeneses@uc.cl>
2024-04-15 11:51:12 -0400

    Re-submit DS-GEN(v807): Generate 6660 CSE-MRI and WF samples in non-MEBCRN format using PI-LDM(v807)

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v318-sup
-#SBATCH --output=out_sup_318.txt
+#SBATCH --job-name=v807-DS
+#SBATCH --output=out_DS_807.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl
 
-python train-sup.py --dataset Sup-318 --data_size 384 --DL_gen True --DL_partial_real True --DL_filename LDM_ds_noMEBCRN_3130 --G_model U-Net --batch_size 16 --lr 0.001
+python gen_LDM_dataset.py --experiment_dir output/GAN-807 --ds_filename 'LDM_ds_noMEBCRN' --MEBCRN False --batch_size 16 --n_samples 8000
