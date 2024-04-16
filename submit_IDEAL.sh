b571981f558689c1ec664a1ed1ef7a98713bee0b
jpmen <jpmeneses@uc.cl>
2024-04-16 13:37:54 -0400

    Submit Sup-320: Train WF U-Net with 6660 synthetic samples for TE base (step) of 1.4 (2.2) ms

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v319-sup
-#SBATCH --output=out_sup_319.txt
+#SBATCH --job-name=v320-sup
+#SBATCH --output=out_sup_320.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl
 
-python train-sup.py --dataset Sup-319 --data_size 384 --DL_gen True --DL_filename LDM_ds_noMEBCRN_6660 --shuffle False --G_model U-Net --batch_size 16 --lr 0.001
+python train-sup.py --dataset Sup-320 --data_size 384 --DL_gen True --DL_filename LDM_ds_noMEBCRN_6660 --sigma_noise 0.02 --shuffle False --TE1 0.0014 --dTE 0.0022 --G_model U-Net --batch_size 16 --lr 0.001
