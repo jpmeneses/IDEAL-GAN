db8ef8b413a528f25bd46332daaad3d3271050f4
jpmen <jpmeneses@uc.cl>
2024-04-17 15:40:02 -0400

    Submit Sup-321: Real-data WF U-Net with batch size 16

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v320-sup
-#SBATCH --output=out_sup_320.txt
+#SBATCH --job-name=v321-sup
+#SBATCH --output=out_sup_321.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl
 
-python train-sup.py --dataset Sup-320 --data_size 384 --DL_gen True --DL_filename LDM_ds_noMEBCRN_6660 --sigma_noise 0.02 --shuffle False --TE1 0.0014 --dTE 0.0022 --G_model U-Net --batch_size 16 --lr 0.001
+python train-sup.py --dataset Sup-321 --data_size 384 --G_model U-Net --batch_size 16 --lr 0.001
