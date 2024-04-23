cfe8197bca04310c89ebd9966239d1dd7a5e422f
jpmen <jpmeneses@uc.cl>
2024-04-23 17:01:27 -0400

    Remove Ada-IN from up-sampling layers

--- DLlib/module.py
+++ DLlib/module.py
@@ -537,13 +537,13 @@ def PM_Generator(
             norm=norm
             )
 
-        if te_input:
-            # Fully-connected network for processing the vector with echo-times
-            y2 = keras.layers.Dense(filters,activation='relu',kernel_initializer='he_uniform')(te)
-            y3 = keras.layers.Dense(filters,activation='relu',kernel_initializer='he_uniform')(te)
-            # Adaptive Instance Normalization for Style-Trasnfer
-            x2 = AdaIN(x2, y2)
-            x3 = AdaIN(x3, y3)
+        # if te_input:
+        #     # Fully-connected network for processing the vector with echo-times
+        #     y2 = keras.layers.Dense(filters,activation='relu',kernel_initializer='he_uniform')(te)
+        #     y3 = keras.layers.Dense(filters,activation='relu',kernel_initializer='he_uniform')(te)
+        #     # Adaptive Instance Normalization for Style-Trasnfer
+        #     x2 = AdaIN(x2, y2)
+        #     x3 = AdaIN(x3, y3)
 
         # Update counter
         cont += 1
