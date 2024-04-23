fea325c1a962405befac77962625ab876940ccaf
jpmen <jpmeneses@uc.cl>
2024-04-23 15:50:10 -0400

    Submit TEaug-300: Train with variable number of echoes

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v321-sup
-#SBATCH --output=out_sup_321.txt
+#SBATCH --job-name=v300-TEa
+#SBATCH --output=out_TEa_300.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl
 
-python train-sup.py --dataset Sup-321 --data_size 384 --G_model U-Net --batch_size 16 --lr 0.001
+python train-IDEAL-TEaug.py --dataset TEaug-300 --data_size 384 --n_echoes 0 --out_vars PM --n_G_filters 36 --batch_size 16 --lr 0.001
