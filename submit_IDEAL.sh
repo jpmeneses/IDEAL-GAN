02e93ba63b68107dd98090643b905dab1add03ec
jpmen <jpmeneses@uc.cl>
2024-04-23 18:33:28 -0400

    Re-submit TEaug-300: Further reduce batch size and LR

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -7,4 +7,4 @@
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl
 
-python train-IDEAL-TEaug.py --dataset TEaug-300 --data_size 384 --n_echoes 0 --out_vars PM --n_G_filters 36 --batch_size 8 --lr 0.0008
+python train-IDEAL-TEaug.py --dataset TEaug-300 --data_size 384 --n_echoes 0 --out_vars PM --n_G_filters 36 --batch_size 4 --lr 0.0005
