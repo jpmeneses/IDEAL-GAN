85d32c9a2405b8c3e0f16c0a23cc8a3504e5dd4c
jpmen <jpmeneses@uc.cl>
2024-04-24 11:39:45 -0400

    Submit TEaug-301: Same as v300 but for 3T field strength

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v300-TEa
-#SBATCH --output=out_TEa_300.txt
+#SBATCH --job-name=v301-TEa
+#SBATCH --output=out_TEa_301.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl
 
-python train-IDEAL-TEaug.py --dataset TEaug-300 --data_size 384 --n_echoes 0 --out_vars PM --n_G_filters 36 --batch_size 4 --lr 0.0005
+python train-IDEAL-TEaug.py --dataset TEaug-301 --data_size 384 --n_echoes 0 --field 3.0 --out_vars PM --n_G_filters 36 --batch_size 4 --lr 0.0005
