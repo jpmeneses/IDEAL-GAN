e55886236ef34f2b2176b74e7fee2adfa1e7149f
jpmen <jpmeneses@uc.cl>
2024-04-29 12:01:02 -0400

    Re-submit VAE-GAN(v813): Try weaker APD-loss

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -7,4 +7,4 @@
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl
 
-python train-IDEAL-GAN.py --dataset GAN-813 --data_size 384 --rand_ne True --rand_ph_offset False --only_mag True --n_G_filt_list 36,72,72,144 --n_downsamplings 3 --div_decod True  --encoded_size 3 --adv_train True --cGAN True --batch_size 2 --epochs 200 --epoch_decay 200 --epoch_ckpt 50 --lr 0.001 --FM_loss_weight 0.5 --ls_reg_weight 5e-7
+python train-IDEAL-GAN.py --dataset GAN-813 --data_size 384 --rand_ne True --rand_ph_offset False --only_mag True --n_G_filt_list 36,72,72,144 --n_downsamplings 3 --div_decod True  --encoded_size 3 --adv_train True --cGAN True --batch_size 2 --epochs 200 --epoch_decay 200 --epoch_ckpt 50 --lr 0.001 --FM_loss_weight 0.5 --phi_0_loss_weight 1e-2 --ls_reg_weight 5e-7
