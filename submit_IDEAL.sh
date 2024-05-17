a17b53813c105d2f382778e38e46250bb4faf77d
jpmen <jpmeneses@uc.cl>
2024-05-17 12:04:19 -0400

    Submit KL-LDM(v813): LDM from ep-200 PI-VAE with linear beta scheduler

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v322-sup
-#SBATCH --output=out_sup_322.txt
+#SBATCH --job-name=v813-LDM
+#SBATCH --output=out_LDM_813.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl
 
-python train-sup.py --dataset Sup-322 --data_size 384 --DL_partial_real False --G_model U-Net --batch_size 16 --lr 0.001
+python train-ldm.py --experiment_dir output/GAN-813 --n_timesteps 500 --batch_size 8 --epochs_ldm 300 --epoch_ldm_ckpt 100 --lr 7e-5
