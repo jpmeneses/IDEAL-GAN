87f6c6e5ef132424b9bf126afcee54d70159582a
jpmen <jpmeneses@uc.cl>
2024-05-17 15:45:22 -0400

    Debug WF-phase dimensions on mag-and-phase mode

--- data.py
+++ data.py
@@ -89,9 +89,9 @@ def load_hdf5(ds_dir,hdf5_file,ech_idx=12,start=0,end=2000,custom_list=None,num_
         if mag_and_phase:
             out_w_mag = np.sqrt(np.sum(out_maps[:,:,:,:2]**2,axis=-1,keepdims=True))
             out_f_mag = np.sqrt(np.sum(out_maps[:,:,:,2:4]**2,axis=-1,keepdims=True))
-            out_w_pha = np.arctan2(out_maps[:,:,:,1:2],out_maps[:,:,:,0:1])
+            out_w_pha = np.arctan2(out_maps[:,:,:,1],out_maps[:,:,:,0])
             out_w_pha = unwrap_slices(out_w_pha)
-            out_f_pha = np.arctan2(out_maps[:,:,:,3:4],out_maps[:,:,:,2:3])
+            out_f_pha = np.arctan2(out_maps[:,:,:,3],out_maps[:,:,:,2])
             out_f_pha = unwrap_slices(out_f_pha)
             # out_wf_pha = np.nan_to_num((out_w_mag*out_w_pha+out_f_mag*out_f_pha)/(out_w_mag+out_f_mag))
             out_mag = np.expand_dims(np.concatenate((out_w_mag,out_f_mag,out_maps[:,:,:,4:5]),axis=-1),axis=1)
