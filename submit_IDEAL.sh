5c4fae194c9242310a4c9967a2a1620193750051
jpmen <jpmeneses@uc.cl>
2024-05-20 12:48:00 -0400

    Submit Sup-323 - WF U-Net trained with synthetic data only - 3330 samples

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-#SBATCH --job-name=v813-LDM
-#SBATCH --output=out_LDM_813.txt
+#SBATCH --job-name=v323-sup
+#SBATCH --output=out_sup_323.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl
 
-python train-ldm.py --experiment_dir output/GAN-813 --n_timesteps 500 --batch_size 8 --epochs_ldm 300 --epoch_ldm_ckpt 100 --lr 7e-5
+python gen_LDM_dataset.py --experiment_dir output/GAN-813 --MEBCRN False --batch_size 10 --n_samples 3330
+python train-sup.py --dataset Sup-323 --data_size 384 --DL_gen True --DL_filename LDM_ds_3330 --shuffle False --G_model U-Net --n_G_filters 36 --batch_size 16
