b75aebcb0a76a33ad3e61bdd5449b775f6290a60
jpmen <jpmeneses@uc.cl>
2024-05-23 12:27:13 -0400

    Submit Sup-326 - WF U-Net trained with synth data for TE base (step) of 1.4 (2.2) ms

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-#SBATCH --job-name=v325-sup
-#SBATCH --output=out_sup_325.txt
+#SBATCH --job-name=v326-sup
+#SBATCH --output=out_sup_326.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl
 
-python gen_LDM_dataset.py --experiment_dir output/GAN-813 --MEBCRN False --batch_size 10 --n_samples 3130
-python train-sup.py --dataset Sup-325 --data_size 384 --DL_gen True --DL_partial_real True --DL_filename LDM_ds_3130 --G_model U-Net --n_G_filters 36 --batch_size 16
+python gen_LDM_dataset.py --experiment_dir output/GAN-813 --MEBCRN False --batch_size 10 --n_samples 3330
+python train-sup.py --dataset Sup-326 --data_size 384 --DL_gen True --DL_filename LDM_ds_3330 --TE1 0.0014 --dTE 0.0021 --G_model U-Net --n_G_filters 36 --batch_size 16
