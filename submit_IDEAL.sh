614669ad25b2b15c8549db3c044fc46d405b5c2b
jpmen <jpmeneses@uc.cl>
2024-05-23 12:38:57 -0400

    Submit VAE-GAN(v814): Same as v813 (relaxed KL-reg, 3-DS, NE-aug, no only-mag) but with phase augmentation

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v326-sup
-#SBATCH --output=out_sup_326.txt
+#SBATCH --job-name=v814-GAN
+#SBATCH --output=out_GAN_814.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl
 
-python gen_LDM_dataset.py --experiment_dir output/GAN-813 --MEBCRN False --batch_size 10 --n_samples 3330
-python train-sup.py --dataset Sup-326 --data_size 384 --DL_gen True --DL_filename LDM_ds_3330 --TE1 0.0014 --dTE 0.0021 --G_model U-Net --n_G_filters 36 --batch_size 16
+python train-IDEAL-GAN.py --dataset GAN-814 --data_size 384 --rand_ne True --rand_ph_offset True --n_G_filt_list 36,72,72,144 --n_downsamplings 3 --encoded_size 3 --adv_train True --cGAN True --batch_size 2 --epochs 200 --epoch_decay 200 --epoch_ckpt 50 --lr 0.001 --FM_loss_weight 0.5 --ls_reg_weight 5e-7
