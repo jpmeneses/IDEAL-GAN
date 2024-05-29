b7978806e7af20eb185dc9544b91e391cf07347b
jpmen <jpmeneses@uc.cl>
2024-05-29 13:06:54 -0400

    Submit Sup-328 - WF U-Net trained with mixed data for TE base (step) of 1.4 (2.2) ms

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-#SBATCH --job-name=v900-GAN
-#SBATCH --output=out_GAN_900.txt
+#SBATCH --job-name=v328-sup
+#SBATCH --output=out_sup_328.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl
 
-python train-IDEAL-GAN.py --dataset GAN-900 --data_size 384 --rand_ne True --rand_ph_offset False --n_G_filt_list 36,72,72,144 --n_downsamplings 3 --encoded_size 3 --VQ_encoder True --adv_train True --cGAN True --batch_size 2 --epochs 200 --epoch_decay 200 --epoch_ckpt 50 --lr 0.001 --FM_loss_weight 0.5 --ls_reg_weight 1e0
+python gen_LDM_dataset.py --experiment_dir output/GAN-813 --MEBCRN False --batch_size 4 --n_samples 3124
+python train-sup.py --dataset Sup-328 --data_size 384 --DL_gen True --DL_partial_real True --DL_filename LDM_ds_3124 --TE1 0.0014 --dTE 0.0022 --G_model U-Net --n_G_filters 36 --batch_size 16
