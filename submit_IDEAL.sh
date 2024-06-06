8bff43aa22e4b108929f90bfebec630b649473c9
jpmen <jpmeneses@uc.cl>
2024-06-06 13:21:45 -0400

    Submit VQ-LDM(v900): LDM from ep-200 PI-VQVAE with linear beta scheduler

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v328-sup
-#SBATCH --output=out_sup_328.txt
+#SBATCH --job-name=v900-LDM
+#SBATCH --output=out_LDM_900.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl
 
-python gen_LDM_dataset.py --experiment_dir output/GAN-813 --MEBCRN False --batch_size 4 --n_samples 3124
-python train-sup.py --dataset Sup-328 --data_size 384 --DL_gen True --DL_partial_real True --DL_filename LDM_ds_3124 --TE1 0.0014 --dTE 0.0022 --G_model U-Net --n_G_filters 36 --batch_size 16
+python train-ldm.py --experiment_dir output/GAN-900 --n_timesteps 500 --batch_size 8 --epochs_ldm 300 --epoch_ldm_ckpt 100 --lr 7e-5
