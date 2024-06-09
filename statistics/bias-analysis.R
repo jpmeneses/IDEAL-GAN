975c3f329b90940ea67dc81caa6ea0d4a951c328
jpmen <jpmeneses@uc.cl>
2024-06-08 22:02:28 -0400

    Visualize each group's variance and add Bland-Altman plot

--- statistics/bias-analysis.R
+++ statistics/bias-analysis.R
@@ -57,7 +57,6 @@ for (i in c(1:length(ls_sheets)))
 	}
 }
 }
-# meas = meas/length(ls_sheets)
 n_data = length(refs)
 
 # Create a data frame
@@ -73,18 +72,17 @@ Site_Prot = factor(c(im_id),labels=c("S1-P1","S1-P2",
 # 		   "S2-P1","S2-P2","S3-P1","S3-P2","S4-P2","S6-P1","S6-P2"))
 
 
+# DEPICT BIAS DIFFERENCES DEPENDING ON THE USED METHOD
+boxplot(bias ~ method, data = pdff_Data)
+# pdff_Data <- subset(pdff_Data, method!="MDWF-Net")
+
 ## LINEAR MIXED MODEL
 mixed.lmer <- lmer(bias ~ refs + (1|Site_Prot) + (1|method), data=pdff_Data)
 summary(mixed.lmer)
-
-plot(mixed.lmer)
-
-qqnorm(resid(mixed.lmer))
-qqline(resid(mixed.lmer))
+# plot(mixed.lmer)
 
 full.lmer <- lmer(bias ~ refs + (1|Site_Prot) + (1|method), data=pdff_Data, REML=FALSE)
 reduced.lmer <- lmer(bias ~ refs + (1|Site_Prot), data=pdff_Data, REML=FALSE)
-
 anova(reduced.lmer, full.lmer)
 
 ## GROUP BY METHOD AND FAT LEVEL
@@ -93,4 +91,27 @@ pdff_group <- pdff_Data %>%
   summarise(mBias = mean(bias),
 		LoA = 1.96*sd(bias),
 		n = n())
-pdff_group
+print(pdff_group, n=55)
+
+## BLAND-ALTMAN BY METHOD
+sel_meth = "MDWF-Net"
+pdff_meth = subset(pdff_Data, method==sel_meth)
+pdff_meth <- within(pdff_meth, meas <- bias+refs)
+meth.lmer <- lmer(meas ~ refs + (1|Site_Prot), data=pdff_meth)
+summary(meth.lmer)
+
+mean_diff <- mean(pdff_meth$bias)
+lower <- mean_diff - 1.96*sd(pdff_meth$bias)
+upper <- mean_diff + 1.96*sd(pdff_meth$bias)
+if (map=="PDFF") {yl_ba=100.0} else {yl_ba=40.0}
+q2= ggplot(pdff_meth, aes(refs, bias)) +
+  geom_point(aes(color = Site_Prot))+
+  geom_hline(yintercept = mean_diff) +
+  geom_hline(yintercept = lower, color = "red", linetype="dashed") +
+  geom_hline(yintercept = upper, color = "red", linetype="dashed") +
+  theme(text = element_text(size = 14)) +
+  ylim(-yl_ba,yl_ba) +
+  ylab("Difference") +
+  xlab("Ground-Truth")
+fn2 = paste(map,sel_meth,"Bias-BlandAltman.png",sep="-")
+ggsave(plot=q2, width=5, height=3, dpi=400, filename=fn2)
