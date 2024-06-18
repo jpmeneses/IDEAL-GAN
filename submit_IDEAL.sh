a0686dd1caf81256da2ba0bee83c9013f8b914a1
jpmen <jpmeneses@uc.cl>
2024-06-18 13:14:44 -0400

    Submit Unsup-104: Train FM model only using new recon uncertainty quantification

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v900-LDM
-#SBATCH --output=out_LDM_900.txt
+#SBATCH --job-name=v104-unsup
+#SBATCH --output=out_unsup_104.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl
 
-python train-ldm.py --experiment_dir output/GAN-900 --n_timesteps 500 --batch_size 8 --epochs_ldm 300 --epoch_ldm_ckpt 100 --lr 7e-5
+python train-IDEAL-unsup.py --dataset Unsup-104 --rand_ne True --UQ True --epochs 40 
