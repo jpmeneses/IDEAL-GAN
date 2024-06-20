0906a3f49ddcbcff88ed6ba38f1dd1f04300ffa2
jpmen <jpmeneses@uc.cl>
2024-06-20 10:01:01 -0400

    Re-submit Unsup-105: Train R2s model only

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v106-unsup
-#SBATCH --output=out_unsup_106.txt
+#SBATCH --job-name=v105-unsup
+#SBATCH --output=out_unsup_105.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl
 
-python train-IDEAL-unsup.py --dataset Unsup-106 --rand_ne False --UQ True --epochs 40 
+python train-IDEAL-unsup.py --dataset Unsup-105 --rand_ne False --out_vars R2s --UQ True --epochs 40 
