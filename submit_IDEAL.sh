deb5491aec9ca1f2a6b464e3748bbbe50d4dab7f
jpmen <jpmeneses@uc.cl>
2024-06-21 09:42:26 -0400

    Re-submit Unsup-107: Continue with R2s training with modified recon for 10 epochs

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v108-unsup
-#SBATCH --output=out_unsup_108.txt
+#SBATCH --job-name=v107-unsup
+#SBATCH --output=out_unsup_107.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl
 
-python train-IDEAL-unsup.py --dataset Unsup-108 --rand_ne False --UQ True --epochs 30 --FM_TV_weight 1e-6
+python train-IDEAL-unsup.py --dataset Unsup-107 --rand_ne False --out_vars R2s --UQ True --epochs 70
