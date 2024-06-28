ef339d1a897308c6b37eaa6390b94521bb7f29b1
jpmen <jpmeneses@uc.cl>
2024-06-28 00:13:31 -0400

    Submit Unsup-109: Train FM model with corrected variance estimation for 30 epochs

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v108-unsup
-#SBATCH --output=out_unsup_108.txt
+#SBATCH --job-name=v109-unsup
+#SBATCH --output=out_unsup_109.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl
 
-python train-IDEAL-unsup.py --dataset Unsup-108 --rand_ne False --out_vars R2s --UQ True --epochs 40 --beta_1 0.5 --beta_2 0.9
+python train-IDEAL-unsup.py --dataset Unsup-109 --rand_ne False --out_vars FM --UQ True --epochs 30 --beta_1 0.5 --beta_2 0.9
