830f533c0e02f97910953b1a90b1cca910e2e41c
jpmen <jpmeneses@uc.cl>
2024-07-03 16:38:28 -0400

    Change to simplified R2s uncertainty

--- wflib/IDEAL_model.py
+++ wflib/IDEAL_model.py
@@ -562,24 +562,18 @@ def acq_uncertainty(mean_maps, var_maps, ne=6, te=None, rem_R2=False, only_mag=F
 
     # r2s = mean_maps[:,2,:,:,1] * r2_sc
     # phi = mean_maps[:,2,:,:,0] * fm_sc
-    r2s_nu = var_maps[...,1] * (r2_sc)
+    r2s_mu = var_maps[...,1] * (r2_sc)
     r2s_sigma = var_maps[...,2] * (r2_sc) #CHECK
     phi_sigma = var_maps[...,0] * (fm_sc)
 
-    r2s_nu_rav = tf.expand_dims(tf.reshape(r2s_nu,[n_batch,-1]),1) # (nb,1,nv)
     r2s_sigma_rav = tf.expand_dims(tf.reshape(r2s_sigma,[n_batch,-1]),1) # (nb,1,nv)
     phi_sigma_rav = tf.expand_dims(tf.reshape(phi_sigma,[n_batch,-1]),1) # (nb,1,nv)
 
     # Diagonal matrix with the exponential of fieldmap variance
     Wp_var = tf.linalg.matmul((2*np.pi * te)**2, phi_sigma_rav**2) # (nb,ne,nv) NEG
     if not(rem_R2):
-        r2s_var_aux = tf.math.divide_no_nan(-r2s_nu_rav**2, 2*r2s_sigma_rav**2)# (nb,1,nv) 
-        r2s_var = (1-r2s_var_aux)*tf.math.special.bessel_i0e(-r2s_var_aux/2)
-        r2s_var -= r2s_var_aux*tf.math.special.bessel_i1e(-r2s_var_aux/2)
-        r2s_var *= tf.math.exp(r2s_var_aux/2) # Laguerre polynomial 
-        r2s_var *= -(np.pi*r2s_sigma_rav**2)/2 # (nb,1,nv)
-        r2s_var += 2*r2s_sigma_rav**2 + r2s_nu_rav**2
-        Wp_var += tf.linalg.matmul((te)**2, r2s_var) # NEG
+        r2s_var_aux = tf.math.exp(tf.linalg.matmul(-te, r2s_mu))
+        Wp_var += tf.linalg.matmul(te**2, r2s_sigma_rav**2) # (nb,ne,nv) NEG
 
     # Matrix operations (variance)
     Mp = tf.linalg.matmul(M, rho_mtx) # (nb,ne,nv)
