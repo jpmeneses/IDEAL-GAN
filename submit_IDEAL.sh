a35025dd1083f6e5d2c479b25f0f5e8140128702
jpmen <jpmeneses@uc.cl>
2024-07-08 12:33:27 -0400

    Submit Unsup-111: Same as v110, but with exact truncated normal's variance

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v110-unsup
-#SBATCH --output=out_unsup_110.txt
+#SBATCH --job-name=v111-unsup
+#SBATCH --output=out_unsup_111.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl
 
-python train-IDEAL-unsup.py --dataset Unsup-110 --rand_ne False --out_vars R2s --UQ True --UQ_R2s True --epochs 40 --beta_1 0.5 --beta_2 0.9
+python train-IDEAL-unsup.py --dataset Unsup-111 --rand_ne False --out_vars R2s --UQ True --UQ_R2s True --epochs 40 --beta_1 0.5 --beta_2 0.9
