0fdcacfb19862594403907efd76f66e83d504365
jpmen <jpmeneses@uc.cl>
2024-07-08 17:58:26 -0400

    Submit Unsup-112: Train FM model considering bi-modal Laplace distribution

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v111-unsup
-#SBATCH --output=out_unsup_111.txt
+#SBATCH --job-name=v112-unsup
+#SBATCH --output=out_unsup_112.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl
 
-python train-IDEAL-unsup.py --dataset Unsup-111 --rand_ne False --out_vars R2s --UQ True --UQ_R2s True --epochs 40 --beta_1 0.5 --beta_2 0.9
+python train-IDEAL-unsup.py --dataset Unsup-112 --rand_ne False --out_vars FM --UQ True --UQ_R2s True --epochs 30 --beta_1 0.5 --beta_2 0.9
