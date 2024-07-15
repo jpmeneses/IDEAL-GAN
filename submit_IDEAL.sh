05db8f015eba8b1390c3bd25df469fcc8e6fc9f5
jpmen <jpmeneses@uc.cl>
2024-07-15 16:54:10 -0400

    Re-submit Unsup-111: Continue joint training for 10 more epochs

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v112-unsup
-#SBATCH --output=out_unsup_112.txt
+#SBATCH --job-name=v111-unsup
+#SBATCH --output=out_unsup_111.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl
 
-python train-IDEAL-unsup.py --dataset Unsup-112 --rand_ne False --out_vars FM --UQ True --UQ_R2s True --batch_size 8 --epochs 30 --epoch_ckpt 2 --lr 0.001
+python train-IDEAL-unsup.py --dataset Unsup-111 --rand_ne False --out_vars PM --UQ True --UQ_R2s True --batch_size 8 --epochs 50 --epoch_ckpt 2 --lr 0.001
