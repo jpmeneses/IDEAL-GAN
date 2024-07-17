c861344f50e0b8c72a2af04b15a0a607bd1a6d89
jpmen <jpmeneses@uc.cl>
2024-07-16 21:22:26 -0400

    Re-submit Unsup-110: Joint training (10 epochs) from pre-trained FM-model and randomly initialized R2s model

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v111-unsup
-#SBATCH --output=out_unsup_111.txt
+#SBATCH --job-name=v110-unsup
+#SBATCH --output=out_unsup_110.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl
 
-python train-IDEAL-unsup.py --dataset Unsup-111 --rand_ne False --out_vars PM --UQ True --UQ_R2s True --batch_size 8 --epochs 50 --epoch_ckpt 2 --lr 0.001
+python train-IDEAL-unsup.py --dataset Unsup-110 --rand_ne False --out_vars PM --UQ True --UQ_R2s True --batch_size 8 --epochs 40 --epoch_ckpt 2 --lr 0.001
