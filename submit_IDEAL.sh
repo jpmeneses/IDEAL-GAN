6eb592ce56720ee2145178de1d69ea386b8c1461
jpmen <jpmeneses@uc.cl>
2024-07-18 23:26:26 -0400

    Re-submit Unsup-110: Re-adjust FM model considering v111 R2s model as baseline

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -7,4 +7,4 @@
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl
 
-python train-IDEAL-unsup.py --dataset Unsup-110 --rand_ne False --out_vars PM --UQ True --UQ_R2s True --batch_size 8 --epochs 40 --epoch_ckpt 2 --lr 0.001
+python train-IDEAL-unsup.py --dataset Unsup-110 --rand_ne False --out_vars PM --UQ True --UQ_R2s True --epochs 50 --epoch_ckpt 2 --beta_1 0.5 --beta_2 0.9
