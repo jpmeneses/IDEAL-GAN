6cae08b034c92498d97a03829613b206f9ca47a0
jpmen <jpmeneses@uc.cl>
2024-07-19 20:28:35 -0400

    Re-submit Unsup-v111: Decrease learning rate

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -7,4 +7,4 @@
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl
 
-python train-IDEAL-unsup.py --dataset Unsup-111 --rand_ne False --out_vars R2s --UQ True --UQ_R2s True --UQ_calib True --epochs 50 --epoch_ckpt 2 --beta_1 0.5 --beta_2 0.9
+python train-IDEAL-unsup.py --dataset Unsup-111 --rand_ne False --out_vars R2s --UQ True --UQ_R2s True --UQ_calib True --epochs 50 --epoch_ckpt 2 --lr 0.000001 --beta_1 0.5 --beta_2 0.9
