7baeb32f00896e05cdfa423611ce768a18220979
jpmen <jpmeneses@uc.cl>
2024-07-20 10:07:10 -0400

    Submit Unsup-v113: Complete training and calibration from partially fitted FM model (v109)

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-#SBATCH --job-name=v111-unsup
-#SBATCH --output=out_unsup_111.txt
+#SBATCH --job-name=v113-unsup
+#SBATCH --output=out_unsup_113.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl
 
-python train-IDEAL-unsup.py --dataset Unsup-111 --rand_ne False --out_vars R2s --UQ True --UQ_R2s True --UQ_calib True --epochs 50 --epoch_ckpt 2 --lr 0.000001 --beta_1 0.5 --beta_2 0.9
+python train-IDEAL-unsup.py --dataset Unsup-113 --rand_ne False --out_vars R2s --UQ True --UQ_R2s True --epochs 33 --beta_1 0.5 --beta_2 0.9
+python train-IDEAL-unsup.py --dataset Unsup-113 --rand_ne False --out_vars PM --UQ True --UQ_R2s True --epochs 35 --beta_1 0.5 --beta_2 0.9
+python train-IDEAL-unsup.py --dataset Unsup-113 --rand_ne False --out_vars R2s --UQ True --UQ_R2s True --UQ_calib True --epochs 40 --epoch_ckpt 2 --lr 0.000001 --beta_1 0.5 --beta_2 0.9
