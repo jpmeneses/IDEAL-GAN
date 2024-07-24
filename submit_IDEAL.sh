047a5fb9221ebfcb0e92bb49dc1afbac2c81b854
jpmen <jpmeneses@uc.cl>
2024-07-24 16:12:09 -0400

    Re-submit Unsup-v113: Train R2s calibration for 10 epochs starting from fine-tuned FM-model

--- submit_IDEAL.sh
+++ submit_IDEAL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=v113a-unsup
-#SBATCH --output=out_unsup_113a.txt
+#SBATCH --job-name=v113-unsup
+#SBATCH --output=out_unsup_113.txt
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:quadro_rtx_8000:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpmeneses@uc.cl
 
-python train-IDEAL-unsup.py --dataset Unsup-113a --rand_ne False --out_vars R2s --UQ True --UQ_R2s True --UQ_calib True --epochs 35 --epoch_ckpt 1 --lr 0.00001 --beta_1 0.5 --beta_2 0.9
+python train-IDEAL-unsup.py --dataset Unsup-113 --rand_ne False --out_vars R2s --UQ True --UQ_R2s True --UQ_calib True --epochs 45 --lr 0.000001 --beta_1 0.5 --beta_2 0.9
