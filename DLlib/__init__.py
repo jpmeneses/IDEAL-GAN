d696e3faa146a6439aa92aa26b223f53edf86a6a
jpmen <jpmeneses@uc.cl>
2025-01-13 11:15:19 -0300

    Remove deprecated MEBCRN lib

--- DLlib/__init__.py
+++ DLlib/__init__.py
@@ -2,7 +2,6 @@ from DLlib.attention import *
 from DLlib.bn import *
 from DLlib.complex_utils import *
 from DLlib.ldm_module import *
-from DLlib.mebcrn import *
 from DLlib.metrics import *
 from DLlib.module import *
 from DLlib.utils import *
