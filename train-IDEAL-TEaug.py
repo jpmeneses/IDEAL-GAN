d0499ef1b5adca0a00667b492cc27cf1d08c532a
jpmen <jpmeneses@uc.cl>
2025-01-21 17:56:08 -0300

    Add more checkpoints

--- train-IDEAL-TEaug.py
+++ train-IDEAL-TEaug.py
@@ -512,6 +512,8 @@ sample_dir = py.join(output_dir, 'samples_training')
 py.mkdir(sample_dir)
 n_div = np.ceil(total_steps/len(out_maps_1))
 
+print(tf.config.experimental.get_memory_info("GPU:0"))
+
 # main loop
 for ep in range(args.epochs):
     if ep < ep_cnt:
@@ -545,6 +547,7 @@ for ep in range(args.epochs):
                 B_FM = B[:,2:,:,:,:1] * tf.random.normal([1],mean=args.FM_mean,stddev=0.25,dtype=tf.float32)
                 B_PM = tf.concat([B_FM,B[:,2:,:,:,1:]], axis=-1)
                 B = tf.concat([B[:,:2,:,:,:],B_PM], axis=1)
+        print(tf.config.experimental.get_memory_info("GPU:0"))
         # ==============================================================================
 
         # ==============================================================================
