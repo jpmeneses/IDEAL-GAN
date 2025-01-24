fa5c00d33b3ea32bba067238a577ab4f9a35e668
jpmen <jpmeneses@uc.cl>
2025-01-24 11:43:10 -0300

    Change linear LR conditional format

--- DLlib/module.py
+++ DLlib/module.py
@@ -855,12 +855,15 @@ class LinearDecay(keras.optimizers.schedules.LearningRateSchedule):
         self.current_learning_rate = tf.Variable(initial_value=initial_learning_rate, trainable=False, dtype=tf.float32)
 
     def __call__(self, step):
-        self.current_learning_rate.assign(tf.cond(
-            tf.math.reduce_all(step >= self._step_decay,self._steps > self._step_decay),
-            true_fn=lambda: self._initial_learning_rate * (1 - 1 / (self._steps - self._step_decay) * (step - self._step_decay)),
-            false_fn=lambda: self._initial_learning_rate
-        ))
-        return self.current_learning_rate
+        if self._steps > self._step_decay:
+            self.current_learning_rate.assign(tf.cond(
+                step >= self._step_decay,
+                true_fn=lambda: self._initial_learning_rate * (1 - 1 / (self._steps - self._step_decay) * (step - self._step_decay)),
+                false_fn=lambda: self._initial_learning_rate
+            ))
+            return self.current_learning_rate
+        else:
+            return self._initial_learning_rate
 
 
 # ==============================================================================
