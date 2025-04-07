2ad867a1b8ec8d5d7b244ba91dcfa2a4aa4beb81
jpmen <jpmeneses@uc.cl>
2025-04-07 16:57:21 +1000

    Debug PM extraction and output concat for B conversion from mag-to-phase format

--- data.py
+++ data.py
@@ -278,8 +278,8 @@ def B_from_MEBCRN(B,mag_and_phase=False,c_pha=3):
         B_F_r = B[:,0,:,:,1:2]* tf.math.cos(c_pha*B[:,1,:,:,1:2]*np.pi)
         B_F_i = B[:,0,:,:,1:2]* tf.math.sin(c_pha*B[:,1,:,:,1:2]*np.pi)
         B_r2 = B[:,0,:,:,2:]
-        B_fm = B[:,0,:,:,2:]
-        B = tf.concat([Z2B_W_r,Z2B_W_i,Z2B_F_r,Z2B_F_i,Z2B_r2,Z2B_fm],axis=-1)
+        B_fm = B[:,1,:,:,2:]
+        B = tf.concat([B_W_r,B_W_i,B_F_r,B_F_i,B_r2,B_fm],axis=-1)
     else:
         B_W = B[:,0,:,:,:]
         B_F = B[:,1,:,:,:]
