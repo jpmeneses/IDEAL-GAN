2bcf151f697d43b167042fe5ce53c8b6d5816519
jpmen <jpmeneses@uc.cl>
2025-05-28 13:58:36 +1000

    Conditional denoising U-Net

--- DLlib/ldm_module.py
+++ DLlib/ldm_module.py
@@ -16,88 +16,115 @@ class denoise_Unet(Model):
                  channels=3,
                  resnet_block_groups=8,
                  learned_variance=False,
-                 sinusoidal_cond_mlp=True
+                 sinusoidal_cond_mlp=True,
+                 num_classes=None,
+                 class_embedder=None,
+                 class_emb_dim=64,
+                 in_res=64
                  ):
         super(denoise_Unet, self).__init__()
-        
+
         # determine dimensions
         self.channels = channels
-        
+        self.in_res = in_res
+
+        self.class_embeddings = nn.Embedding(num_classes, class_emb_dim) if class_embedder is None else class_embedder
+
         init_dim = default(init_dim, dim // 3 * 2)
         self.init_conv = nn.Conv2D(filters=init_dim, kernel_size=7, strides=1, padding='SAME')
-        
+
         dims = [init_dim, *map(lambda m: dim * m, dim_mults)]
         in_out = list(zip(dims[:-1], dims[1:]))
-        
+
         block_klass = partial(ResnetBlock, groups = resnet_block_groups)
         
         # time embeddings
         time_dim = dim * 4
         self.sinusoidal_cond_mlp = sinusoidal_cond_mlp
-        
-        self.time_mlp = Sequential([
-            SinusoidalPosEmb(dim),
-            nn.Dense(units=time_dim),
-            GELU(),
-            nn.Dense(units=time_dim)
-        ], name="time embeddings")
-        
+
+        if sinusoidal_cond_mlp:
+            self.time_mlp = Sequential([
+                SinusoidalPosEmb(dim),
+                nn.Dense(units=time_dim),
+                GELU(),
+                nn.Dense(units=time_dim)
+            ], name="time embeddings")
+        else:
+            self.time_mlp = MLP(time_dim)
+
         # layers
         self.downs = []
         self.ups = []
         num_resolutions = len(in_out)
-        
+
+        now_res = in_res
+
         for ind, (dim_in, dim_out) in enumerate(in_out):
             is_last = ind >= (num_resolutions - 1)
 
             self.downs.append([
-                block_klass(dim_in, dim_out, time_emb_dim=time_dim),
+                ClassConditioning(now_res),
+                block_klass(dim_in+1, dim_out, time_emb_dim=time_dim),
                 block_klass(dim_out, dim_out, time_emb_dim=time_dim),
                 Residual(PreNorm(dim_out, LinearAttention(dim_out))),
                 Downsample(dim_out) if not is_last else Identity()
             ])
-  
+
+            now_res //= 2 if not is_last else 1
+
         mid_dim = dims[-1]
-        self.mid_block1 = block_klass(mid_dim, mid_dim, time_emb_dim=time_dim)
+        self.mid_class_conditioning = ClassConditioning(now_res)
+        self.mid_block1 = block_klass(mid_dim+1, mid_dim, time_emb_dim=time_dim)
         self.mid_attn = Residual(PreNorm(mid_dim, Attention(mid_dim)))
         self.mid_block2 = block_klass(mid_dim, mid_dim, time_emb_dim=time_dim)
-        
+
         for ind, (dim_in, dim_out) in enumerate(reversed(in_out[1:])):
             is_last = ind >= (num_resolutions - 1)
 
             self.ups.append([
-                block_klass(dim_out * 2, dim_in, time_emb_dim=time_dim),
+                ClassConditioning(now_res),
+                block_klass((dim_out * 2) + 1, dim_in, time_emb_dim=time_dim),
                 block_klass(dim_in, dim_in, time_emb_dim=time_dim),
                 Residual(PreNorm(dim_in, LinearAttention(dim_in))),
                 Upsample(dim_in) if not is_last else Identity()
             ])
-        
+
+            now_res *= 2 if not is_last else 1
+
         default_out_dim = channels * (1 if not learned_variance else 2)
         self.out_dim = default(out_dim, default_out_dim)
-        
+
         self.final_conv = Sequential([
             block_klass(dim * 2, dim),
             nn.Conv2D(filters=self.out_dim, kernel_size=1, strides=1)
         ], name="output")
-        
-    def call(self, x, time=None, training=True, **kwargs):
+
+    def call(self, x, time=None, class_vector=None, training=True, **kwargs):
         x = self.init_conv(x)
         t = self.time_mlp(time)
+
+        class_vector = self.class_embeddings(class_vector)
         
         h = []
 
-        for block1, block2, attn, downsample in self.downs:
+        for class_conditioning, block1, block2, attn, downsample in self.downs:
+            cv = class_conditioning(class_vector)
+            x = tf.concat([x, cv], axis=-1)
             x = block1(x, t)
             x = block2(x, t)
             x = attn(x)
             h.append(x)
             x = downsample(x)
-
+        
+        cv = self.mid_class_conditioning(class_vector)
+        x = tf.concat([x, cv], axis=-1)
         x = self.mid_block1(x, t)
         x = self.mid_attn(x)
         x = self.mid_block2(x, t)
 
-        for block1, block2, attn, upsample in self.ups:
+        for class_conditioning, block1, block2, attn, upsample in self.ups:
+            cv = class_conditioning(class_vector)
+            x = tf.concat([x, cv], axis=-1)
             x = tf.concat([x, h.pop()], axis=-1)
             x = block1(x, t)
             x = block2(x, t)
