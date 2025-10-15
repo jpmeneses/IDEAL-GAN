82f71e4c82d579b92352d3564c25eaaa696f7684
jpmen <jpmeneses@uc.cl>
2025-10-15 11:09:19 +1100

    Limit variance value in stochastic U-Net using a sigmoid activation function

--- DLlib/module.py
+++ DLlib/module.py
@@ -409,7 +409,7 @@ def UNet(
     if bayesian or std_out:
         x_std = keras.layers.Conv2D(16, (1,1), activation='relu', kernel_initializer='he_uniform')(x)
         # Compute standard deviation (sigma; NOT sigma^2)
-        out_var = keras.layers.Conv2D(n_out, (1,1), activation='softplus', kernel_initializer='he_normal')(x_std)
+        out_var = keras.layers.Conv2D(n_out, (1,1), activation='sigmoid', kernel_initializer='he_normal')(x_std)
         if ME_layer:
             out_var = keras.layers.Lambda(lambda z: tf.expand_dims(z,axis=1))(out_var)
         if bayesian:
