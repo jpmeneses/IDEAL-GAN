7c02161d7454dd0c77a8b38ea33a9acc2e43976a
jpmen <jpmeneses@uc.cl>
2025-10-22 23:00:29 +1100

    Update PDFF variance calculation to magnitude-disc format

--- ROI-realPhantom.py
+++ ROI-realPhantom.py
@@ -178,10 +178,16 @@ for A, B, TE in tqdm.tqdm(A_B_dataset_test, desc='Testing Samples Loop', total=l
     F_var = tf.abs(tf.complex(A2B_var[:,3,:,:,:1],A2B_var[:,3,:,:,1:]))
     r2s_var = A2B_var[:,-1,:,:,1:]*(r2_sc**2)
 
-    PDFF_var = W_var/(A2B_WF_abs[...,:1]**2)
-    PDFF_var -= 2 * WF_var / (A2B_WF_abs[...,:1]*A2B_WFsum_abs)
-    PDFF_var += (W_var + F_var + 2*WF_var)/(A2B_WF_abs[...,:1])
-    PDFF_var *= A2B_WF_abs[...,:1]**2 / (A2B_WFsum_abs)**2 #[W_var,WF_var,F_var]
+    # if args.magnitude_disc:
+    #   Aux_WF = tf.where(A2B_WF_abs[...,:1]<A2B_WF_abs[...,1:],A2B_WF_abs[...,1:],A2B_WF_abs[...,:1])
+    #   Aux_var = tf.where(A2B_WF_abs[...,:1]<A2B_WF_abs[...,1:],F_var,W_var)
+    # else:
+    Aux_WF = A2B_WF_abs[...,1:]
+    Aux_var = F_var
+    PDFF_var = Aux_var / (Aux_WF**2)
+    PDFF_var -= 2 * WF_var / (Aux_WF*A2B_WFsum_abs)
+    PDFF_var += (W_var + F_var + 2*WF_var) / A2B_WFsum_abs
+    PDFF_var *= Aux_WF**2 / (A2B_WFsum_abs)**2 #[W_var,WF_var,F_var]
 
     A2B = tf.concat([A2B,PDFF_var],axis=-1)
 
